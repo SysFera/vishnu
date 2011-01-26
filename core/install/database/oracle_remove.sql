fee926c67f6e76fecc14c288ac51175f1bee0f19
kcoulomb <kevin.coulomb@gmail.com>
2011-01-26 17:54:54 +0100

    Add creation/remove script for sql db

---
+++ core/install/database/oracle_remove.sql
@@ -0,0 +1,35 @@
+CREATE SEQUENCE GlobalSequence;
+
+DROP TABLE job;
+
+DROP TABLE fileSub;
+
+DROP TABLE filetransfer;
+
+DROP TABLE command;
+
+DROP TABLE threshold;
+
+DROP TABLE optionvalue;
+
+DROP TABLE account;
+
+DROP TABLE vsession;
+
+DROP TABLE description;
+
+DROP TABLE cpu;
+
+DROP TABLE state;
+
+DROP TABLE users;
+
+DROP TABLE machine;
+
+DROP TABLE vishnu;
+
+DROP TABLE clmachine;
+
+DROP TABLE optionu;
+
+
