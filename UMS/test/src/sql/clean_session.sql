2f8528f44648da97f6fd34ce02992779a19010f5
kcoulomb <kevin.coulomb@gmail.com>
2011-02-03 12:30:12 +0100

    Add some UMS test. Debug POSTGREDb.cc, some debut cout are still in the code

--- UMS/test/src/sql/clean_session.sql
+++ UMS/test/src/sql/clean_session.sql
@@ -1,2 +1,5 @@
-DELETE * FROM sessions;
-DELETE * FROM users WHERE userid='toto_1';
+DELETE FROM sessions;
+DELETE FROM account;
+DELETE FROM users    WHERE userid='toto_1';
+DELETE FROM users    WHERE userid='toto_1';
+DELETE FROM machine  WHERE machineid='machine_2';
