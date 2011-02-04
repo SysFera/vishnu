66306c1be9294af91c3ec32b616822313b964944
traore <traore@DaoudaSys.(none)>
2011-02-04 09:53:25 +0100

    enlèvement des lignes commentées dans cli

--- UMS/src/cli/api.h
+++ UMS/src/cli/api.h
@@ -11,3 +11,9 @@ int connect(const std::string& userId, const std::string& password, std::string&
 int reconnect(const std::string& userId, const std::string& password, const std::string& sessionId, std::string& sessionKey);
 
 int close(const std::string& sessionKey);
+
+int addVishnuUser(const std::string& sessionKey, const UMS_Data::User& user);
+
+int updateUser(const std::string& sessionKey, const UMS_Data::User& user);
+
+int deleteUser(const std::string& sessionKey, const std::string& userId);
