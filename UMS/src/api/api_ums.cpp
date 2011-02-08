c1f5837d5a3b620d103824bb669ab0cf3ba7c61d
Daouda Traore <daouda.traore@sysfera.com>
2011-02-08 15:30:55 +0100

    ajout des fonctions configurationSave et configurationRestore dans l'api et client

--- UMS/src/api/api_ums.cpp
+++ UMS/src/api/api_ums.cpp
@@ -3,6 +3,42 @@
 
 using namespace std;
 
+///////////////////////////////////////A ENLEVER APRES C'EST JUSTE POUR DES TESTS//////////////////////////////
+void print(UMS_Data::User_ptr user, int id) {
+
+  cout << "============ User " << id << " Content ===========" << endl;
+  cout << "    firstname=" << user->getFirstname() << endl;
+  cout << "    lastname=" << user->getLastname() << endl;
+  cout << "    privilege=" << user->getPrivilege() << endl;
+  cout << "    mail=" << user->getEmail() << endl;
+  cout << "    userId=" << user->getUserId() << endl;
+  cout << "=======================================" << endl;
+}
+
+void print(UMS_Data::Machine_ptr machine, int id) {
+
+  cout << "============ machine " << id << " Content ===========" << endl;
+  cout << "    machineId=" << machine->getMachineId() << endl;
+  cout << "    name=" << machine->getName() << endl;
+  cout << "    site=" << machine->getSite() << endl;
+  cout << "    machineDescription=" << machine->getMachineDescription() << endl;
+  cout << "    language=" << machine->getLanguage() << endl;
+  cout << "=======================================" << endl;
+}
+
+void  print(UMS_Data::LocalAccount_ptr localAccount, int id) {
+
+  cout << "============ localAccount " << id << " Content ===========" << endl;
+  cout << "    UserId=" << localAccount->getUserId() << endl;
+  cout << "    machineId=" << localAccount->getMachineId() << endl;
+  cout << "    acLogin=" << localAccount->getAcLogin() << endl;
+  cout << "    sshKeyPath=" << localAccount->getSshKeyPath() << endl;
+  cout << "    homeDirectory=" << localAccount->getHomeDirectory() << endl;
+  cout << "=======================================" << endl;
+
+}
+//////////////////////////////////////////FIN A ENLEVER////////////////////////////////////////////////////////
+
 int connect(const string& userId, const string& password, string& sessionKey, const UMS_Data::ConnectOptions& connectOpt) {
 
   UserProxy userProxy(userId, password);
@@ -169,3 +205,62 @@ int deleteLocalAccount(const std::string& sessionKey, const std::string& userId,
 
  return res;
 }
+
+int saveConfiguration(const std::string& sessionKey, const std::string filePath, UMS_Data::Configuration& config) {
+  
+   SessionProxy sessionProxy(sessionKey);
+   ConfigurationProxy configurationProxy(filePath, sessionProxy);
+
+   int res = configurationProxy.save();
+   UMS_Data::Configuration configData = configurationProxy.getData();
+   
+   //To set the file path 
+   config.setFilePath(configData.getFilePath());
+  //To set the user list 
+  for(int i = 0; i < configData.getListConfUsers().size(); i++) {
+      UMS_Data::User_ptr user = configData.getListConfUsers().get(i);
+      config.getListConfUsers().push_back(user);
+  }
+  //To set the machine list 
+  for(int i = 0; i < configData.getListConfMachines().size(); i++) {
+      UMS_Data::Machine_ptr machine = configData.getListConfMachines().get(i);
+      config.getListConfMachines().push_back(machine);
+  }
+  //To set the LocalAccounts list 
+  for(int i = 0; i < configData.getListConfLocalAccounts().size(); i++) {
+      UMS_Data::LocalAccount_ptr localAccount = configData.getListConfLocalAccounts().get(i);
+      config.getListConfLocalAccounts().push_back(localAccount);
+  }
+
+
+ //A enlever apres, c'est juste pour les tests
+    //To set the file path 
+  std::cout << "config filePath=" << config.getFilePath() << std::endl;
+  //To set the user list 
+  for(int i = 0; i < config.getListConfUsers().size(); i++) {
+      UMS_Data::User_ptr user = config.getListConfUsers().get(i);
+      print(user, i);
+  }
+  //To set the machine list 
+  for(int i = 0; i < config.getListConfMachines().size(); i++) {
+      UMS_Data::Machine_ptr machine = config.getListConfMachines().get(i);
+      print(machine, i);
+  }
+  //To set the LocalAccounts list 
+  for(int i = 0; i < config.getListConfLocalAccounts().size(); i++) {
+      UMS_Data::LocalAccount_ptr localAccount = config.getListConfLocalAccounts().get(i);
+      print(localAccount, i);
+  }
+
+  return res;
+}
+
+int saveConfiguration(const std::string& sessionKey, const UMS_Data::Configuration& config) {
+
+   SessionProxy sessionProxy(sessionKey);
+   ConfigurationProxy configurationProxy(config.getFilePath(), sessionProxy);
+
+   int  res = configurationProxy.restoreFromFile();
+ return res;
+}
+
--- UMS/src/client/ConfigurationProxy.cpp
+++ UMS/src/client/ConfigurationProxy.cpp
@@ -3,42 +3,183 @@
 #include <list>
 #include <iostream>
 #include <assert.h>
+#include <fstream>
+
+#include "UMSVishnuException.hh"
+#include "debug.hh"
 
 #include "ConfigurationProxy.h"
 
 
-void ConfigurationProxy::ConfigurationProxy(string filePath, SessionProxy session)
+ConfigurationProxy::ConfigurationProxy(const std::string& filePath, const SessionProxy& session):
+mfilePath(filePath), msessionProxy(session)
 {
 }
  
-void ConfigurationProxy::ConfigurationProxy(UMS_Data::Configuration config, SessionProxy session)
+ConfigurationProxy::ConfigurationProxy(const UMS_Data::Configuration& config, const SessionProxy& session):
+mconfiguration(config), msessionProxy(session)
 {
 }
  
 int ConfigurationProxy::save()
 {
-	return 0;
+   diet_profile_t* profile = NULL;
+   std::string sessionKey;
+   char* errorInfo;
+   char* configurationInString;
+
+   profile = diet_profile_alloc("configurationSave", 0, 0, 2);
+   sessionKey = msessionProxy.getSessionKey();
+
+   //IN Parameters
+   if(diet_string_set(diet_parameter(profile,0), strdup(sessionKey.c_str()), DIET_VOLATILE)) {
+       ERRMSG("Error in diet_string_set");
+   }
+
+   //OUT Parameters
+   if(diet_string_set(diet_parameter(profile,1), NULL, DIET_VOLATILE)) {
+      ERRMSG("Error in diet_string_set");
+   }
+   if(diet_string_set(diet_parameter(profile,2), NULL, DIET_VOLATILE)) {
+      ERRMSG("Error in diet_string_set");
+   }
+
+   if(!diet_call(profile)) {
+       if(diet_string_get(diet_parameter(profile,1), &configurationInString, NULL)){
+        ERRMSG("Error in diet_string_set");
+       };
+       if(diet_string_get(diet_parameter(profile,2), &errorInfo, NULL)){
+        ERRMSG("Error in diet_string_set");
+       };
+       if(strlen(errorInfo) > 0) std::cout << "errorInfo=" << errorInfo << std::endl;
+       else std::cout << "The service was performed successfull" << std::endl;
+   }
+   else {
+       ERRMSG("Error in diet_string_get");
+   }
+
+   if(strlen(errorInfo) > 0 ) {
+      UMSVishnuException e(1, errorInfo);
+      throw e;
+   }
+
+   // CREATE DATA MODEL
+  UMS_Data::UMS_DataPackage_ptr ecorePackage = UMS_Data::UMS_DataPackage::_instance();
+  ecorecpp::MetaModelRepository::_instance()->load(ecorePackage);
+
+  // Parse the model
+  ecorecpp::parser::parser parser;
+  UMS_Data::Configuration_ptr config_ptr = parser.load(configurationInString)->as< UMS_Data::Configuration >();
+   
+  //To set the file path 
+  mconfiguration.setFilePath(mfilePath);
+  //To set the user list 
+  for(int i = 0; i < config_ptr->getListConfUsers().size(); i++) {
+      UMS_Data::User_ptr user = config_ptr->getListConfUsers().get(i);
+      mconfiguration.getListConfUsers().push_back(user);
+  }
+  //To set the machine list 
+  for(int i = 0; i < config_ptr->getListConfMachines().size(); i++) {
+      UMS_Data::Machine_ptr machine = config_ptr->getListConfMachines().get(i);
+      mconfiguration.getListConfMachines().push_back(machine);
+  }
+  //To set the LocalAccounts list 
+  for(int i = 0; i < config_ptr->getListConfLocalAccounts().size(); i++) {
+      UMS_Data::LocalAccount_ptr localAccount = config_ptr->getListConfLocalAccounts().get(i);
+      mconfiguration.getListConfLocalAccounts().push_back(localAccount);
+  }
+
+  //To save the configuration in the file
+  std::ofstream ofile(mfilePath.c_str());
+  ofile << configurationInString;
+  ofile.close();
+   
+  return 0;
 }
  
+int ConfigurationProxy::restore(bool fromFile)
+{
+   diet_profile_t* profile = NULL;
+   std::string sessionKey;
+   char* errorInfo;
+   char* configurationInString;
+  
+   if(fromFile) {
+     size_t length;
+     std::ifstream ifile(mfilePath.c_str()); 
+ 
+     if(!ifile.is_open()) return 1;
+ 
+     ifile.seekg(0, std::ios::end);
+     length = ifile.tellg();
+     ifile.seekg(0, std::ios::beg);
+
+     configurationInString = new char[length];
+     ifile.read(configurationInString, length);
+     ifile.close();
+   }
+   else {
+     const char* name = "ConfigurationRestore";
+     ::ecorecpp::serializer::serializer _ser(name);
+     configurationInString =  strdup((_ser.serialize(&mconfiguration)).c_str());
+   }
+   profile = diet_profile_alloc("configurationRestore", 1, 1, 2);
+   sessionKey = msessionProxy.getSessionKey();
+
+   //IN Parameters
+   if(diet_string_set(diet_parameter(profile,0), strdup(sessionKey.c_str()), DIET_VOLATILE)) {
+       ERRMSG("Error in diet_string_set");
+   }
+   if(diet_string_set(diet_parameter(profile,1), configurationInString, DIET_VOLATILE)) {
+       ERRMSG("Error in diet_string_set");
+   }
+   //OUT Parameters
+   if(diet_string_set(diet_parameter(profile,2), NULL, DIET_VOLATILE)) {
+      ERRMSG("Error in diet_string_set");
+   }
+
+   if(!diet_call(profile)) {
+       if(diet_string_get(diet_parameter(profile,2), &errorInfo, NULL)){
+        ERRMSG("Error in diet_string_set");
+       };
+       if(strlen(errorInfo) > 0) std::cout << "errorInfo=" << errorInfo << std::endl;
+       else std::cout << "The service was performed successfull" << std::endl;
+   }
+   else {
+       ERRMSG("Error in diet_string_get");
+   }
+
+   if(strlen(errorInfo) > 0 ) {
+      UMSVishnuException e(1, errorInfo);
+      throw e;
+   }
+  
+  
+   if(fromFile) free(configurationInString);
+
+  return 0;
+}
+
 int ConfigurationProxy::restoreFromFile()
 {
-	return 0;
+  return restore();
 }
  
 int ConfigurationProxy::restoreFromData()
 {
-	return 0;
+  return restore(false);
 }
  
-void ConfigurationProxy::~ConfigurationProxy()
+UMS_Data::Configuration ConfigurationProxy::getData()
 {
-}
- 
-void ConfigurationProxy::getData()
-{
+ return mconfiguration;
 }
  
 SessionProxy ConfigurationProxy::getSessionProxy()
 {
-	return 0;
+  return msessionProxy;
+}
+
+ConfigurationProxy::~ConfigurationProxy()
+{
 }
