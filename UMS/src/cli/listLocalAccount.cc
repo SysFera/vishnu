5ca7d553f5110c07a4c77a04ff269f8cb764e9ac
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-02-09 19:33:04 +0100

    rajout des cli (list*)

--- UMS/src/cli/listLocalAccount.cc
+++ UMS/src/cli/listLocalAccount.cc
@@ -1,6 +1,6 @@
 
 
-#include "addMachine.hh"
+#include "listLocalAccount.hh"
 
 namespace po = boost::program_options;
 
@@ -17,27 +17,24 @@ int main (int ac, char* av[]){
 
 		string defaultConfig = "VishnuConfig.cfg";
 
-		int reqParam=0;   // to count the required parameters for the command
+
 		/******* Parsed value containers ****************/
        
 		string dietConfig;
-
-		std::string name;
-        
-		std::string site;
-
-		std::string language;
 		
-		std::string machineDescription; 
+		std::string userId;
+        
+		std::string machineId;
 		
 		std::string sessionKey;
 
-
 		/********** EMF data ************/
 
-		UMS_Data::Machine newMachine;
+		UMS_Data::ListLocalAccounts listLocalAccount;
+		
+		UMS_Data::ListLocalAccOptions listOptions;
 
-
+	
 
 
 /***************  Default configuration file ***************** */
@@ -55,7 +52,6 @@ int main (int ac, char* av[]){
 /**************** Describe options *************/
 
 
-
 		Options opt(&config );
   
 		opt.add("version,v",
@@ -67,29 +63,19 @@ int main (int ac, char* av[]){
 												ENV,
 												dietConfig);
 				
-				opt.add("name",
-		                 "The name of the machine",
-										 HIDDEN,
-										 name);
+        opt.add("adminListOption,a", 
+						            "is an admin option for listing all local configurations of all users	",
+												CONFIG);
+				opt.add("userId,u",
+		                 "the Vishnu user identifier of the user of the local user configuration",
+										 CONFIG,
+										 userId);
 
-				opt.setPosition("name",1);
-
-
-				opt.add("site",
-								"The location of the machine",
-								HIDDEN,
-								site);
-
-				opt.setPosition("site",1);
-
-				opt.add("language",
-												"The language in which the description of the machine has been done",
-												HIDDEN,
-												language);
-
-				opt.setPosition("language",1);
-
-
+				opt.add("machineId,i",
+									    	"the identifier of the machine associated to the local user configuration",
+												CONFIG,
+												machineId);
+										
 				opt.add("sessionKey",
 												"The session key",
 												ENV,
@@ -98,6 +84,8 @@ int main (int ac, char* av[]){
 /**************  Parse to retrieve option values  ********************/
  
 		opt.parse_cli(ac,av);
+
+		bool isEmpty=opt.empty();//if no value was given in the command line
 
 		opt.parse_cfile();
 		
@@ -111,35 +99,34 @@ int main (int ac, char* av[]){
 
 /********  Process **************************/
 
-		if (opt.count("name")){
+
+		if (opt.count("adminListOption")){
 			
-			cout <<"The name of the machine is " << name << endl;
+			cout <<"It is an admin list option " << endl;
 			
-			newMachine.setName(name);
+			listOptions.setAdminListOption(true);
+		}
 			
-			reqParam=reqParam+1;
+		
+		if (opt.count("userId")){
+			
+			cout <<"The user identifier is " << userId << endl;
+			
+			listOptions.setUserId(userId);
 			
 		}
 		
-		if(opt.count("site")){
+		if(opt.count("machineId")){
 			
-			cout <<"the site is : " << site << endl;
+			cout <<"the machineId is : " << machineId << endl;
 			
-			newMachine.setSite(site);
-			
-			reqParam=reqParam+1;
+			listOptions.setMachineId(machineId);
 		}
 		
-		if(opt.count("language")){
-
-			cout << "The language is " << language << endl;
+		if(opt.count("sessionKey")){
 			
-			newMachine.setLanguage(language);
-			
-			reqParam=reqParam+1;
-                        
+			cout <<"the session key is : " << sessionKey << endl;
 		}
-
             
 		if (opt.count("dietConfig")){
            
@@ -153,22 +140,13 @@ int main (int ac, char* av[]){
 		}
 
 
-		if ((reqParam < AMPARAM) || (opt.count("help"))){
+		if ( isEmpty ||(opt.count("help"))){
 			
-			cout << "Usage: " << av[0] <<" name site language "<<endl;
-				     
+			cout << "Usage: " << av[0] <<" [options]  "<<endl;
+			
 			cout << opt << endl;
 							      
 			return 0;
-		}
-		else{//Fix me
-
-		cout << "Enter the Machine Description:\n ";
-
-		getline(cin, machineDescription);
-			
-		newMachine.setMachineDescription(machineDescription);
-
 		}
 
 
@@ -176,6 +154,7 @@ int main (int ac, char* av[]){
 
 /************** Call UMS connect service *******************************/
 
+		/*
                // initializing DIET
 							 
 							  if (diet_initialize(dietConfig.c_str(), ac, av)) {
@@ -185,8 +164,11 @@ int main (int ac, char* av[]){
 
     
 		
-							int res = addMachine(sessionKey,newMachine);
+							int res = listLocalAccount(sessionKey,listLocalAccount,listOptions);
 
+
+							// Display the list
+	*/
 
 	}// End of try bloc
 
