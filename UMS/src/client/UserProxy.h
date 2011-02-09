9cb58c12eb98d342a57407a885b0227783d03349
Daouda Traore <daouda.traore@sysfera.com>
2011-02-09 19:48:38 +0100

    ajout des lists dans client et api

--- UMS/src/client/UserProxy.h
+++ UMS/src/client/UserProxy.h
@@ -17,29 +17,29 @@ class UserProxy
 {
 
 public:
-        //Constructors	
-        UserProxy(const std::string& userId, const std::string& password);
-	explicit UserProxy(SessionProxy session);
-	explicit UserProxy(const UMS_Data::User& user);
+  //Constructors	
+  UserProxy(const std::string& userId, const std::string& password);
+  explicit UserProxy(SessionProxy session);
+  explicit UserProxy(const UMS_Data::User& user);
 
-        //methods	
-        int add(const UMS_Data::User& user);
-	int update(const UMS_Data::User& user);
-	int deleteUser(const UMS_Data::User& user);
-	int changePassword(const std::string& password, const std::string& newPassword);
-	int resetPassword(UMS_Data::User& user);
-	UMS_Data::User getData() const;
-        SessionProxy getSessionProxy() const;
+  //methods	
+  int add(const UMS_Data::User& user);
+  int update(const UMS_Data::User& user);
+  int deleteUser(const UMS_Data::User& user);
+  int changePassword(const std::string& password, const std::string& newPassword);
+  int resetPassword(UMS_Data::User& user);
+  UMS_Data::User getData() const;
+  SessionProxy getSessionProxy() const;
 
-        //Destructor	
-        ~UserProxy();
+  //Destructor	
+  ~UserProxy();
 
 private:
 
-        int _addUserInformation(const UMS_Data::User& user, bool isNewUser=true);
+  int _addUserInformation(const UMS_Data::User& user, bool isNewUser=true);
 
-        UMS_Data::User muser;
-        SessionProxy* msessionProxy;
+  UMS_Data::User muser;
+  SessionProxy* msessionProxy;
 
 };
 #endif
