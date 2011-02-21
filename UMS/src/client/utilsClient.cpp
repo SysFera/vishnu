faaeb259f6ca2c56ffadddffa9fdbda132940fff
Daouda Traore <daouda.traore@sysfera.com>
2011-02-21 09:46:18 +0100

    ajout des commentaires dans la partie cliente

--- UMS/src/client/utilsClient.cpp
+++ UMS/src/client/utilsClient.cpp
@@ -1,12 +1,27 @@
+/**
+ * \file utilsClient.hpp
+ * \brief This file contains client utils functions for VISHNU client Proxy classes 
+ * \authors Daouda Traore (daouda.traore@sysfera.com)
+ */
 #include "utilsClient.hpp"
 
-void sendErrorMsg(std::string msg) {
+/**
+ * \brief Function to spread error message to C++ API, Python API, Web service API and command line program
+ * \fn void sendErrorMsg(const std::string& msg) 
+ * \param msg to spread 
+ */
+void sendErrorMsg(const std::string& msg) {
     std::string msgWithHead = "Error: "+msg;
     UMSVishnuException e(1, msgWithHead);
     throw e;
 }
 
-void checkErrorMsg(std::string msg) {
+/**
+ * \brief Function to split the receiving message into error code and message error 
+ * \fn void checkErrorMsg(const std::string& msg) 
+ * \param msg to split 
+ */
+void checkErrorMsg(const std::string& msg) {
 
   if(msg.size() > 0 ) {
     char* codeInString = strtok(strdup(msg.c_str()), "#");
