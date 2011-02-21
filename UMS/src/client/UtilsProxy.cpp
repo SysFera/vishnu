fbf49f06e3fdd781f075ec3f7a8de7f2ad4fcc81
kcoulomb <kevin.coulomb@gmail.com>
2011-02-21 12:42:05 +0100

    Add restore function

--- UMS/src/client/UtilsProxy.cpp
+++ UMS/src/client/UtilsProxy.cpp
@@ -8,6 +8,7 @@
 #include <list>
 #include <iostream>
 #include <assert.h>
+#include <fstream>
 
 #include "UMSVishnuException.hh"
 #include "UtilsProxy.hpp"
@@ -75,15 +76,26 @@ UtilsProxy::finalize() {
  */
 int 
 UtilsProxy::restore() {
-
-   std::ifstream file(mfilePath.c_str(), std::ios::in);
-   if(!file) {
-     std::cerr << "Failed to open sql script file" << std::endl;
-     return -1;
-   }
-   
-   
-   
+  int READSIZE = 1000;
+  char tmp[READSIZE];
+  diet_profile_t* profile = NULL;
+  profile = diet_profile_alloc("restore", 0, 0, 0);
+  std::ifstream file(mfilePath.c_str(), std::ios::in);
+  if(!file) {
+    std::cerr << "Failed to open sql script file" << std::endl;
+    return -1;
+  }
+  // While all has not been read
+  while (-1 != file.tellg()){
+    file.getline(tmp, READSIZE);
+    char          tmp[READSIZE];   
+    //IN Parameters
+    diet_string_set(diet_parameter(profile,0), tmp, DIET_VOLATILE);
+    if(!diet_call(profile)){
+      std::cerr << "Failed to restore database " << std::endl;
+    }    
+  }
+  return 0;
 }
 
 /**
