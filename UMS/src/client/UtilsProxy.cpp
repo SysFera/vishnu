b164f725c47e61f32163f943d3a722a6597d937f
Daouda Traore <daouda.traore@sysfera.com>
2011-02-21 12:31:43 +0100

    ajout restore

---
+++ UMS/src/client/UtilsProxy.cpp
@@ -0,0 +1,96 @@
+/**
+ * \file UtilsProxy.cpp
+ * \brief This file contains the VISHNU UtilsProxy class.
+ * \authors Daouda Traore (daouda.traore@sysfera.com)
+ */
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <assert.h>
+
+#include "UMSVishnuException.hh"
+#include "UtilsProxy.hpp"
+#include "utilsClient.hpp"
+
+/**
+ * \fn UtilsProxy(char* cfg,
+ *                int argc,
+                  char** argv) 
+ * \param cfg The SYSFERA-DS configuration file 
+ * \param argc The number of arguments of the program
+ * \briefargv The list of arguments 
+ */
+UtilsProxy::UtilsProxy()
+{
+  mcfg  = NULL;
+  margc = NULL;
+  margv = NULL;
+}
+
+/**
+ * \fn UtilsProxy(char* cfg,
+ *                int argc,
+                  char** argv) 
+ * \param cfg The SYSFERA-DS configuration file 
+ * \param argc The number of arguments of the program
+ * \briefargv The list of arguments 
+ */
+UtilsProxy::UtilsProxy(char* cfg, int argc, char** argv):
+mcfg(cfg), margc(argc), margv(argv)
+{
+}
+
+UtilsProxy::UtilsProxy(const std::string& filePath):
+mfilePath(filePath)
+{
+}
+
+/**
+ * \brief Function to initialize the SYSFERA-DS configuration 
+ * \fn  int initilialize()
+ * \return 0 if success else -1 if error
+ */
+int 
+UtilsProxy::initilialize() {
+  if (diet_initialize(mcfg, margc, margv)) {
+     return 1;
+   }
+ return 0;
+}
+
+/**
+ * \brief Function to finalize 
+ * \fn  void finalize()
+ * \return raises an exception on error
+ */
+int
+UtilsProxy::finalize() {
+ diet_finalize();
+}
+/**
+ * \brief Function to initialize the database 
+ * \fn  void restore()
+ * \return raises an exception on error
+ */
+int 
+UtilsProxy::restore() {
+
+   std::ifstream file(mfilePath.c_str(), std::ios::in);
+   if(!file) {
+     std::cerr << "Failed to open sql script file" << std::endl;
+     return -1;
+   }
+   
+   
+   
+}
+
+/**
+ * \fn ~UtilsProxy()
+ * \brief Destructor, raises an exception on error
+ */ 
+UtilsProxy::~UtilsProxy()
+{
+}
+ 
