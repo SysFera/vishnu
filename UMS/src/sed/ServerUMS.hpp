26fcffcb70b2d1f33246ec170cf59be5d3c6ce2f
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-02-22 19:14:01 +0100

    maj du serveur pour acces serveur Oracle

--- UMS/src/sed/ServerUMS.hpp
+++ UMS/src/sed/ServerUMS.hpp
@@ -2,7 +2,7 @@
  * \file ServerUMS.hh
  * \brief This file presents the implementation of the UMS server.
  * \author Eugène PAMBA CAPO-CHICHI (eugene.capochichi@sysfera.com)
- * \date 31/01/2001 
+ * \date 31/01/2001
 */
 
 #ifndef _SERVERUMS_H_
@@ -11,10 +11,10 @@
 #include <string>
 #include "internalApi.hpp"
 #define NB_SRV 25
-  
+
 static const char* SRV[NB_SRV] = {
-  "sessionConnect", 
-  "sessionReconnect", 
+  "sessionConnect",
+  "sessionReconnect",
   "sessionClose",
   "userCreate",
   "userUpdate",
@@ -31,7 +31,7 @@ static const char* SRV[NB_SRV] = {
   "configurationRestore",
   "optionValueSet",
   "optionValueSetDefault",
-  "sessionList", 
+  "sessionList",
   "localAccountList",
   "machineList",
   "commandList",
@@ -44,39 +44,46 @@ static const char* SRV[NB_SRV] = {
  */
 class ServerUMS {
 public :
-   /**
+  /**
    * \brief To get the path of the configuration file used by the UMS server
    * \fn std::string getCfg()
    * \return the path of the configuration file
    */
-   std::string
-   getCfg();  
+  std::string
+  getCfg();
+
    /**
-   * \brief To initialize the UMS server
-   * \fn int init(std::string vishnuid)
-   * \param vishnuid The password of the root user vishnu_user for the connection with the database
-   * \return an error code (0 if success and 1 if an error occurs) 
-   */ 
-   int
-   init(std::string vishnuid, std::string password);
+   * \brief To initialize the UMS server with individual parameters instead of configuration file
+   * \fn int init(int vishnuId, int dbType, std::string dbHost, std::string dbUsername, std::string dbPassword)
+   * \param vishnuId The password of the root user vishnu_user for the connection with the database
+   * \param dbType   The type of the database (POSTGREDB|ORACLEDB)
+   * \param dbHost   The host of the database server
+   * \param dbUsername The name of the database user on the server
+   * \param dbPassword The password of the database user on the server
+   * \return an error code (0 if success and 1 if an error occurs)
+   */
+  int
+  init(int vishnuId, int dbType, std::string dbHost, std::string dbUsername, std::string dbPassword);
+
   /**
-  * \brief Constructor, raises an exception on error
-  * \fn ServerUMS(std::string cfg)
-  * \param cfg The vishnu configuration filepath
-  */  
-  ServerUMS(std::string cfg);
+   * \brief Constructor, raises an exception on error
+   * \fn ServerUMS(std::string cfg)
+   * \param cfg The vishnu configuration filepath
+   */
+  ServerUMS(std::string cfg="");
+
   /**
    * \brief Destructor, raises an exception on error
    * \fn ~ServerUMS()
    */
   ~ServerUMS();
-  
+
 private :
-  
+
   /////////////////////////////////
   // Attributes
   /////////////////////////////////
-  
+
   /**
   * \brief The VISHNU configuration file path
   */
