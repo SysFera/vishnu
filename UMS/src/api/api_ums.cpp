e83fba165de87ba4faaf156185532be218599f28
Daouda Traore <daouda.traore@sysfera.com>
2011-02-22 10:58:15 +0100

    maj de la fonction restore

--- UMS/src/api/api_ums.cpp
+++ UMS/src/api/api_ums.cpp
@@ -727,6 +727,13 @@ vishnuFinalize() {
  */
 int
 restore(const std::string& filePath) {
+
+  int argc = 2;
+  char* argv[argc];
+  argv[0] = (char*)"./automTest";
+  argv[1] = getenv("VISHNU_CONFIG_FILE");
+  std::cout << argv[0] << " " << argv[1] << std::endl;
+  UtilsProxy utilsProxy(filePath, argv[1],  argc, argv) ;
   
-  return UtilsProxy(filePath).restore();
+  return utilsProxy.restore();
 }
