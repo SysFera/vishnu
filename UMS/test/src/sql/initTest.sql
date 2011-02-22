4ef69e74a93adea1b7f9d1fac031550b26c220b4
kcoulomb <kevin.coulomb@gmail.com>
2011-02-22 15:07:26 +0100

    En minuscule pour nom colone

--- UMS/test/src/sql/initTest.sql
+++ UMS/test/src/sql/initTest.sql
@@ -1,4 +1,4 @@
-INSERT INTO vishnu("updatefreq", "formatiduser", "formatidjob", "formatidfiletransfer", "formatidmachine", "usercpt", "jobcpt", "fileSubcpt", "machinecpt") values('100', 'user_$CPT', 'job_$CPT', 'transfer_$CPT', 'machine_$CPT', '2', '2', '2', '2');
+INSERT INTO vishnu("updatefreq", "formatiduser", "formatidjob", "formatidfiletransfer", "formatidmachine", "usercpt", "jobcpt", "filesubcpt", "machinecpt") values('100', 'user_$CPT', 'job_$CPT', 'transfer_$CPT', 'machine_$CPT', '2', '2', '2', '2');
 
 INSERT INTO users("userid", "pwd", "firstname", "lastname", email, "passwordstate", "vishnu_vishnuid") values('admin_1', 'admin', 'jean', 'dupont', 'admin@admin.com', 'A', '1', '1');
 INSERT INTO users("userid", "pwd", "firstname", "lastname", email, "passwordstate", "vishnu_vishnuid") values('user_1', 'user', 'jeannot', 'dupont', 'user@user.com', 'U', '1', '1');
