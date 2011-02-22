e32307c823023c9d5a40d9cbcba0e0ccf78ba6a1
kcoulomb <kevin.coulomb@gmail.com>
2011-02-22 16:04:41 +0100

    update script

--- UMS/test/src/sql/initTest.sql
+++ UMS/test/src/sql/initTest.sql
@@ -1,5 +1,5 @@
 INSERT INTO vishnu("updatefreq", "formatiduser", "formatidjob", "formatidfiletransfer", "formatidmachine", "usercpt", "jobcpt", "filesubcpt", "machinecpt") values('100', 'user_$CPT', 'job_$CPT', 'transfer_$CPT', 'machine_$CPT', '2', '2', '2', '2');
-INSERT INTO users("userid", "pwd", "firstname", "lastname", "privilege", "email", "passwordstate", "vishnu_vishnuid") values('admin_1', 'admin', 'jean', 'dupont', 'A', 'admin@admin.com', '1', '1');
-INSERT INTO users("userid", "pwd", "firstname", "lastname", "privilege", "email", "passwordstate", "vishnu_vishnuid") values('user_1', 'user', 'jeannot', 'dupont', 'U', 'user@user.com', '1', '1');
+INSERT INTO users("userid", "pwd", "firstname", "lastname", "privilege", "email", "passwordstate", "vishnu_vishnuid") values('admin_1', 'admin', 'jean', 'dupont', '1', 'admin@admin.com', '1', '1');
+INSERT INTO users("userid", "pwd", "firstname", "lastname", "privilege", "email", "passwordstate", "vishnu_vishnuid") values('user_1', 'user', 'jeannot', 'dupont', '0', 'user@user.com', '1', '1');
 INSERT INTO machine("vishnu_vishnuid", "name", "site", "machineid")values('1', 'tiger', 'berlin', 'machine_1');
 
