e281ed09d7706b267d4a67a6d5f9e6467ad8e261
kcoulomb <kevin.coulomb@gmail.com>
2011-02-23 09:51:11 +0100

    Add code and finalize function to mapper

--- core/src/registry/Mapper_impl.hpp
+++ core/src/registry/Mapper_impl.hpp
@@ -56,6 +56,34 @@ public :
   virtual int
   getKey(const string& command, string& key);
 
+  /**
+   * \brief Function to add an element to code
+   * \fn  virtual int code(const string& cmd, unsigned int code = 0);
+   * \param cmd The string to add
+   * \param code If 0, the code is created, otherwize it is the key in the map to add the string
+   * \return if param code = 0, the assigned code to add other element to the same item, otherwize return 0
+   */
+  virtual int
+  code(const string& cmd, unsigned int code = 0);
+
+  /**
+   * \brief To end a command, delete it from the map and get its value
+   * \fn virtual string finalize(int key);
+   * \param The entry in the map to get an remove
+   * \return The coded value of the command
+   */
+  virtual string
+  finalize(int key);
+
+  /**
+   * \brief To get, from a coded string, the CPP command that made it
+   * \fn virtual string decodeCPP (const string& msg) ;
+   * \param msg The coded string
+   * \return The CPP command
+   */
+  virtual string
+  decodeCPP (const string& msg);
+
 protected:
 private:
 };
