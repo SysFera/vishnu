c953fccaeb8fa398aefa72cca596352edbc984bf
kcoulomb <kevin.coulomb@gmail.com>
2011-02-25 11:11:57 +0100

    Remove example because don't compile on graal

--- core/src/exception/IMSVishnuException.cpp
+++ core/src/exception/IMSVishnuException.cpp
@@ -1,13 +1,13 @@
 #include "IMSVishnuException.hpp"
 
-IMSVishnuException::IMSVishnuException(const IMSVishnuException& e):SystemException(e){
+IMSVishnuException::IMSVishnuException(const IMSVishnuException& e):UserException(e){
   mtype = VishnuException::IMS;
 }
 
-IMSVishnuException::IMSVishnuException(int msg, std::string msgComp):SystemException(msg, msgComp){
+IMSVishnuException::IMSVishnuException(int msg, std::string msgComp):UserException(msg, msgComp){
   mtype = VishnuException::IMS;
 }
 
-IMSVishnuException::IMSVishnuException():SystemException(){
+IMSVishnuException::IMSVishnuException():UserException(){
   mtype = VishnuException::IMS;
 }
