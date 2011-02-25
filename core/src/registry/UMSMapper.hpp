dde2e77e252a3527daa73ba095c89dd6ef834afb
kcoulomb <kevin.coulomb@gmail.com>
2011-02-25 17:51:08 +0100

    Add documentation for private function

--- core/src/registry/UMSMapper.hpp
+++ core/src/registry/UMSMapper.hpp
@@ -17,29 +17,101 @@
 
 using namespace std;
 
+/**
+ * \brief Connection key
+ */
 const int VISHNU_CONNECT                  = 1 ;
+/**
+ * \brief Reconnection key
+ */
 const int VISHNU_RECONNECT 	     	  = 2 ;
+/**
+ * \brief Closure key
+ */
 const int VISHNU_CLOSE 	     	          = 3 ;
+/**
+ * \brief Add user key
+ */
 const int VISHNU_ADD_VISHNU_USER   	  = 4 ;
+/**
+ * \brief Update user key
+ */
 const int VISHNU_UPDATE_VISHNU_USER 	  = 5 ;
+/**
+ * \brief Delete user key
+ */
 const int VISHNU_DELETE_VISHNU_USER 	  = 6 ;
+/**
+ * \brief Change password key
+ */
 const int VISHNU_CHANGE_PASSWORD   	  = 7 ;
+/**
+ * \brief Reset password key
+ */
 const int VISHNU_RESET_PASSWORD    	  = 8 ;
+/**
+ * \brief Add account key
+ */
 const int VISHNU_ADD_LOCAL_ACCOUNT        = 9 ;
+/**
+ * \brief Update account key
+ */
 const int VISHNU_UPDATE_LOCAL_ACCOUNT     = 10;
+/**
+ * \brief Delete account key
+ */
 const int VISHNU_DELETE_LOCAL_ACCOUNT     = 11;
+/**
+ * \brief Save configuration key
+ */
 const int VISHNU_SAVE_CONFIGURATION       = 12;
+/**
+ * \brief Restore configuration key
+ */
 const int VISHNU_RESTORE_CONFIGURATION    = 13;
+/**
+ * \brief Add machine key
+ */
 const int VISHNU_ADD_MACHINE 	          = 14;
+/**
+ * \brief Update machine key
+ */
 const int VISHNU_UPDATE_MACHINE           = 15;
+/**
+ * \brief Delete machine key
+ */
 const int VISHNU_DELETE_MACHINE           = 16;
+/**
+ * \brief List account key
+ */
 const int VISHNU_LIST_LOCAL_ACCOUNT       = 17;
+/**
+ * \brief List machine key
+ */
 const int VISHNU_LIST_MACHINE 	          = 18;
+/**
+ * \brief List history key
+ */
 const int VISHNU_LIST_HISTORY_CMD         = 19;
+/**
+ * \brief List option key
+ */
 const int VISHNU_LIST_OPTIONS 	          = 20;
+/**
+ * \brief List user key
+ */
 const int VISHNU_LIST_USERS 	          = 21;
+/**
+ * \brief List session key
+ */
 const int VISHNU_LIST_SESSIONS 	          = 22;
+/**
+ * \brief Configure default option key
+ */
 const int VISHNU_CONFIGURE_DEFAULT_OPTION = 23;
+/**
+ * \brief Configure option key
+ */
 const int VISHNU_CONFIGURE_OPTION	  = 24;
 
 
@@ -49,8 +121,22 @@ const int VISHNU_CONFIGURE_OPTION	  = 24;
  */
 class UMSMapper : Mapper{
 public :
+  /**
+   * \brief Constructor
+   * \fn UMSMapper(MapperRegistry* reg, string name)
+   * \param reg Registry to be linked to
+   * \param name Name of the mapper
+   */
   UMSMapper(MapperRegistry* reg, string name);
+  /**
+   * \brief Default constructor
+   * \fn UMSMapper()
+   */
   UMSMapper();
+  /**
+   * \brief Destructor
+   * \fn ~UMSMapper()
+   */
   ~UMSMapper();
   /**
    * \brief To register a mapper in the list of active mapper
@@ -116,76 +202,242 @@ public :
 protected:
 private:
 
+  /**
+   * \brief To decode the close call sequence of the string returned by finalize
+   * \fn string decodeClose(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like close command
+   */
+  string
+  decodeClose(vector<int> separator, const string& msg);
 
-string
-decodeClose(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeAddUser(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like add user command
+   */
+  string
+  decodeAddUser(vector<int> separator, const string& msg);
 
-string
-decodeAddUser(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeUpUser(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like update user command
+   */
+  string
+  decodeUpUser(vector<int> separator, const string& msg);
 
-string
-decodeUpUser(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeDelUser(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like delete user command
+   */
+  string
+  decodeDelUser(vector<int> separator, const string& msg);
 
-string
-decodeDelUser(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeChangePwd(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like change password command
+   */
+  string
+  decodeChangePwd(vector<int> separator, const string& msg);
 
-string
-decodeChangePwd(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeResetPwd(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like reset password command
+   */
+  string
+  decodeResetPwd(vector<int> separator, const string& msg);
 
-string
-decodeResetPwd(vector<int> separator, const string& msg);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeAddAcc(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like add account command
+   */
+  string
+  decodeAddAcc(vector<int> separator, const string& msg);
 
-string
-decodeAddAcc(vector<int> separator, const string& msg);
-
-string
-decodeUpAcc(vector<int> separator, const string& msg);
-
-string
-decodeDelAcc(vector<int> separator, const string& msg);
-
-string
-decodeSaveConf(vector<int> separator, const string& msg);
-
-string
-decodeRestoreConf(vector<int> separator, const string& msg);
-
-string
-decodeAddM(vector<int> separator, const string& msg);
-
-string
-decodeUpM(vector<int> separator, const string& msg);
-
-string
-decodeDelM(vector<int> separator, const string& msg);
-
-string
-decodeListAcc(vector<int> separator, const string& msg);
-
-string
-decodeListM(vector<int> separator, const string& msg);
-
-string
-decodeListH(vector<int> separator, const string& msg);
-
-string
-decodeListOp(vector<int> separator, const string& msg);
-
-string
-decodeListUser(vector<int> separator, const string& msg);
-
-string
-decodeListSession(vector<int> separator, const string& msg);
-
-string
-decodeConfDefaultOp(vector<int> separator, const string& msg);
-
-string
-decodeConfOp(vector<int> separator, const string& msg);
-
-  void
-findSeparator(const string& s, vector<int>& vec);
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeUpAcc(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like update account command
+   */
+  string
+  decodeUpAcc(vector<int> separator, const string& msg);
   
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeDelAcc(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like delete account command
+   */
+  string
+  decodeDelAcc(vector<int> separator, const string& msg);
+  
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeSaveConf(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like save configuration command
+   */
+  string
+  decodeSaveConf(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeRestoreConf(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like crestore configuration command
+   */
+  string
+  decodeRestoreConf(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeAddM(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like add machine command
+   */
+  string
+  decodeAddM(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeUpM(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like update machine command
+   */
+  string
+  decodeUpM(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeDelM(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like delete machine command
+   */
+  string
+  decodeDelM(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListAcc(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list account command
+   */
+  string
+  decodeListAcc(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListM(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list machine command
+   */
+  string
+  decodeListM(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListH(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list history command
+   */
+  string
+  decodeListH(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListOp(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list option command
+   */
+  string
+  decodeListOp(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListUser(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list user command
+   */
+  string
+  decodeListUser(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeListSession(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like list session command
+   */
+  string
+  decodeListSession(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeConfDefaultOp(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like configure default option command
+   */
+  string
+  decodeConfDefaultOp(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To decode the add user call sequence of the string returned by finalize
+   * \fn string decodeConfOp(vector<int> separator, const string& msg)
+   * \param separator A vector containing the position of the separator in the message msg
+   * \param msg The message to decode
+   * \return The cli like configure option command
+   */
+  string
+  decodeConfOp(vector<int> separator, const string& msg);
+
+  /**
+   * \brief To find all the separator in the string s and fill the vector with their position in the string
+   * \fn void findSeparator(const string& s, vector<int>& vec)
+   * \param s The string to find the separator
+   * \param separator A vector containing the position of the separator in the string s (OUT)
+   * \return The cli like close command
+   */
+  void
+  findSeparator(const string& s, vector<int>& vec);
+  
+  /**
+   * \brief To deserialize the serial string, make a user objet, and get the values in the same string
+   * \fn string getU(string serial)
+   * \param serial The serialized object
+   * \return A string containing the user fields in the right order
+   */
   string
   getU(string serial);
 
