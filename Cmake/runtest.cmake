d5ac4068156d37966bd3f9a1e2e0c2fac3302401
kcoulomb <kevin.coulomb@gmail.com>
2011-02-25 15:28:15 +0100

    Test for hudson

--- Cmake/runtest.cmake
+++ Cmake/runtest.cmake
@@ -2,6 +2,15 @@
 
 # don't use $ENV{XXX} as in documentation ==> does not work
 set( ENV{BOOST_TEST_CATCH_SYSTEM_ERRORS} "no" )
+set (ENV{VISHNU_CONFIG_FILE} "${VISHNU_CONFIG}")
+set (ENV{OMNIORB4_DIR} "/usr/local")
+set (ENV{OMNIORB4_INCLUDE_DIR} "$ENV{OMNIORB4_DIR}/include")
+set (ENV{OMNIORB_CONFIG} "$ENV{OMNIORB4_DIR}/etc/omniORB4.cfg")
+set (ENV{OMNINAMES_LOGDIR} "/tmp")
+set (ENV{PATH} "$ENV{OMNIORB4_DIR}/bin:$PATH")
+
+execute_process( COMMAND export )
+
 execute_process( COMMAND ${BIN_PATH}/${TEST_PROG}
   --report_format=xml
   --report_level=detailed
