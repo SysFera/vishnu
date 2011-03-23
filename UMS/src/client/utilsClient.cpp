799569d59d05c81dd931efaebf62fdaf483e514d
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-03-23 15:10:04 +0100

    fix pb with exception factory

--- UMS/src/client/utilsClient.cpp
+++ UMS/src/client/utilsClient.cpp
@@ -1,6 +1,6 @@
 /**
  * \file utilsClient.hpp
- * \brief This file contains client utils functions for VISHNU client Proxy classes 
+ * \brief This file contains client utils functions for VISHNU client Proxy classes
  * \author Daouda Traore (daouda.traore@sysfera.com)
  * \date February 2011
  */
@@ -8,8 +8,8 @@
 
 /**
  * \brief Function to spread error message to C++ API, Python API, Web service API and command line program
- * \fn void raiseDietMsgException(const std::string& msg) 
- * \param msg to spread 
+ * \fn void raiseDietMsgException(const std::string& msg)
+ * \param msg to spread
  * \return raises an exception on error
  */
 void raiseDietMsgException(const std::string& msg) {
@@ -18,10 +18,10 @@ void raiseDietMsgException(const std::string& msg) {
 }
 
 /**
- * \brief Function to split the receiving message into error code and message error 
- * \fn void raiseExceptionIfNotEmptyMsg(const std::string& msg) 
+ * \brief Function to split the receiving message into error code and message error
+ * \fn void raiseExceptionIfNotEmptyMsg(const std::string& msg)
  * \param msg to split
- * \return raises an exception on error 
+ * \return raises an exception on error
  */
 void raiseExceptionIfNotEmptyMsg(const std::string& msg) {
 
@@ -31,12 +31,20 @@ void raiseExceptionIfNotEmptyMsg(const std::string& msg) {
       std::string codeInString = msg.substr(0,pos);
       if(codeInString.size()!=0) {
         std::istringstream isCode(codeInString);
-        int ret;
-        isCode >> ret;
+        int code;
+        isCode >> code;
         std::string message = msg.substr(pos+1);
-	ExceptionFactory fact = ExceptionFactory();
-	throw fact.create(ret, message);
-	//        throw UMSVishnuException(ret, message);
+        if (code<10){
+          throw SystemException(code, message);
+        } else if (code<100){
+          throw UMSVishnuException(code, message);
+        } else {
+          std::string tmp = "Invalid code: ";
+          tmp += vishnu::convertToString(code);
+          tmp += " with message: ";
+          tmp += message;
+          throw SystemException(ERRCODE_INVEXCEP, tmp);
+        }
       }
     } else {
       throw UMSVishnuException(-1, msg);
@@ -46,11 +54,11 @@ void raiseExceptionIfNotEmptyMsg(const std::string& msg) {
 }
 
 /**
- * \brief Function to check if a text is empty 
+ * \brief Function to check if a text is empty
  * \param text The text to check
  * \param comMsg The complementary message to print
  * \param execpetionType The type of exception to raise
- * \return raises an exception on error 
+ * \return raises an exception on error
  */
 void checkIfTextIsEmpty(const std::string& text, const std::string& compMsg, const int& execpetionType) {
 
@@ -58,12 +66,12 @@ void checkIfTextIsEmpty(const std::string& text, const std::string& compMsg, con
     throw UMSVishnuException(execpetionType, compMsg);
   }
 
-} 
+}
 
 /**
- * \brief Function to check if an email is valid  
+ * \brief Function to check if an email is valid
  * \param mail The email to check
- * \return raises an exception on error 
+ * \return raises an exception on error
  */
 void checkEmail(const std::string& mail) {
 
