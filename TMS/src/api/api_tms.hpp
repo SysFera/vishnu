5bbe8a7b1b3bfbd8b19c3a27195293f269009a84
Paco <caplachiche@yahoo.fr>
2011-04-05 11:44:21 +0200

    add getJobProgress + MAJ commentaire machineId

--- TMS/src/api/api_tms.hpp
+++ TMS/src/api/api_tms.hpp
@@ -50,7 +50,7 @@ namespace vishnu {
   * \brief The cancelJob function cancels a job from its id
   * \fn int cancelJob(const std::string& sessionKey, const std::string& machineId, const std::string& jobId, std::string& infoMsg)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param jobId : The Id of the job
   * \param infoMsg : The information message
   * \return int : an error code
@@ -66,7 +66,7 @@ namespace vishnu {
   * \brief The getJobInfo function gets information on a job from its id
   * \fn int getJobInfo(const std::string& sessionKey, const std::string& machineId, const std::string& jobId, Job& jobInfos)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param jobId : The id of the job
   * \param jobInfos : The resulting information on the job
   * \return int : an error code
@@ -82,7 +82,7 @@ namespace vishnu {
   * \brief The listJobs function gets a list of all submitted jobs
   * \fn int listJobs(const std::string& sessionKey, const std::string& machineId, ListJobs& listOfJobs, const ListJobsOptions& options)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param listOfJobs : The constructed object list of jobs
   * \param options : Additional options for jobs listing
   * \return int : an error code
@@ -99,14 +99,14 @@ namespace vishnu {
   * \fn int getJobProgress(const std::string& sessionKey, const std::string& machineId, Progression& progress, const ProgressOptions& options)
   * \param sessionKey : The session key
   * \param machineId : Is the id of the machine to get the jobs progression.
-  * \param progress : Is the object containing jobs progression information
+  * \param listOfProgress : Is the object containing jobs progression information
   * \param options : Is an object containing the available options jobs for progression .
   * \return int : an error code
   */
   int
   getJobProgress(const std::string& sessionKey,
                  const std::string& machineId,
-                 Progression& progress,
+                 ListProgression& listOfProgress,
                  const ProgressOptions& options)
   throw(UserException, SystemException);
 
@@ -114,7 +114,7 @@ namespace vishnu {
   * \brief The listQueues function gets queues information
   * \fn int listQueues(const std::string& sessionKey, const std::string& machineId, ListQueues& listofQueues)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param listofQueues : The list of queues
   * \return int : an error code
   */
@@ -128,7 +128,7 @@ namespace vishnu {
   * \brief The getJobOutput function gets outputPath and errorPath of a job from its id
   * \fn int getJobOutput(const std::string& sessionKey, const std::string& machineId, const std::string& jobId, JobResult& outputInfo, std::string& infoMsg)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param jobId : The Id of the job
   * \param outputInfo : The  Job object  containing the job output information (ex: outputPath and errorPath) of the job to submit
   * \param infoMsg : The information message
@@ -146,7 +146,7 @@ namespace vishnu {
   * \brief The getAllJobsOutput function dynamically gets outputPath and errorPath of completed jobs
   * \fn int getAllJobsOutput(const std::string& sessionKey, const std::string& machineId, ListJobResults& listOfResults)
   * \param sessionKey : The session key
-  * \param machineId : Machine hash key
+  * \param machineId : The id of the machine
   * \param listOfResults : Is the list of jobs results
   * \return int : an error code
   */
