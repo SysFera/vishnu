6b53667c10c8ba64f7cda788ccdcb59281131cc0
Daouda Traore <daouda.traore@sysfera.com>
2011-04-07 09:53:39 +0200

    maj de TorqueServer.cpp

--- TMS/src/server/TorqueServer.cpp
+++ TMS/src/server/TorqueServer.cpp
@@ -19,16 +19,25 @@ int TorqueServer::submit(const char* scriptPath, const TMS_Data::SubmitOptions& 
   if(options.getQueue().size()!=0) {
      destination = strdup(options.getQueue().c_str());  
   }
-  /*if(options.getOutputPath().size()!=0) {
-    set_attr(&attrib, ATTR_e, Job.getOutputath());
+  if(options.getOutputPath().size()!=0) {
+    set_attr(&attrib, ATTR_o, strdup(options.getOutputPath().c_str()));
   }
   if(options.getErrorPath().size()!=0) {
-    set_attr(&attrib, ATTR_e, Job.getErrorPath());
-  }*/ 
+    set_attr(&attrib, ATTR_e, strdup(options.getErrorPath().c_str()));
+  } 
 
   int connect = cnt2server(mserverName);
   std::cerr << "******* connect = " << connect << std::endl;
   if (connect <= 0) {
+    std::cerr << "vishnu_job_submit: cannot connect to server ";
+    std::cerr <<  pbs_server << " (errno=" << pbs_errno << ") " << pbs_strerror(pbs_errno) << std::endl;
+
+    if (getenv("PBSDEBUG") != NULL)
+    {
+      std::cerr <<  "vishnu_job_submit: pbs_server daemon may not be running on host";
+      std::cerr << pbs_server << "or hostname in file '$TORQUEHOME/server_name' may be incorrect" << std::endl;
+    }
+
     return -1;
   }
   std::cout << "  scriptPath=" << scriptPath << std::endl;
@@ -41,17 +50,19 @@ int TorqueServer::submit(const char* scriptPath, const TMS_Data::SubmitOptions& 
 
     if (errmsg != NULL)
     {
-      std::cerr << "tms_submit:" << errmsg << std::endl;
+      std::cerr << "vishnu_job_submit:" << errmsg << std::endl;
     }
     else
     {
-      std::cerr << "tms_submit: Error (" << pbs_errno << "-";
+      std::cerr << "vishnu_job_submit: Error (" << pbs_errno << "-";
       std::cerr << pbs_strerror(pbs_errno) << std::endl;
     }
     pbs_disconnect(connect);
 
     return -1;
   }
+
+  pbs_disconnect(connect);
 
   job.setJobId(std::string(jobId));
 
