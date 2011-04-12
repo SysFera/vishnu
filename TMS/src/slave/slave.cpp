0a462d84ea979c0e2be59a5cd4cf25a5f77b7b0f
Daouda Traore <daouda.traore@sysfera.com>
2011-04-12 20:28:02 +0200

    ajout de BatchFactory et LLServer

--- TMS/src/slave/slave.cpp
+++ TMS/src/slave/slave.cpp
@@ -22,66 +22,22 @@
 #include "BatchServer.hpp"
 #include "TorqueServer.hpp"
 #include "VishnuException.hpp"
+#include "BatchFactory.hpp"
+#include "TMSVishnuException.hpp"
+#include "UMSVishnuException.hpp"
 #include "utilVishnu.hpp"
 
 namespace bfs=boost::filesystem; // an alias for boost filesystem namespac
 using namespace std;
 using namespace vishnu;
 
-/////////////////////////////////////////ATTENTION A METRE dans le fichier de cli/////////////////////
-std::ostream&
-operator<<(std::ostream& os, const TMS_Data::SubmitOptions_ptr& options) {
 
-  if(options!=NULL) {
-    std::string name = options->getName();
-    std::string queuename = options->getQueue();
-    int wallTime = options->getWallTime();
-    int nbCpu = options->getNbCpu();
-    int nbNodesAndCpuPerNode = options->getNbNodesAndCpuPerNode();
-    std::string outputPath = options->getOutputPath();
-    std::string errorPath =  options->getErrorPath();
-    int mem = options->getMemory();
-
-    os << "==============SubmitOptions content ================" << endl;
-    if(name.size()!=0) os << setw(25) << right << "name: " << name  << endl;
-    if(queuename.size()!=0) os << setw(25) << right << "queuename: " << queuename  << endl;
-    if(wallTime!=-1) os << setw(25) << right << "wallTime: " << wallTime  << endl;
-    if(nbCpu!=-1) os << setw(25) << right << "nbCpu: " << nbCpu  << endl;
-    if(nbNodesAndCpuPerNode!=-1) os << setw(25) << right << "nbNodesAndCpuPerNode: " << nbNodesAndCpuPerNode  << endl;
-    if(outputPath.size()!=0) os << setw(25) << right << "outputPath: " << outputPath  << endl;
-    if(errorPath.size()!=0)  os << setw(25) << right << "errorPath: " << errorPath  << endl;
-    if(mem!=-1) os << setw(25) << right << "memory: " << mem  << endl;
-    os << endl;
-  }
-  return os;
-}
-
-std::ostream&
-operator<<(std::ostream& os, const TMS_Data::Job_ptr& job) {
-
-  std::string jobId = job->getJobId();
-  std::string jobScriptPath = job->getJobPath();
-  std::string errorInfo = job->getJobDescription();
-
-  os << "==============Job content ================" << endl;
-  if(jobId.size()!=0) os << setw(25) << right << "jobId: " << jobId  << endl;
-  if(jobScriptPath.size()!=0) os << setw(25) << right << "jobScriptPath: " << jobScriptPath  << endl;
-  if(errorInfo.size()!=0) os << setw(25) << right << "errorInfos: " << errorInfo << endl;
-  os << endl;
-
-  return os;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
-int jobSubmit(TMS_Data::Job& job, char* jobScriptPath,
-              const TMS_Data::SubmitOptions& options, 
-              BatchServer* batchServer, char **envp) {
-      return batchServer->submit(jobScriptPath, options, job, envp);
-}
-
-int jobCancel(TMS_Data::Job& job, BatchServer* batchServer) {
-        return batchServer->cancel(job.getJobId().c_str());
+void usage(char* cmd) {
+  cerr << "Usage: " << cmd << "COMMANDE_TYPE[SUBMIT] <BatchType> <JobSerializedPath> <SlaveErrorPath> <JobUpdatedSerializedPath>"; 
+  cerr << " <SubmitOptionsSerializedPath> <job_script_path>" << endl;
+  cerr << "\t\t\t\t\t" << " or " << endl;
+  cerr << "Usage: " << cmd << "COMMANDE_TYPE[CANCEL] <BatchType> <JobSerializedPath>  <SlaveErrorPath>" << endl;
+  exit(EXIT_FAILURE);
 }
 
 /*
@@ -101,60 +57,76 @@ main(int argc, char* argv[], char* envp[])
   char* slaveJobFile = NULL;
   char* slaveErrorPath = NULL;
   char* jobScriptPath = NULL;
+  BatchType batchType ;
+  std::string batchTypeStr;
+
+  std::ostringstream os1;
+  std::ostringstream os2;//temporaire
 
   if(argc < 2) {
-    cerr << "Usage: " << argv[0] << " ACTION[SUBMIT|CANCEL] <JobSerializedPath> <SlaveErrorPath> <JobUpdatedSerializedPath>  <SubmitOptionsSerializedPath>" << endl;    
-    cerr << " <job_script_path>" << endl;
-    exit(EXIT_FAILURE);
+    usage(argv[0]);
   }
   action = std::string(argv[1]);
-
   if(action.compare("SUBMIT")==0) {
-    if(argc < 6) {
-      cerr << "Usage: " << argv[0] << " <SUBMIT> <JobSerializedPath> <SlaveErrorPath> <JobUpdatedSerializedPath> <SubmitOptionsSerializedPath>" << endl;
-      cerr << " <JobScriptPath>" << endl;
-      exit(EXIT_FAILURE);
-    }
-    slaveJobFile = argv[4];
-    optionsPath = argv[5];
-    jobScriptPath = argv[6];
+    if(argc < 8) {
+       usage(argv[0]);
+     }
+    slaveJobFile = argv[5];
+    optionsPath = argv[6];
+    jobScriptPath = argv[7];
+    os2 << slaveJobFile << " " << optionsPath << " " << jobScriptPath << std::endl;
   }
   else if(action.compare("CANCEL")==0) {
-    if(argc < 4) {
-      cerr << "Usage: " << argv[0] << " <CANCEL> <JobSerializedPath>  <SlaveErrorPath>" << endl;
-      exit(EXIT_FAILURE);
+    if(argc < 5) {
+     usage(argv[0]);
     }
   } else {
-    cerr << "Unknown ACTION, it must be SUBMIT or CANCEL..." << endl;
-    cerr << "Usage: " << argv[0] << " ACTION[SUBMIT|CANCEL] <JobSerializedPath> <SlaveErrorPath> <JobUpdatedSerializedPath>  <SubmitOptionsSerializedPath>" << endl;
-    exit(EXIT_FAILURE);
+    usage(argv[0]);
   }
+
+  // Other command-line parameters
+  batchTypeStr = argv[2];
+  if (batchTypeStr == "TORQUE") {
+    batchType = TORQUE;
+  } else if (batchTypeStr == "LOADLEVELER") {
+    batchType = LOADLEVELER;
+  } else {
+    std::cerr << "Error: invalid value for batch type parameter (must be 'TORQUE' or 'LOADLEVLER')" << std::endl;
+    throw UMSVishnuException(ERRCODE_INVALID_PARAM, "slave: invalid value for batch type parameter (must be 'TORQUE' or 'LOADLEVLER')");
+  }
+  
+  jobSerializedPath = argv[3];
+  slaveErrorPath = argv[4];
+  os1 << argv[0] << " " << action << " " << batchTypeStr << " " << jobSerializedPath << " " << slaveErrorPath << " ";
+  //Just to verify command line
+  std::cout << os1.str() << os2.str() << std::endl;
  
-  jobSerializedPath = argv[2];
-  slaveErrorPath = argv[3];
+  TMS_Data::Job_ptr job = NULL;
+  TMS_Data::SubmitOptions_ptr submitOptions = NULL;
+  BatchServer* batchServer;
 
   try {
-   
-    std::string jobSerialized = vishnu::get_file_content(jobSerializedPath);
-    TMS_Data::Job_ptr job = NULL;
-    if(!parseTMSEmfObject(std::string(jobSerialized), job)) {
-      cout << "parseEmfObject returns NULL...." << endl;
-      return 1;
+  
+    //To create batchServer Factory
+    BatchFactory factory;
+    batchServer = factory.getBatchServerInstance(batchType);
+    if(batchServer==NULL) {
+      throw UMSVishnuException(ERRCODE_INVALID_PARAM, "slave: getBatchServerInstance return NULL instance");
     }
 
-    cout << job << endl;
-    TorqueServer* torqueServer = new TorqueServer();
+    std::string jobSerialized = vishnu::get_file_content(jobSerializedPath);
+    if(!parseTMSEmfObject(std::string(jobSerialized), job)) {
+      throw UMSVishnuException(ERRCODE_INVALID_PARAM, "slave: job object is not well built");
+    }
+
     if(action.compare("SUBMIT")==0) {
       std::string options  = vishnu::get_file_content(optionsPath);
-      TMS_Data::SubmitOptions_ptr submitOptions = NULL;
       if(!parseTMSEmfObject(std::string(options), submitOptions)) {
-        cout << "parseEmfObject returns NULL...." << endl;
-        return 1;
+        throw UMSVishnuException(ERRCODE_INVALID_PARAM, "slave: SubmitOptions object is not well built");
       }
 
-      cout << submitOptions << endl;
-
-      jobSubmit(*job, jobScriptPath, *submitOptions, torqueServer, envp);
+      //Submits the job
+      batchServer->submit(jobScriptPath, *submitOptions, *job, envp);
 
       //To serialize the user object
       const char* name = "Job";
@@ -168,14 +140,11 @@ main(int argc, char* argv[], char* envp[])
       os_slaveJobFile << slaveJob;
       os_slaveJobFile.close();
 
-      delete submitOptions;
     } else if(action.compare("CANCEL")==0) {
-
-      jobCancel(*job, torqueServer);
+      //To cancel the job
+      batchServer->cancel((*job).getJobId().c_str());
 
     }
-    delete job;
-    delete torqueServer;
   } catch (VishnuException& ve) {
     std::string errorInfo =  ve.buildExceptionString();
     std::ofstream os_error(slaveErrorPath);
@@ -189,7 +158,10 @@ main(int argc, char* argv[], char* envp[])
     //os_error.close();
   }
 
- 
+  delete job;
+  delete submitOptions;
+  delete batchServer; 
+
   return 0;
 }
 
