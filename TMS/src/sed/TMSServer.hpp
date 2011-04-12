0bf25417aecb12b9c0fe1d98a627845b10b22b77
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-04-12 16:38:01 +0200

    use configuration file for TMS sed

--- TMS/src/sed/TMSServer.hpp
+++ TMS/src/sed/TMSServer.hpp
@@ -2,7 +2,7 @@
  * \file TMSServer.hpp
  * \brief This file presents the implementation of the TMS server.
  * \author Daouda Traore (daouda.traore@sysfera.com)
- * \date April 
+ * \date April
 */
 
 #ifndef _TMS_SERVER_H_
@@ -11,13 +11,13 @@
 #include <string>
 
 #include "DIET_server.h"
-
+#include "DbConfiguration.hpp"
 #include "utilVishnu.hpp"
 
 /**
  *  * \brief Number of service in TMS
  *   */
-#define NB_SRV 2 
+#define NB_SRV 2
 
 static const char* SERVICES[NB_SRV] = {
   "jobSubmit_",
@@ -38,25 +38,27 @@ public :
 
   /**
   * \brief To get the batchType
-  * \return the type of the underlying batch scheduler 
+  * \return the type of the underlying batch scheduler
   */
-  static BatchType 
+  static BatchType
   getBatchType();
 
  /**
-  * \brief To get the machine id of the TMS server 
-  * \return the machine id  
+  * \brief To get the machine id of the TMS server
+  * \return the machine id
   */
-  static std::string 
+  static std::string
   getMachineId();
 
    /**
    * \brief To initialize the TMS Server class
-   * \param bachType the type of batch scheduler 
-   * \param machineId the id of the machine 
+   * \param vishnuId The identifier of the vishnu instance
+   * \param dbConfig  The configuration of the database
+   * \param machineId the id of the machine
+   * \param batchType the type of batch scheduler
    */
   int
-  init(BatchType batchType, std::string machineId);
+  init(int vishnuId, DbConfiguration dbConfig, std::string machineId, BatchType batchType);
 
   /**
    * \brief Destructor, raises an exception on error
