8ed3f2480f552a7bf029d2f0d3c04bedd677a04b
Kevin Coulomb <kevin.coulomb@sysfera.com>
2011-04-13 18:02:28 +0200

    Enhance ll code for listjob and getjob

--- TMS/src/server/LLServer.hpp
+++ TMS/src/server/LLServer.hpp
@@ -12,36 +12,80 @@
 class LLServer : public BatchServer
 {
 
-  public:
-    LLServer();
-    int submit(const char* scriptPath, const TMS_Data::SubmitOptions& options, TMS_Data::Job& job, char** envp=NULL);
-    int cancel(const char* jobId) { };
-    TMS_Data::Job getJob() { };
-    /**
-     * \brief To list the jobs on the torque batch
-     * \fn TMS_Data::ListJobs* listJobs(ListJobsOptions op)
-     * \param op: Options to list the jobs
-     * \return The list of the jobs
-     */
-    TMS_Data::ListJobs*
-      listJobs(TMS_Data::ListJobsOptions op);
-    TMS_Data::JobResult getJobResults() { };
-    TMS_Data::ListQueues* listQueues() { };
-    TMS_Data::ListJobResults* getAllJobsResults() { };
-    /**
-     * \brief To get the info about a job
-     * \fn TMS_Data::Job* getJobInfo(string job)
-     * \param job: The id of the job
-     * \return The filled job
-     */
-    TMS_Data::Job* 
-      getJobInfo(string job);
+public:
+  LLServer();
+  int submit(const char* scriptPath, const TMS_Data::SubmitOptions& options, TMS_Data::Job& job, char** envp=NULL);
+  int cancel(const char* jobId) { };
+  TMS_Data::Job getJob() { };
+  /**
+   * \brief To list the jobs on the torque batch
+   * \fn TMS_Data::ListJobs* listJobs(ListJobsOptions op)
+   * \param op: Options to list the jobs
+   * \return The list of the jobs
+   */
+  TMS_Data::ListJobs*
+  listJobs(TMS_Data::ListJobsOptions op);
+  TMS_Data::JobResult getJobResults() { };
+  TMS_Data::ListQueues* listQueues() { };
+  TMS_Data::ListJobResults* getAllJobsResults() { };
+  /**
+   * \brief To get the info about a job
+   * \fn TMS_Data::Job* getJobInfo(string job)
+   * \param job: The id of the job
+   * \return The filled job
+   */
+  TMS_Data::Job* 
+  getJobInfo(string job);
 
-    TMS_Data::ListProgression* getJobProgress(TMS_Data::ProgressOptions) {};
+  TMS_Data::ListProgression* getJobProgress(TMS_Data::ProgressOptions) {};
 
-    ~LLServer() { }
+  ~LLServer() { }
 
+private:
+  /**
+   * \brief To list the jobs on the torque batch
+   * \fn TMS_Data::ListJobs* listJobs(ListJobsOptions op)
+   * \param op: Options to list the jobs
+   * \return The list of the jobs
+   */
+  TMS_Data::ListJobs*
+  listAllJobs();
+
+  /**
+   * \brief Check if the job in the iterator corresponds to the option
+   * \fn bool notIn(TMS_Data::Jobs it, TMS_Data::ListJobsOptions op)
+   * \param it: Iterator on the job
+   * \param op: The options to use as a filter
+   * \return If the job pointed by the iterator does not fit the options (to remove from list if true returned)
+   */
+  bool 
+  notIn(TMS_Data::Job_ptr it, TMS_Data::ListJobsOptions op);
+
+  /**
+   * \brief To extract the job corresponding to the jobid from the member job list
+   * \fn TMS_Data::Job* filterJob(string jobid)
+   * \param jobid: The id of the job searched
+   * \return The corresponding job, throw an exception if it is not found
+   */
+  TMS_Data::Job*
+  filterJob(string jobid);
+  
+  /**
+   * \brief Filter the jobs in the member list mjobs, filters are based on the options op
+   * \fn TMS_Data::ListJobs* filterJobs(TMS_Data::ListJobsOptions op)
+   * \param op: The option to use as a filter on the list
+   * \return The filtered mjobs list
+   */
+  TMS_Data::ListJobs*
+  filterJobs(TMS_Data::ListJobsOptions op);
+  /**
+   * \brief List of jobs
+   */  
   TMS_Data::ListJobs_ptr mjobs;
+  /**
+   * \brief A job
+   */  
+  TMS_Data::Job_ptr mjob;
 };
 
 #endif
