96b7f740f8b3e37dbcecc52c2c4571cbe76f29e5
keo <keo@ukalerk.(none)>
2011-04-14 14:34:41 +0200

    Factorize finalize function and stub for TMS decode

--- core/src/registry/Mapper.cpp
+++ core/src/registry/Mapper.cpp
@@ -20,3 +20,32 @@ Mapper::~Mapper(){
   pthread_mutex_destroy(&mutex);
 }
 
+
+string
+Mapper::finalize(int key){
+  map<int, string>::iterator it;
+  string res;
+  pthread_mutex_lock(&mutex);
+  it = mcmd.find(key);
+  if (it==mcmd.end()){
+    throw new SystemException(ERRCODE_SYSTEM, "Unknown key to finalize");
+  }
+  res = it->second;
+  mcmd.erase(it->first);
+  pthread_mutex_unlock(&mutex);
+  return res;
+}
+
+void
+Mapper::findSeparator(const string& s, vector<int>& vec){
+  size_t cpt = 0;
+  do{
+    cpt=s.find_first_of("#", cpt);
+    if (cpt ==string::npos){
+      return;
+    }
+    vec.push_back(cpt);
+    cpt++;
+  }
+  while ((true));
+}
