d11e07377b31ceeb1d111f450c68ceeece141870
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-04-15 15:18:31 +0200

    replaced exception codes

--- core/src/exception/TMSVishnuException.cpp
+++ core/src/exception/TMSVishnuException.cpp
@@ -22,7 +22,6 @@ TMSVishnuException::initMsg(){
   mp.insert(std::pair<int, std::string>(ERRCODE_UNKNOWN_BATCH_SCHEDULER, std::string("The batch scheduler type is unknown")));
   mp.insert(std::pair<int, std::string>(ERRCODE_BATCH_SCHEDULER_ERROR, std::string("The batch scheduler indicates an error")));
   mp.insert(std::pair<int, std::string>(ERRCODE_UNKNOWN_JOBID, std::string("The job id is unknown")));
-  mp.insert(std::pair<int, std::string>(ERRCODE_CANT_CANCEL, std::string("Can not cancel this job")));
-  mp.insert(std::pair<int, std::string>(ERRCODE_TMS_NOT_ADMIN, std::string("The user is not an administrator")));
+  mp.insert(std::pair<int, std::string>(PERMISSION_DENIED, std::string("Permission denied")));
 
 }
