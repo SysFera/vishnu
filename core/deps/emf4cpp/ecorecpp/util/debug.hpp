b074ed58456eabd2d47308985d93ebb8445f9d05
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-04-19 10:13:09 +0200

    make EMF debug messages appear only when DEBUG_EMF is set to 1

--- core/deps/emf4cpp/ecorecpp/util/debug.hpp
+++ core/deps/emf4cpp/ecorecpp/util/debug.hpp
@@ -2,17 +2,17 @@
 /*
  * util/debug.hpp
  * Copyright (C) Cátedra SAES-UMU 2010 <andres.senac@um.es>
- * 
+ *
  * EMF4CPP is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * EMF4CPP is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,7 +22,7 @@
 
 #include <iostream>
 
-#ifdef DEBUG
+#ifdef DEBUG_EMF
 
 #define DEBUG_MSG( X, Y ) \
         std::X <<__FILE__ << ":" <<__LINE__ << ":" << __FUNCTION__ << ": " << Y << std::endl
