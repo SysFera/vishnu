a92fdd8ed9c6382373844660286c752838165d67
Kevin Coulomb <kevin.coulomb@sysfera.com>
2011-04-21 09:51:25 +0200

    IMS added

---
+++ IMS/src/sed/internalApiIMS.cpp
@@ -0,0 +1,92 @@
+#include "internalApiIMS.hpp"
+#include <exception>
+#include "VishnuException.hpp"
+#include "SystemException.hpp"
+#include "IMSMapper.hpp"
+#include "MapperRegistry.hpp"
+
+int 
+solveExport(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveCurMetric(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveOldMetric(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solvePS(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetSysInfo(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetThreshold(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveGetThreshold(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetUID(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetJID(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetTID(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetMID(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveLoadShed(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveSetUpFreq(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveGetUpFreq(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveRestart(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveStop(diet_profile_t* pb){
+  return 0;
+}
+
+int 
+solveGetSysInfo(diet_profile_t* pb){
+  return 0;
+}
+
