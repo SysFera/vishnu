8e7e5a6ab2483d2eebf70b487b989ea57564788c
Kevin Coulomb <kevin.coulomb@sysfera.com>
2011-04-28 16:51:01 +0200

    Add cli missing files

---
+++ IMS/src/cli/displayer.hpp
@@ -0,0 +1,29 @@
+#include "api_ims.hpp"
+
+/**
+ * \brief To display the list of processes
+ * \param li: The list of processes to display
+ */
+void 
+displayListProc(IMS_Data::ListProcesses* li);
+
+/**
+ * \brief To display a process
+ * \param p: The process to display
+ */
+void 
+displayProc(IMS_Data::Process* p);
+
+/**
+ * \brief To display the list of metrics
+ * \param li: The list of metrics to display
+ */
+void 
+displayListMetric(IMS_Data::ListMetric* li);
+
+/**
+ * \brief To display a metric
+ * \param p: The metric to display
+ */
+void 
+displayMetric(IMS_Data::Metric* p);
