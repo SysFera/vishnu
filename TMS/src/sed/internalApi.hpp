7481a59e821238ede8d1f78711f8ef702ea34efd
Daouda Traore <daouda.traore@sysfera.com>
2011-05-04 16:35:20 +0200

    rename internalTMSAPI in internalApi

---
+++ TMS/src/sed/internalApi.hpp
@@ -0,0 +1,60 @@
+/**
+* \file internalTMSAPI.hpp
+* \brief This file presents the the implementation of the TMS internalAPI.
+* \author Daouda Traore (daouda.traore@sysfera.com)
+* \date April
+*/
+
+#ifndef INTERNAL_TMS_API_H
+#define INTERNAL_TMS_API_H
+
+/*
+ * SOLVE FUNCTIONS
+ */
+
+/* submitJob */
+int
+solveSubmitJob(diet_profile_t* pb);
+
+/* cancelJob */
+int
+solveCancelJob(diet_profile_t* pb);
+
+/**
+ * \brief Function to solve the service solveGenerique
+ * \param pb is a structure which corresponds to the descriptor of a profile
+ * \return raises an exception on error
+ */
+int
+solveJobInfo(diet_profile_t* pb);
+
+/**
+* \brief Function to solve the service solveGenerique
+* \fn    int solveGenerique(diet_profile_t* pb)
+* \param pb is a structure which corresponds to the descriptor of a profile
+* \return raises an exception on error
+*/
+template <class QueryParameters, class List, class QueryType>
+int
+solveGenerique(diet_profile_t* pb);
+
+/* getListOfJobs */
+int
+solveGetListOfJobs(diet_profile_t* pb);
+
+/* listJobsProgression */
+int
+solveGetListOfJobsProgression(diet_profile_t* pb);
+
+/* listQueues */
+int
+solveListOfQueues(diet_profile_t* pb);
+
+/* jobOutPutGetResult */
+int
+solveJobOutPutGetResult(diet_profile_t* pb);
+
+/* JobOutPutGetCompletedJobsOutput */
+int
+solveJobOutPutGetCompletedJobs(diet_profile_t* pb);
+#endif
