5546ff94cd5cfbbb156978745e416978f3a7c38f
Kevin Coulomb <kevin.coulomb@sysfera.com>
2011-05-04 10:38:07 +0200

    Update python test script

--- TMS/test/src/toto.py
+++ TMS/test/src/toto.py
@@ -1,6 +1,7 @@
 
 import VISHNU_UMS
 import VISHNU_TMS
+import time
 
 def displayQueue (q):
   print "Queue number 1:"
@@ -36,6 +37,17 @@ def displayProg(li):
 #  print "progress jname", li.getProgress().get(3).getJobName()
 #  print "progress jpercent", li.getProgress().get(3).getPercent()
 
+def displayOut(jr):
+  print "JobId: ", jr.getJobId()
+  print "output: ", jr.getOutputPath()
+  print "error: ", jr.getErrorPath()
+
+def displayComp(lijr):
+  displayOut(lijr.getResults().get(0))
+
+def displayListJ(lij):
+  displayJob(lij.getJobs().get(0))
+
 VISHNU_UMS.vishnuInitialize("/home/keo/Bureau/depot_git_edf/vishnu/UMS/test/src/sql/client.cfg")
 
 j = VISHNU_TMS.Job()
@@ -46,16 +58,33 @@ r, k = VISHNU_UMS.connect("root", "vishnu_user")
 q = VISHNU_TMS.ListQueues()
 opp = VISHNU_TMS.ProgressOptions()
 li = VISHNU_TMS.ListProgression()
+jr = VISHNU_TMS.JobResult()
+lijr = VISHNU_TMS.ListJobResults()
+out = "/tmp"
+lij = VISHNU_TMS.ListJobs()
+opj = VISHNU_TMS.ListJobsOptions()
 
 try :
   VISHNU_TMS.submitJob(k, mid, path, j, op)
   print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-3"
+  VISHNU_TMS.listJobs(k, mid, lij, opj)
+  print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-3.1"
   VISHNU_TMS.listQueues(k, mid, q)
   print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-2"
   VISHNU_TMS.getJobInfo(k, mid, j.getJobId(), j)
   print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-1"
   VISHNU_TMS.getJobProgress(k, mid, li, opp)
   print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0"
+  print "sleeping to wait for completion"
+  time.sleep(20)
+  VISHNU_TMS.getJobOutput(k, mid, j.getJobId(), jr, out)
+  print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0.1"
+  VISHNU_TMS.getCompletedJobsOutput(k, mid, lijr, out)
+  print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0.2"
+  VISHNU_TMS.submitJob(k, mid, path, j, op)
+  print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0.3"
+  VISHNU_TMS.cancelJob(k, mid, j.getJobId())
+  print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0.4"
 except VISHNU_TMS.TMSVishnuException, e:
   print e.what()
 except VISHNU_UMS.UMSVishnuException, e:
@@ -70,4 +99,13 @@ print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2"
 displayJob(j)
 print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@3"
 displayProg(li)
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@4"
+displayOut(jr)
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5"
+displayComp(lijr)
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@6"
+displayListJ(lij)
 
+VISHNU_TMS.getJobProgress(k, mid, li, opp)
+
+# command execfile("/home/keo/Bureau/depot_git_edf/vishnu/TMS/test/src/toto.py")
