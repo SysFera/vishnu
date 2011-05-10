c59b0052448a77de305a0e100ffa799587e4378e
Paco <caplachiche@yahoo.fr>
2011-05-10 18:00:24 +0200

    Ajout de get_SystemInfo client/cli

--- IMS/src/cli/displayer.hpp
+++ IMS/src/cli/displayer.hpp
@@ -4,28 +4,28 @@
  * \brief To display the list of processes
  * \param li: The list of processes to display
  */
-void 
+void
 displayListProc(IMS_Data::ListProcesses* li);
 
 /**
  * \brief To display a process
  * \param p: The process to display
  */
-void 
+void
 displayProc(IMS_Data::Process* p);
 
 /**
  * \brief To display the list of metrics
  * \param li: The list of metrics to display
  */
-void 
+void
 displayListMetric(IMS_Data::ListMetric* li);
 
 /**
  * \brief To display a metric
  * \param p: The metric to display
  */
-void 
+void
 displayMetric(IMS_Data::Metric* p);
 /**
  * \brief To display the update frequency
@@ -33,3 +33,19 @@ displayMetric(IMS_Data::Metric* p);
  */
 void
 displayFreq(int freq);
+
+/**
+* \brief To display a system information
+* \param sysInfo: The system information to display
+*/
+void
+displaySystemInfo(IMS_Data::SystemInfo* sysInfo);
+
+/**
+ * \brief To display the list of system info
+ * \param li: The list of system information to display
+ */
+void
+displayListSysInfo(IMS_Data::ListSysInfo* li);
+
+
