4605345012095493a33f07897eca9655971cd746
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-05-10 17:00:59 +0200

    maj du setCopyright: ignore third_party

--- scripts/setCopyright.sh
+++ scripts/setCopyright.sh
@@ -2,10 +2,9 @@
 # Bash script to append the copyright to the beginning of all files
 # Author : Ibrahima Cisse (Ibrahima.cisse@sysfera.com)
 # Date   : 03/03/2011
-# Usage    : ./setCopyright.sh copyright
+# Usage    : ./setCopyright.sh copyright  
 
-#
 
-for file in $(find . -name "*.?pp"); do cat $1 $file > tmp.txt && cp tmp.txt $file; done;
+for file in $(find . -name third-party -prune -o -name "*.?pp"); do cat $1 $file > tmp.txt && cp tmp.txt $file; done;
 
 
