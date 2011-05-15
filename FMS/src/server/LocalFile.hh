59c571341070697d474ddef14c9be7b1cf53d9ff
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-05-15 23:43:19 +0200

    maj FMS: chaine complete pour head et tail

---
+++ FMS/src/server/LocalFile.hh
@@ -0,0 +1,36 @@
+#ifndef LOCALFILE_HH
+#define LOCALFILE_HH
+
+#include <string>
+
+#include "File.hh"
+
+/* Local implementation of a DIET file. */
+class LocalFile : public File {
+private:
+  mutable bool upToDate;
+public:
+  LocalFile();
+  LocalFile(const std::string& path);
+  LocalFile(const LocalFile& file);
+  
+  LocalFile& operator=(const LocalFile& file);
+  
+  virtual bool isUpToDate() const;
+  virtual void getInfos() const;
+  
+  virtual int chgrp(const std::string& group);
+  virtual int chmod(const mode_t mode);
+  virtual File* cp(const std::string& dest);
+  virtual std::string head(const unsigned int nline);
+  virtual int mkdir(const mode_t mode);
+  virtual File* mv(const std::string& dest);
+  virtual int rm();
+  virtual int rmdir();
+  virtual std::string tail(const unsigned int nline); 
+  virtual std::list<std::string> lsDir() const;
+  virtual std::list<std::string> lsDirRec() const;
+  virtual std::list<std::string> lsDirSimple() const;
+};
+
+#endif
