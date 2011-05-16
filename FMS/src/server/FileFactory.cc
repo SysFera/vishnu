a4ccd8aafb7a41cb401c3552f9471cb4a9d62890
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-05-16 23:58:16 +0200

    Prise en compte de la sessionKey:cote client

--- FMS/src/server/FileFactory.cc
+++ FMS/src/server/FileFactory.cc
@@ -28,28 +28,15 @@ void FileFactory::setSCPCommand(const string& scpCommand) {
   FileFactory::scpCommand = scpCommand;
 }
 
-File* FileFactory::getFileClient(const string& path,
-                                 const string& user) {
-  string distUser(user);
-  if (File::extHost(path)=="localhost")
-    return new LocalFile(path);
-
-  if (user=="") {
-    uid_t uid = getuid();
-    struct passwd*  pw = getpwuid(uid);
-    distUser = pw->pw_name;
-  }
-  return new RemoteFile(path, distUser);
-}
 
 File* FileFactory::getFileServer(const string& path,
                                  const string& user,
                                  const string& key) {
   
-  if (File::extHost(path)=="localhost")
+  //if (File::extHost(path)=="localhost")
     return new SSHFile(path, sshServer, user, "", key, "",
                        sshPort, sshCommand, scpCommand);
-  return new RemoteFile(path, user);
+  //return new RemoteFile(path, user);
 }
 
 string FileFactory::sshServer = "localhost";
