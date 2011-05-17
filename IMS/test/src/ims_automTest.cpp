1ace2dffe3adcb7cf509100c2c7ffde0cc4fe48a
Paco <caplachiche@yahoo.fr>
2011-05-17 11:35:15 +0200

    IMS: Added Get Metric data

--- IMS/test/src/ims_automTest.cpp
+++ IMS/test/src/ims_automTest.cpp
@@ -37,21 +37,47 @@ namespace bfs= boost::filesystem;
 
 BOOST_GLOBAL_FIXTURE(IMSSeDFixture)
 
-//Test category 1
 
-// T1.1 : submit a job
+//Test category 3
 
+// I2 - B : I2 – B: Get metric  data
 
-BOOST_AUTO_TEST_SUITE(submit_a_job)
+BOOST_AUTO_TEST_SUITE(get_metric_data)
 
+  // Get metric data : normal call
 
-  // submit a job: normal call
+BOOST_AUTO_TEST_CASE( get_metric_data_normal_call) {
 
-BOOST_AUTO_TEST_CASE( submit_a_Job_normal_call) {
+  string sqlPath = IMSSQLPATH;
 
-  BOOST_TEST_MESSAGE("\n\n Testing normal job submission corresponding to use case T1.1" << "\n");
-  sleep(1);
+  BOOST_TEST_MESSAGE("\n\n Use case I2 – B: Get metric  data" << "\n");
+  int nbResMetric = 2;
+  VishnuConnexion vc("root","vishnu_user");
+  // get the session key and the machine identifier
+  string sessionKey=vc.getConnexion();
+  string machineId="machine_1";
+  IMS_Data::ListMetric list;
+  IMS_Data::MetricHistOp op;
+  //Set the type of metric to get
+  op.setType(5);
 
+  //Initialize the databatase: put the update frequency to 10
+  //and cleaning of the database state
+  if (restore(sqlPath + "/IMSTestGetMetric.sql") != 0) {
+    cout << "Database update failed" << endl;
+    return;
+  }
+  sleep (5);
+  //Wait for metric recording in database
+  //10 represents the update frequency
+  sleep (nbResMetric*10);
+
+  BOOST_CHECK_EQUAL(getMetricHistory(sessionKey, machineId, list, op),0  );
+  BOOST_TEST_MESSAGE( "list.getMetric().size()" << list.getMetric().size() << "\n");;
+
+  BOOST_REQUIRE(list.getMetric().size() == nbResMetric);
+
+ // BOOST_TEST_MESSAGE("\n\n  Testing Use case I2 – B: Get metric  data" << "\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
