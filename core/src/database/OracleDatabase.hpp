2203e45b5133f1d36e52844816c8651f0c7ad051
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-05-19 11:43:28 +0200

    add new method to get the database type

--- core/src/database/OracleDatabase.hpp
+++ core/src/database/OracleDatabase.hpp
@@ -88,6 +88,13 @@ public :
   DatabaseResult*
   getResult(std::string request);
 
+  /**
+   * \brief To get the type of database
+   * \return An enum identifying the type of database
+   */
+  DbConfiguration::db_type_t
+  getDbType() { return DbConfiguration::ORACLE; };
+
 private :
 
   /////////////////////////////////
