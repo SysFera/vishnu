4d7dd8aaba72d30aaaae00aaf26d5f0312a8f3dc
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-05-20 15:44:32 +0200

    Fix call to invalid sql query

--- UMS/test/src/sql/clean_session_tempPwd.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM vsession;
-DELETE FROM account;
-DELETE FROM users    WHERE userid='toto_1';
-DELETE FROM users    WHERE userid='toto_1';
-DELETE FROM machine  WHERE machineid='machine_2';
-UPDATE users SET passwordstate=0 WHERE userid='admin_1';
--- FIXME REMOVE THIS FILE!!!
