abc7abf67503410e9a11cf1c14d3f0477082f36e
Paco <caplachiche@yahoo.fr>
2011-05-23 16:27:36 +0200

    IMS test: Added GetProcesses Tests

--- IMS/test/src/ims_automTest.cpp
+++ IMS/test/src/ims_automTest.cpp
@@ -778,11 +778,176 @@ BOOST_AUTO_TEST_SUITE(Information_Managment_System_test)
     //To set the machineId
     sysInfoOp.setMachineId(machineId);
 
-    //To define the file transfer format
     BOOST_CHECK_THROW(getSystemInfo(sessionKey, listSysInfo, sysInfoOp), VishnuException);
   }
 
+   //IA-5: Set system info normal call
+   //Set system info: normal call
+   BOOST_AUTO_TEST_CASE( set_system_info_normal_call) {
 
+    BOOST_TEST_MESSAGE("Use case IA-5: Set system info normal call");
+    string sqlPath = IMSSQLPATH;
+    VishnuConnexion vc("root","vishnu_user");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+    string machineId="machine_1";
+    IMS_Data::ListSysInfo listSysInfo;
+    IMS_Data::SysInfoOp sysInfoOp;
+    //To set the machineId
+    sysInfoOp.setMachineId(machineId);
+    //To set the SystemInfo values
+    IMS_Data::SystemInfo systemInfo;
+    systemInfo.setMachineId(machineId);
+    systemInfo.setDiskSpace(5000000);
+    systemInfo.setMemory(512);
+
+    try {
+      //Set the memory and the diskspace of machine_1 respectively to 512 and 5000000
+      BOOST_CHECK_EQUAL(setSystemInfo(sessionKey, systemInfo),0  );
+      //To get the values
+      BOOST_CHECK_EQUAL(getSystemInfo(sessionKey, listSysInfo, sysInfoOp),0  );
+      //If if the list is not empty
+      BOOST_REQUIRE(listSysInfo.getSysInfo().size() > 0);
+      //To check the memory
+      BOOST_REQUIRE(listSysInfo.getSysInfo().get(0)->getMemory() == 512);
+      //To check the diskspace
+      BOOST_REQUIRE(listSysInfo.getSysInfo().get(0)->getDiskSpace() == 5000000);
+      //BOOST_REQUIRE(listSysInfo.getSysInfo().size() != 0);
+    }
+    catch (VishnuException& e) {
+      BOOST_MESSAGE("FAILED\n");
+      BOOST_MESSAGE(e.what());
+      BOOST_CHECK(false);
+    }
+  }
+
+    //IA5-E1: Set system info with bad machine Id call
+   //Set system info: bad machine Id
+  BOOST_AUTO_TEST_CASE(set_system_info_bad_machine_Id_call) {
+
+    BOOST_TEST_MESSAGE("Use case IA5-E1: Set system info with bad machine Id call");
+    VishnuConnexion vc("root","vishnu_user");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+    string machineId="unknown_name";
+
+    //To set the SystemInfo values
+    IMS_Data::SystemInfo systemInfo;
+    systemInfo.setMachineId(machineId);
+    systemInfo.setDiskSpace(5000000);
+    systemInfo.setMemory(512);
+
+    BOOST_CHECK_THROW(setSystemInfo(sessionKey, systemInfo), VishnuException);
+  }
+
+  //IA5-E1: Set system info with bad metric value call
+  //Set system info: bad metric value call
+  BOOST_AUTO_TEST_CASE(set_system_info_bad_mertric_value_call) {
+
+    BOOST_TEST_MESSAGE("Use case IA5-E2: Set system info with bad metric value call");
+    VishnuConnexion vc("root","vishnu_user");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+    string machineId="unknown_name";
+
+    //To set the SystemInfo values
+    IMS_Data::SystemInfo systemInfo;
+    systemInfo.setMachineId(machineId);
+    systemInfo.setDiskSpace(-5000000);
+    systemInfo.setMemory(-512);
+
+    BOOST_CHECK_THROW(setSystemInfo(sessionKey, systemInfo), VishnuException);
+  }
+
+  //IA5-E2: Set system info for no admin user
+  //Set system info: no admin user
+  BOOST_AUTO_TEST_CASE(set_system_info_no_admin_user_call) {
+
+    BOOST_TEST_MESSAGE("Use case IA5-E2: Set system info for no admin user");
+    //no admin user
+    VishnuConnexion vc("user_1","toto");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+    string machineId="machine_1";
+
+    //To set the SystemInfo values
+    IMS_Data::SystemInfo systemInfo;
+    systemInfo.setMachineId(machineId);
+    systemInfo.setDiskSpace(5000000);
+    systemInfo.setMemory(512);
+
+    BOOST_CHECK_THROW(setSystemInfo(sessionKey, systemInfo), VishnuException);
+  }
+    //Test category 3
+    //IA1-B:  Get the running processes normal call
+   // Get the running processes: normal call
+   BOOST_AUTO_TEST_CASE( get_running_processes_normal_call) {
+
+    BOOST_TEST_MESSAGE("Use case IA1-B: Get the running processes normal call");
+    bool imssedFound = false;
+    bool tmssedFound = false;
+    bool umssedFound = false;
+    VishnuConnexion vc("root","vishnu_user");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+    string machineId="machine_1";
+    IMS_Data::ListProcesses listProcess;
+    IMS_Data::ProcessOp op;
+    op.setMachineId(machineId);
+
+    try {
+      BOOST_CHECK_EQUAL(getProcesses(sessionKey, listProcess, op),0  );
+      BOOST_REQUIRE(listProcess.getProcess().size() != 0);
+
+      //To check if the main processes are on the list of processes
+      for(unsigned int i = 0; i < listProcess.getProcess().size(); i++) {
+        //To check imssed process
+        if (listProcess.getProcess().get(i)->getProcessName().compare("IMS") == 0) {
+          imssedFound = true;
+        }
+        //To check tmssed process
+        if (listProcess.getProcess().get(i)->getProcessName().compare("TMS") == 0) {
+          tmssedFound = true;
+        }
+        //To check umssed process
+        if (listProcess.getProcess().get(i)->getProcessName().compare("UMS") == 0) {
+          umssedFound = true;
+        }
+      }
+      //To check if the main processes are found on the list
+      BOOST_REQUIRE(imssedFound ==  true);
+      BOOST_REQUIRE(umssedFound ==  true);
+      BOOST_REQUIRE(tmssedFound ==  true);
+      //TODO: a la fin des tests nettoyer la table processes
+    }
+    catch (VishnuException& e) {
+      BOOST_MESSAGE("FAILED\n");
+      BOOST_MESSAGE(e.what());
+      BOOST_CHECK(false);
+    }
+  }
+
+  BOOST_AUTO_TEST_CASE( clean_table_process_call) {
+
+    string sqlPath = IMSSQLPATH;
+
+    BOOST_TEST_MESSAGE("Clean process table");
+    VishnuConnexion vc("root","vishnu_user");
+    // get the session key and the machine identifier
+    string sessionKey=vc.getConnexion();
+
+    try {
+      //Clean of the table process
+      if (restore(sqlPath + "/IMScleanProcesses.sql") != 0) {
+        BOOST_TEST_MESSAGE("Database update failed for restore(sqlPath + /IMScleanProcesses.sql)");
+      }
+    }
+    catch (VishnuException& e) {
+      BOOST_MESSAGE("FAILED\n");
+      BOOST_MESSAGE(e.what());
+      BOOST_CHECK(false);
+    }
+  }
 BOOST_AUTO_TEST_SUITE_END()
 // THE END
 
