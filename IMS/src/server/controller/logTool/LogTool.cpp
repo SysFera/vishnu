402b021b59040fb54931ccc938c50bc7d98732a5
keo <keo@ukalerk.(none)>
2011-05-23 09:54:29 +0200

    Add random integer in log tool name

--- IMS/src/server/controller/logTool/LogTool.cpp
+++ IMS/src/server/controller/logTool/LogTool.cpp
@@ -1,7 +1,19 @@
 #include "LogTool.hpp"
 #include <unistd.h>
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include "utilVishnu.hpp"
+
+using namespace vishnu;
 
 LogTool::LogTool(){
+  boost::mt19937 rng;
+  boost::uniform_int<> uni(1,1<<25); // Randon int between 1 and 1<<25
+  boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+    die(rng, uni);
   char host[HOSTSIZE];
   if (gethostname(host, HOSTSIZE)){
     mname = "IMSLogT";
@@ -10,6 +22,7 @@ LogTool::LogTool(){
     mname = "IMSLogT_";
     mname += string(host);
   }
+  mname += "_"+convertToString(die());
 }
 
 LogTool::~LogTool(){
