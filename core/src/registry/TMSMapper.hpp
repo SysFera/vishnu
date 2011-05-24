8cb47e7eaa1957bfea0b2c19062d5ac9e345bbfa
keo <keo@ukalerk.(none)>
2011-05-24 09:48:53 +0200

    Add mapper for shell export, implement export server, update registry with the decoder set as virtual and protected, start implement IMS mapper

--- core/src/registry/TMSMapper.hpp
+++ core/src/registry/TMSMapper.hpp
@@ -128,7 +128,6 @@ public :
   decode (const string& msg);
 
 protected:
-private:
   /**
    * \brief To decode the submit call sequence of the string returned by finalize
    * \fn string decodeSubmit(vector<int> separator, const string& msg)
@@ -136,7 +135,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeSubmit(vector<int> separator, const string& msg);
   
   /**
@@ -146,7 +145,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeProg(vector<int> separator, const string& msg);
 
   /**
@@ -156,7 +155,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeQueue(vector<int> separator, const string& msg);
 
   /**
@@ -166,7 +165,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeListJob(vector<int> separator, const string& msg);
 
   /**
@@ -176,7 +175,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeOutput(vector<int> separator, const string& msg);
 
   /**
@@ -186,7 +185,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeCompletedJob(vector<int> separator, const string& msg);
 
   /**
@@ -196,7 +195,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeCancel(vector<int> separator, const string& msg);
 
   /**
@@ -206,7 +205,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeGetPeriod(vector<int> separator, const string& msg);
 
   /**
@@ -216,7 +215,7 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeSetPeriod(vector<int> separator, const string& msg);
 
   /**
@@ -226,9 +225,9 @@ private:
    * \param msg The message to decode
    * \return The cli like close command
    */
-  string
+  virtual string
   decodeJobInfo(vector<int> separator, const string& msg);
-
+private:
 };
 
 
