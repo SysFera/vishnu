3bd6a8fdd24991e04ae16e54971c5d8e3400d323
Daouda Traore <daouda.traore@sysfera.com>
2011-05-27 11:54:02 +0200

    FMS: maj de LocalFileProxy

--- FMS/src/client/LocalFileProxy.hpp
+++ FMS/src/client/LocalFileProxy.hpp
@@ -23,20 +23,21 @@ public:
   virtual bool isUpToDate() const;
   virtual void getInfos() const;
   
-  virtual int chgrp(const std::string& group);
-  virtual int chmod(const mode_t mode);
-  virtual std::string head(const HeadOfFileOptions& options);
-  virtual std::string getContent() ;
-  virtual int mkfile(const mode_t mode=420);
-  virtual int mkdir(const mode_t mode=493) ;
-  virtual int rm();
-  virtual int rmdir();
-  virtual std::string tail(const TailOfFileOptions& options); 
+  virtual int chgrp(const std::string& group) { }
+  virtual int chmod(const mode_t mode) { }
+  virtual std::string head(const HeadOfFileOptions& options) { }
+  virtual std::string getContent() { }
+  virtual int mkfile(const mode_t mode=420) { }
+  virtual int mkdir(const mode_t mode=493) { }
+  virtual int rm() { }
+  virtual int rmdir() { }
+  virtual std::string tail(const TailOfFileOptions& options) { }
+
   virtual int cp(const std::string& dest, const CpFileOptions& options, const std::string& serviceName="FileCopy");
 
   virtual int mv(const std::string& dest, const CpFileOptions& options);
 
-  virtual std::list<std::string> ls(const LsDirOptions& options) const;
+  virtual std::list<std::string> ls(const LsDirOptions& options) const { }
 };
 
 #endif
