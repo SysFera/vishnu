aee22272635f0f3796ce79fdb944eafcb1ea45f4
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-05-30 14:21:28 +0200

    FMS ajout de la classe FileTransferProxy

---
+++ FMS/src/client/FileTransferProxy.hpp
@@ -0,0 +1,21 @@
+#ifndef FILETRANSFERPROXY
+#define FILETRANSFERPROXY
+/**
+ * \file FileTransferProxy.hpp
+ * \brief Contains a declaration of all File transfer services
+ */
+
+#include "FMS_Data.hpp"
+
+class FileTransferProxy{
+
+  public:
+  
+    FileTransferProxy(const FMS_Data::FileTransfer& filetranfer);
+    long addCpThread();
+    long addMvThread();
+    int stopThread(long thrId);
+
+};
+
+#endif
