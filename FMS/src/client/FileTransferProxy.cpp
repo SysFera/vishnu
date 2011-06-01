b94abbfa22c32a939ba6b0a2d93b0d7b05fd0b7b
Daouda Traore <daouda.traore@sysfera.com>
2011-06-01 17:53:24 +0200

    FMS: maj de FileTransferProxy

--- FMS/src/client/FileTransferProxy.cpp
+++ FMS/src/client/FileTransferProxy.cpp
@@ -1,15 +1,53 @@
+#include <boost/scoped_ptr.hpp>
+
 #include "FileTransferProxy.hpp"
+#include "FileProxyFactory.hpp"
+#include "SessionProxy.hpp"
 
+using namespace FMS_Data;
+using namespace UMS_Data;
+using namespace std;
 
-FileTransferProxy::FileTransferProxy(const FMS_Data::FileTransfer& filetranfer):mfileTransfer(fileTransfer){}
+FileTransferProxy::FileTransferProxy(const std::string& sessionKey):msessionKey(sessionKey){
+}
 
-long FileTransferProxy::addCpThread();
+FileTransferProxy::FileTransferProxy(const std::string& sessionKey,
+                                     const std::string& srcFilePath,
+                                     const std::string& destFilePath): msessionKey(sessionKey),
+ msrcFilePath(srcFilePath), mdestFilePath(destFilePath){
+}
 
+int FileTransferProxy::addCpThread(const CpFileOptions& options){
+  
+  SessionProxy sessionProxy(msessionKey);
 
-long FileTransferProxy::addMvThread();
+  boost::scoped_ptr<FileProxy> f (FileProxyFactory::getFileProxy(sessionProxy,msrcFilePath));
 
+  int result= f->cpAsync(mdestFilePath,options,mtransferInfo);
 
+  return result;
 
-int FileTransferProxy::stopThread(long thrId);
+}
 
+int FileTransferProxy::addMvThread(const CpFileOptions& options){
 
+  SessionProxy sessionProxy(msessionKey);
+
+  boost::scoped_ptr<FileProxy> f (FileProxyFactory::getFileProxy(sessionProxy,msrcFilePath));
+
+  int result= f->mvAsync(mdestFilePath,options,mtransferInfo);
+
+  return result;
+}
+
+int FileTransferProxy::stopThread(const StopTransferOptions& options) {
+
+}
+
+const FMS_Data::FileTransfer& FileTransferProxy::getFileTransfer() const {
+  return mtransferInfo;
+}
+
+FileTransferProxy::~FileTransferProxy(){
+
+}
