40b84337fd79c392dd7e953bd1dd6bc8e12bcde3
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-06-08 16:46:36 +0200

    FMS:Maj de FMS.apim

--- FMS/src/client/FileProxy.hpp
+++ FMS/src/client/FileProxy.hpp
@@ -13,57 +13,92 @@
 
 using namespace FMS_Data;
 
-/* Definition of the system endianness. */
-#define LT_ENDIAN 0
-#define BG_ENDIAN 1
 
-/* FileProxy "locality" enumeration. */
+/**
+ * \brief FileProxy "locality" enumeration.
+ */
 typedef enum {
   local,
   remote
 } file_host_t;
 
-/* The stat structure differs depending on the system type. */
-/* BSD file systems used the same structure for large and small
+/**
+ * \brief The stat structure differs depending on the system type. 
+ * BSD file systems used the same structure for large and small
  * partition type.
  */
 #if FS_32 | BSD_LIKE_SYSTEM
-  typedef struct stat file_stat_t;
-  typedef off_t file_size_t;
+typedef struct stat file_stat_t;
+typedef off_t file_size_t;
 #define STAT(path, buf) stat((path), (buf))
 #else
-  typedef struct stat64 file_stat_t;
-  typedef off64_t file_size_t;
+typedef struct stat64 file_stat_t;
+typedef off64_t file_size_t;
 #define STAT(path, buf) stat64((path), (buf))
 #endif
 
-/* Main FileProxy class. Encapsulates all the files attributes. */
-/* Defines the common operation on files (local or remote) */
+/**
+ * \brief Main FileProxy class. Encapsulates all the files attributes. 
+ *  Defines the common operation on files (local or remote)
+ */
 class FileProxy {
-private:
+
+  private:
+    /**
+     * \brief The host of the file
+     */
+    std::string mhost;
   
-  std::string mhost;
-  mutable bool exist;
-  SessionProxy msessionProxy;
-  mutable FileStat mfileStat;
+    /**
+     * \brief To check if the file exists
+     */  
+    mutable bool exist;
+    
+    /**
+     * \brief The session proxy object
+     */  
+      SessionProxy msessionProxy;
+    /**
+     * \brief The inode information of the file
+     */  
+      mutable FileStat mfileStat;
 
-protected:
+    protected:
 
-  void setFileStat(const FileStat& fileStat)const;
-  void setPath(const std::string& path);
-  void setHost(const std::string& host);
-  void setOwner(const std::string& owner) const;
-  void setGroup(const std::string& group) const;
-  void setPerms(const mode_t perms) const;
-  void setUid(const uid_t uid) const;
-  void setGid(const gid_t gid) const;
-  void setSize(const file_size_t size) const;
-  void setAtime(const time_t atime) const;
-  void setMtime(const time_t mtime) const;
-  void setCtime(const time_t ctime) const;
-  void setType(const FileType& type) const;
-  void exists(const bool exist) const;
-public:
+    /**
+     * \brief To update the inode information of the file
+     * \param fileStat the new inode information
+     */  
+      void setFileStat(const FileStat& fileStat)const;
+    /**
+     * \brief To update the host  of the file
+     * \param fileStat the new host
+     */  
+      void setPath(const std::string& path);
+   
+      /**
+     * \brief To update the host  of the file
+     * \param fileStat the new host
+     */  
+     
+      void setHost(const std::string& host);
+    
+      /**
+     * \brief To update the host  of the file
+     * \param fileStat the new host
+     */  
+      void setOwner(const std::string& owner) const;
+      void setGroup(const std::string& group) const;
+      void setPerms(const mode_t perms) const;
+      void setUid(const uid_t uid) const;
+      void setGid(const gid_t gid) const;
+      void setSize(const file_size_t size) const;
+      void setAtime(const time_t atime) const;
+      void setMtime(const time_t mtime) const;
+      void setCtime(const time_t ctime) const;
+      void setType(const FileType& type) const;
+      void exists(const bool exist) const;
+    public:
  
   FileProxy();
   
