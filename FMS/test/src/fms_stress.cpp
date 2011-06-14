6ae8984dc567069d1c9926be58f3558821abbd80
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-06-14 17:27:51 +0200

    Maj Plan et rapport de test FMS

--- FMS/test/src/fms_stress.cpp
+++ FMS/test/src/fms_stress.cpp
@@ -9,26 +9,30 @@ using namespace std;
 using namespace UMS_Data;
 using namespace vishnu;
 
-void stress(int cpt,const string& sessionKey,int type){
+void stress(int cpt,const string& sessionKey,const string& machineId,int type){
 
   string dietClientConfigPath = CONFIG_DIR + string("/client_testing.cfg");
-  FMS_Data::LsTransferOptions lsFileTransferOptions;
-  lsFileTransferOptions.setStatus(1);
-  FMS_Data::FileTransferList fileTransferList;
   ostringstream fileName;
-  fileName <<"machine_1:/tmp/test-";
+  fileName <<machineId;
+  fileName <<":/tmp/test-";
   fileName <<cpt;
   fileName << "-";
   vishnuInitialize((char*) dietClientConfigPath.c_str(), 0, NULL);
 
-  if (type == 1) {
+  if (type == 1) { // list file transfers
+  
+  FMS_Data::LsTransferOptions lsFileTransferOptions;
+  lsFileTransferOptions.setStatus(1);
+    FMS_Data::FileTransferList fileTransferList;
+    
     for (int i=0;i<5;i++) {
       cout << " In loop : " <<  i << endl;
     listFileTransfers(sessionKey,fileTransferList,lsFileTransferOptions);
     }
     std::cout << "5 iterations done \n";
   }
-  else {
+  else if (type ==2){ // create an remove file
+  
     for (int i=0;i<5;i++) {
       cout << " In loop : " <<  i << endl;
 
@@ -41,24 +45,52 @@ void stress(int cpt,const string& sessionKey,int type){
 
     }
     std::cout << "5 iterations done \n";
+
+  } else if (type ==3){ //copy
+    
+      for (int i=0;i<5;i++) {
+      cout << " In loop : " <<  i << endl;
+
+      fileName <<i;
+      // create adn remove file
+
+      std::string path(fileName.str());
+      copyFile(sessionKey,dietClientConfigPath,path);
+
+    }
+    std::cout << "5 iterations done \n";
+
+  
+  }
+  else if (type ==4){// getFilesInfo
+  
+    FileStat fileInfos; 
+      for (int i=0;i<5;i++) {
+      cout << " In loop : " <<  i << endl;
+      getFilesInfo(sessionKey,machineId+":/proc",fileInfos);
+
+    }
+    std::cout << "5 iterations done \n";
+
+  
   }
   vishnuFinalize();
 }
 
 
-void forker(int cpt,const string& sessionKey, int type){
+void forker(int cpt,const string& sessionKey,const std::string& machineId, int type){
   cpt--;
 
   switch(fork()){
     case 0 :
-      stress( cpt,sessionKey,type) ;
+      stress( cpt,sessionKey,machineId,type) ;
       break;
     case -1 :
       cout << " Fork number " << cpt << " failed " << endl;
       break;
     default :
       if(cpt>0)
-        forker(cpt,sessionKey, type);
+        forker(cpt,sessionKey,machineId,type);
       break;
   }
   return;
@@ -70,8 +102,9 @@ int main(int argc, char** argv){
   int i;
   int type;
   string key  = ""       ;
-  string pwd  = "vishnu_user"  ;
-  string uid  = "root";
+  string pwd  = "toto"  ;
+  string uid  = "user_1";
+  string machineId  = "MA_2";
   ConnectOptions cop  ;
   Session sess ;
 
@@ -96,12 +129,12 @@ int main(int argc, char** argv){
        return 1;
     }
     //To check the type value
-    if ((type != 1) && (type != 2)) {
+    if ((type != 1) && (type != 2) && (type != 3) && (type != 4)) {
        std::cerr << "The value of the type is incorrect" << std::endl;
        return 1;
     }
     std::cout << "sess.getSessionKey()=" << sess.getSessionKey() << std::endl;
-    forker(cpt, sess.getSessionKey(),type);
+    forker(cpt, sess.getSessionKey(),machineId,type);
   }
   catch(VishnuException& e){
     std::cout << e.what() << std::endl;
