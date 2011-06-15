7dd6585a0342e6010a61507811d07a40fa86f2ff
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-06-15 11:19:37 +0200

    creation fichier de maj sql pour db existante

---
+++ core/database/database_update.sql
@@ -0,0 +1,15 @@
+-- This script is for update of the VISHNU database content
+-- Script name          : database_update.sql
+-- Script owner         : SysFera SAS
+
+-- REVISIONS
+-- Revision nb          : 1.0
+-- Revision date        : 22/02/11
+-- Revision author      : Benjamin Isnard <benjamin.isnard@sysfera.com>
+-- Revision comment     : initial version for upgrade from release 1.1.0 to 1.2.0
+
+-- Creates VISHNU record
+UPDATE vishnu SET updatefreq=100 WHERE vishnuid='1';
+
+-- Creates required user options
+INSERT INTO optionu (optionid, description, defaultvalue) VALUES (3, 'VISHNU_TRANSFER_CMD', 0);
