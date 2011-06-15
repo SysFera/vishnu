324b4b6216108561f73ea127ecc0578f7324a16d
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-06-15 09:43:17 +0200

    removed signal handler and add debug in HM

--- IMS/src/server/controller/HM/HM.cpp
+++ IMS/src/server/controller/HM/HM.cpp
@@ -3,6 +3,7 @@
 #include "unistd.h"
 
 #include "utilServer.hpp"
+#include <fstream>
 
 HM::HM(string mail):mms(UserServer(SessionServer("")), mail){
 
@@ -21,18 +22,20 @@ HM::run(){
 
   IMS_Data::IMS_DataFactory_ptr ecoreFactory = IMS_Data::IMS_DataFactory::_instance();
   IMS_Data::ListMetric_ptr li = ecoreFactory->createListMetric();
-
+  std::ofstream of("HM.log");
   while(true){
     try {
       freq = mms.checkUpFreq();
 //       sleep(freq);
-      sleep(10);
+      unsigned int t = sleep(10);
+      of << t << std::endl;
       li = mms.getCurMet();
       mms.addMetricSet(li, mid);
     } catch (VishnuException &e) {
       // DO nothing to keep on trying to log
     }
   }
+  of.close();
 }
 
 
