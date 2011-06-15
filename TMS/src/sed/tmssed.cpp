c873284038899424cf7acd21b6351bf624a4c72d
kcoulomb <kevin.coulomb@sysfera.com>
2011-06-15 11:31:21 +0200

    Add unregister and do not sow error in case of problem unregistering

--- TMS/src/sed/tmssed.cpp
+++ TMS/src/sed/tmssed.cpp
@@ -117,6 +117,7 @@ int main(int argc, char* argv[], char* envp[]) {
       if (!res) {
         diet_print_service_table();
         res = diet_SeD(dietConfigFile.c_str(), argc, argv);
+        unregisterSeD(TMSTYPE);
       } else {
         std::cerr << "There was a problem during services initialization" << std::endl;
         exit(1);
