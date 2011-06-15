8f8dd1d528581ccb045cc99fa3006c79abde23e3
Benjamin Isnard <benjamin.isnard@sysfera.com>
2011-06-15 09:50:42 +0200

    removed HM debug

--- IMS/src/server/controller/HM/HM.cpp
+++ IMS/src/server/controller/HM/HM.cpp
@@ -3,7 +3,6 @@
 #include "unistd.h"
 
 #include "utilServer.hpp"
-#include <fstream>
 
 HM::HM(string mail):mms(UserServer(SessionServer("")), mail){
 
@@ -22,20 +21,17 @@ HM::run(){
 
   IMS_Data::IMS_DataFactory_ptr ecoreFactory = IMS_Data::IMS_DataFactory::_instance();
   IMS_Data::ListMetric_ptr li = ecoreFactory->createListMetric();
-  std::ofstream of("HM.log");
+
   while(true){
     try {
       freq = mms.checkUpFreq();
-//       sleep(freq);
-      unsigned int t = sleep(10);
-      of << t << std::endl;
+      sleep(freq);
       li = mms.getCurMet();
       mms.addMetricSet(li, mid);
     } catch (VishnuException &e) {
       // DO nothing to keep on trying to log
     }
   }
-  of.close();
 }
 
 
