77e47efe368e5a1d6db216f1084223d80065ab3b
Daouda Traore <daouda.traore@sysfera.com>
2011-07-06 13:43:34 +0200

    factorisation cli UMS

--- UMS/src/cli/list_sessions.cpp
+++ UMS/src/cli/list_sessions.cpp
@@ -106,7 +106,7 @@ int main (int ac, char* av[]){
            fMachineId);
 
   CLICmd cmd = CLICmd (ac, av, opt);
-
+  /*
   try {
     opt->parse_cli(ac,av);
   } catch(po::error& e){ // catch all other bad parameter errors
@@ -129,7 +129,11 @@ int main (int ac, char* av[]){
   if ( opt->count("help")){
     helpUsage(*opt,"[option]");
     return 0;
-  }
+  }*/
+
+  bool isEmpty;
+  //To process list options
+  GenericCli().processListOpt(opt, cmd, isEmpty, ac, av);
 
 
   /********  Process **************************/
