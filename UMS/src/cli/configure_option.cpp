07479cc6dec60ac499642f3bb4c33c9a3eca9e19
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-07-06 10:26:22 +0200

    Factorisation de UMS::configure_options/configure_default_option

--- UMS/src/cli/configure_option.cpp
+++ UMS/src/cli/configure_option.cpp
@@ -10,62 +10,10 @@ using namespace vishnu;
 int main (int ac, char* av[]){
 
 
-
   boost::shared_ptr<Options> opt(new Options(av[0])) ;
 
-// {{RELAX<CODEREDUCER> The error handling is the same in all command
+  return  commonConfigure(opt,  ac,  av,&configureOption);
 
-  try {
-
-    commonConfigure(opt,  ac,  av,&configureOption);
-      
-    printSuccessMessage();
-
-
-  }
-
-  catch(po::required_option& e){// a required parameter is missing
-
-    usage(*opt,"[options] optionName value ",requiredParamMsg);
-
-    return CLI_ERROR_MISSING_PARAMETER;
-  }
-
-  catch(po::error& e){ // catch all other bad parameter errors
-
-    errorUsage(av[0], e.what());
-
-    return CLI_ERROR_INVALID_PARAMETER;
-  }
-
-
-  catch(VishnuException& e){// catch all Vishnu runtime error
-
-    std::string  msg = e.getMsg()+" ["+e.getMsgComp()+"]";
-
-    errorUsage(av[0], msg,EXECERROR);
-
-    //check the bad session key
-    
-    if (checkBadSessionKeyError(e)){
-
-      removeBadSessionKeyFromFile(getppid());
-    }
-
-
-    return e.getMsgI() ;
-  }
-
-  catch(std::exception& e){// catch all std runtime error
-
-    errorUsage(av[0], e.what());
-
-    return CLI_ERROR_RUNTIME;
-  }
-
-  return 0;
-
-  // }}RELAX<CODEREDUCER>
 
 }// end of main
 
