2c9f20078b78a96193052c51ea48f4491ae0151a
Daouda Traore <daouda.traore@sysfera.com>
2011-07-06 10:25:56 +0200

    factorisation cli UMS

--- UMS/src/cli/reset_password.cpp
+++ UMS/src/cli/reset_password.cpp
@@ -10,17 +10,31 @@
 #include "sessionUtils.hpp"
 #include "cliUtil.hpp"
 #include "utils.hpp"
+#include "GenericCli.hpp"
+
 namespace po = boost::program_options;
 
 using namespace std;
 using namespace vishnu;
 
+struct ResetPassWordFunc {
+
+  std::string muserId;
+  std::string mtmpPassword;
+
+  ResetPassWordFunc(std::string userId, std::string tmpPassword):
+    muserId(userId),mtmpPassword(tmpPassword)
+  {};
+
+  int operator()(std::string sessionKey) {
+   return resetPassword(sessionKey,muserId, mtmpPassword);
+  }
+};
+
 int main (int ac, char* av[]){
 
 
   string userId;
-
-  string sessionKey;
 
   string dietConfig;
 
@@ -30,96 +44,25 @@ int main (int ac, char* av[]){
 
   opt->setPosition("userId",-1);
 
+  CLICmd cmd = CLICmd (ac, av, opt);
 
-  try {
+  // Parse the cli and setting the options found
+  int ret = cmd.parse(env_name_mapper());
 
-    /**************  Parse to retrieve option values  ********************/
-
-    opt->parse_cli(ac,av);
-
-    opt->parse_env(env_name_mapper());
-
-    opt->notify();
-
-    /********  Process **************************/
-
-    checkVishnuConfig(*opt);
-
-    /************** Call UMS reset Password service *******************************/
-
-
-    // initializing DIET
-    if (vishnuInitialize(const_cast<char*>(dietConfig.c_str()), ac, av)) {
-
-      errorUsage(av[0],dietErrorMsg,EXECERROR);
-
-      return  CLI_ERROR_DIET ;
-
-
-    }
-
-
-    // get the sessionKey
-
-    sessionKey=getLastSessionKey(getppid());
-
-    if(false==sessionKey.empty()){
-
-      printSessionKeyMessage();
-
-      std::string tmpPassword;
-
-      resetPassword(sessionKey,userId, tmpPassword);
-
-      printSuccessMessage();
-    }
-
-
-
-  }// End of try bloc
-
-
-  catch(po::required_option& e){// a required parameter is missing
-
-    usage(*opt,"[options] userId ",requiredParamMsg);
-
-    return CLI_ERROR_MISSING_PARAMETER;
+  if (ret != CLI_SUCCESS){
+    helpUsage(*opt,"[options] userId");
+    return ret;
   }
 
-  catch(po::error& e){ // catch all other bad parameter errors
-
-    errorUsage(av[0], e.what());
-
-    return CLI_ERROR_INVALID_PARAMETER;
+  // PreProcess (adapt some parameters if necessary)
+  checkVishnuConfig(*opt);
+  if ( opt->count("help")){
+    helpUsage(*opt,"[options] userId");
+    return 0;
   }
-
-  catch(VishnuException& e){// catch all Vishnu runtime error
-
-
-    std::string  msg = e.getMsg()+" ["+e.getMsgComp()+"]";
-
-    errorUsage(av[0], msg,EXECERROR);
-    
-    //check the bad session key
-    
-    if (checkBadSessionKeyError(e)){
-
-      removeBadSessionKeyFromFile(getppid());
-    }
-
-    return e.getMsgI() ;
-
-  }
-
-  catch(std::exception& e){// catch all std runtime error
-
-    errorUsage(av[0], e.what());
-
-    return CLI_ERROR_RUNTIME;
-
-  }
-
-  return 0;
+  std::string tmpPassword;
+  ResetPassWordFunc resetPassWordFunc(userId, tmpPassword);
+  return GenericCli().run(resetPassWordFunc, dietConfig, ac, av);
 
 }// end of main
 
