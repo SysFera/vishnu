79a87d8e4050fab6ee5a24acd34a4c5f0b808d26
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-07-07 10:12:25 +0200

    MAj FMS: Factorisation commandes de transfer de fichiers

--- FMS/src/cli/move_file.cpp
+++ FMS/src/cli/move_file.cpp
@@ -25,64 +25,20 @@ using namespace vishnu;
 
 
 
-
 int main (int argc, char* argv[]){
   
-  int ret; // Return value
 
   /******* Parsed value containers ****************/
   string dietConfig;
   string src;
   string dest;
-  string trCmdStr;
-  int trCmd;
 
    /********** EMF data ************/
   FMS_Data::CpFileOptions cpFileOptions;
 
-  /**************** Describe options *************/
-  boost::shared_ptr<Options> opt(makeTransferCommandOptions(argv[0], dietConfig, trCmdStr, src, dest));
-
-/*
-  CLICmd cmd = CLICmd (argc, argv, opt);
-
- // Parse the cli and setting the options found
-  ret = cmd.parse(FMS_env_name_mapper());
-
-  if(trCmdStr.size()!=0) {
-    size_t pos = trCmdStr.find_first_not_of("0123456789");
-    if(pos!=std::string::npos) {
-      if(trCmdStr.compare("scp")==0 || trCmdStr.compare("SCP")==0){
-        trCmd = 0;
-      } else if(trCmdStr.compare("rsync")==0 || trCmdStr.compare("RSYNC")==0){
-        trCmd = 1;
-      } else {
-        std::cerr << "Unknown file transfer command type " << trCmdStr << std::endl;
-        return 0;
-      }
-    } else {
-      trCmd = convertToInt(trCmdStr);
-    }
-    cpFileOptions.setTrCommand(trCmd);
-  }
- */
   
-   ret=copyParseOptions ( opt, argc, argv,cpFileOptions );
-
-
-  if (ret != CLI_SUCCESS){
-    helpUsage(*opt,"[options] src dest");
-    return ret;
-  }
-
-  // PreProcess (adapt some parameters if necessary)
+   copyParseOptions ( argc, argv,dietConfig,src,dest,cpFileOptions,MV);
   
-  checkVishnuConfig(*opt);  
-  
-  if ( opt->count("help")){
-    helpUsage(*opt,"[options] src dest");
-    return 0;
-  }
 
   TransferSyncFunc<MV> apiFunc(src, dest, cpFileOptions);
  
