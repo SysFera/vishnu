9756ced5c5f4fb6e61b3f011b3610bf93ccae18d
Daouda Traore <daouda.traore@sysfera.com>
2011-07-07 15:43:36 +0200

    factorisation cli IMS et maj cli TMS

--- IMS/src/cli/stop.cpp
+++ IMS/src/cli/stop.cpp
@@ -14,9 +14,24 @@
 #include "sessionUtils.hpp"
 #include <boost/bind.hpp>
 
+#include "GenericCli.hpp"
 
 using namespace std;
 using namespace vishnu;
+
+struct StopFunc {
+
+  IMS_Data::Process mprocess;
+
+  StopFunc(IMS_Data::Process process):
+   mprocess(process)
+  {};
+
+  int operator()(std::string sessionKey) {
+    return stop(sessionKey, mprocess);
+  }
+};
+
 
 boost::shared_ptr<Options>
 makeStopOpt(string pgName,
@@ -43,11 +58,8 @@ makeStopOpt(string pgName,
 
 int main (int argc, char* argv[]){
 
-  int ret; // Return value
-
   /******* Parsed value containers ****************/
   string dietConfig;
-  string sessionKey;
   string machineId;
   string processName;
 
@@ -56,11 +68,6 @@ int main (int argc, char* argv[]){
 
   /******** Callback functions ******************/
   boost::function1<void, string> fDietId (boost::bind(&IMS_Data::Process::setDietId,boost::ref(process),_1));
-
-  /*********** Out parameters *********************/
-
-  /**************** Describe options *************/
-
 
   /**************** Describe options *************/
   boost::shared_ptr<Options> opt=makeStopOpt(argv[0], fDietId, dietConfig);
@@ -80,56 +87,16 @@ int main (int argc, char* argv[]){
 
   opt->setPosition("machineId",1);
 
-
-  CLICmd cmd = CLICmd (argc, argv, opt);
-
-  // Parse the cli and setting the options found
-  ret = cmd.parse(env_name_mapper());
-
   process.setMachineId(machineId);
   process.setProcessName(processName);
 
-  if (ret != CLI_SUCCESS){
-    helpUsage(*opt,"[options] processName machineId");
-    return ret;
-  }
+  bool isEmpty;
+  //To process list options
+  GenericCli().processListOpt(opt, isEmpty, argc, argv, "processName machineId");
 
-  // PreProcess (adapt some parameters if necessary)
-  checkVishnuConfig(*opt);
-  if ( opt->count("help")){
-    helpUsage(*opt,"[options] processName machineId");
-    return 0;
-  }
+  //call of the api function
+  StopFunc stopFunc(process);
+  return GenericCli().run(stopFunc, dietConfig, argc, argv);
 
-  // Process command
-  try {
-    // initializing DIET
-    if (vishnuInitialize(const_cast<char*>(dietConfig.c_str()), argc, argv)) {
-      errorUsage(argv[0],dietErrorMsg,EXECERROR);
-      return  CLI_ERROR_DIET ;
-    }
-
-    // get the sessionKey
-    sessionKey=getLastSessionKey(getppid());
-
-    // DIET call : get job output
-    if(false==sessionKey.empty()){
-      printSessionKeyMessage();
-      stop(sessionKey, process);
-    }
-  } catch(VishnuException& e){// catch all Vishnu runtime error
-    std::string  msg = e.getMsg()+" ["+e.getMsgComp()+"]";
-    errorUsage(argv[0], msg,EXECERROR);
-    //check the bad session key
-    if (checkBadSessionKeyError(e)){
-      removeBadSessionKeyFromFile(getppid());
-    }
-    return e.getMsgI() ;
-  } catch(std::exception& e){// catch all std runtime error
-    errorUsage(argv[0],e.what());
-    return CLI_ERROR_RUNTIME;
-  }
-
-  return 0;
 }
 
