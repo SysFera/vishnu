c7f089405b4e0fb78a7bb79beac1128eb85d44b7
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2011-07-07 15:59:51 +0200

    Factorisation FMS cli:services de display

--- FMS/src/cli/create_dir.cpp
+++ FMS/src/cli/create_dir.cpp
@@ -26,23 +26,25 @@ struct CreateDirFunc {
 
   std::string mpath;
 
-CreateDirFunc(const std::string& path):mpath(path){}
+  CreateDirFunc(const std::string& path):mpath(path){}
 
   int operator()(std::string sessionKey) {
-    
+
     int res = createDir(sessionKey,mpath);
-    
+
     return res;
   }
 };
+
+
 int main (int argc, char* argv[]){
-  
+
   /******* Parsed value containers ****************/
   string dietConfig;
   string path;
 
   //buid options and parse 
-  
+
   ParseRemoteCommandOptions (argc,argv,dietConfig,path);
   CreateDirFunc apiFunc(path);
   return GenericCli().run(apiFunc, dietConfig, argc, argv);
