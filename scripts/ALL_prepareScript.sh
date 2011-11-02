56fad9a1351042697666789ad3ad95d63a2cdaf0
kcoulomb <kevin.coulomb@sysfera.com>
2011-11-02 16:04:20 +0100

    Fix bad name

--- scripts/ALL_prepareScript.sh
+++ scripts/ALL_prepareScript.sh
@@ -442,7 +442,7 @@ cd $path
 cd ..
 
 # Archive to send
-tar -czvf vishnu_v${NO_VERSION}.tgz vishnu_$NO_VERSION >/dev/null
+tar -czvf vishnu_v${NO_VERSION}.tgz VISHNU_$NO_VERSION >/dev/null
 
 # Moving archive in /tmp
 mv vishnu_v${NO_VERSION}.tgz /tmp/
