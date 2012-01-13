daa284746f9a7362ec23cb7f5af6b86ff09018cd
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2012-01-13 17:09:37 +0100

    Added tests for automatic submit job

--- TMS/test/src/TMS_fixtures.hpp
+++ TMS/test/src/TMS_fixtures.hpp
@@ -4,8 +4,9 @@
  * Author : bisnard
  */
 
-#include "UMS_fixtures.hpp"
+//#include "UMS_fixtures.hpp"
 #include "TMS_testconfig.h"
+#include "TMS_fixtures_init.hpp"
 #include <boost/test/unit_test.hpp>
 #include "api_ums.hpp"
 #include "api_tms.hpp"
@@ -13,49 +14,7 @@ using namespace std;
 using namespace vishnu;
 
 char TMSSeD[] = "tmssed";
-char ConfigTMSSeD[] = TMSSEDCONF;
+char ConfigTMSSeD[] = TMSSEDCONF2;
 char BinDirTMSSeD[] = TMSSEDBINDIR;
 
-class TMSFixture : public UMSSeDFixture {
-
-public:
-  TMSFixture():mac(2){
-
-    BOOST_TEST_MESSAGE( "== Test setup [BEGIN]: Initializing client ==" );
-    // Name of the test executable
-    mav[0]= (char*)"./tms_automTest";
-    // Client configuration file
-    string dietClientConfigPath = CONFIG_DIR + string("/client_testing.cfg");
-    mav[1]= (char*) dietClientConfigPath.c_str();
-
-    if (vishnu::vishnuInitialize(mav[1], mac, mav)) {
-      BOOST_TEST_MESSAGE( "Error in VishnuInitialize..." );
-    }
-    BOOST_TEST_MESSAGE( "== Test setup [END]: Initializing client ==" );
-
-    BOOST_TEST_MESSAGE( "== Test setup [BEGIN]: Initializing database ==" );
-    string sqlPath = TMSSQLPATH;
-    if (restore(sqlPath + "/cleanall.sql") != 0) {
-      cout << "Clean database failed" << endl;
-      return;
-    }
-    if (restore(sqlPath + "/TMSinitTest.sql")!=0) {
-      cout << "Database initialization failed" << endl;
-      return;
-    }
-    BOOST_TEST_MESSAGE( "== Test setup [END]: Initializing database ==" );
-
-  }
-
-  ~TMSFixture() {
-    BOOST_TEST_MESSAGE( "== Test teardown [BEGIN]: Finalizing client ==" );
-    vishnu::vishnuFinalize();
-    BOOST_TEST_MESSAGE( "== Test teardown [END]: Finalizing client ==" );
-  }
-
-//private:
-  int mac;
-  char* mav[2];
-};
-
-typedef DietSeDFixture<TMSSeD, BinDirTMSSeD, ConfigTMSSeD, TMSFixture> TMSSeDFixture;
+typedef DietSeDFixture<TMSSeD, BinDirTMSSeD, ConfigTMSSeD, TMSSeDFixtureInit> TMSSeDFixture;
---
+++ TMS/test/src/TMS_fixtures_init.hpp
@@ -0,0 +1,61 @@
+/**
+ * TMS_fixtures.hpp
+ *
+ * Author : bisnard
+ */
+
+#include "UMS_fixtures.hpp"
+#include "TMS_testconfig.h"
+#include <boost/test/unit_test.hpp>
+#include "api_ums.hpp"
+#include "api_tms.hpp"
+using namespace std;
+using namespace vishnu;
+
+char TMSSeDInit[] = "tmssed";
+char ConfigTMSSeDInit[] = TMSSEDCONF;
+char BinDirTMSSeDInit[] = TMSSEDBINDIR;
+
+class TMSFixtureInit : public UMSSeDFixture {
+
+public:
+  TMSFixtureInit():mac(2){
+
+    BOOST_TEST_MESSAGE( "== Test setup [BEGIN]: Initializing client ==" );
+    // Name of the test executable
+    mav[0]= (char*)"./tms_automTest";
+    // Client configuration file
+    string dietClientConfigPath = CONFIG_DIR + string("/client_testing.cfg");
+    mav[1]= (char*) dietClientConfigPath.c_str();
+
+    if (vishnu::vishnuInitialize(mav[1], mac, mav)) {
+      BOOST_TEST_MESSAGE( "Error in VishnuInitialize..." );
+    }
+    BOOST_TEST_MESSAGE( "== Test setup [END]: Initializing client ==" );
+
+    BOOST_TEST_MESSAGE( "== Test setup [BEGIN]: Initializing database ==" );
+    string sqlPath = TMSSQLPATH;
+    if (restore(sqlPath + "/cleanall.sql") != 0) {
+      cout << "Clean database failed" << endl;
+      return;
+    }
+    if (restore(sqlPath + "/TMSinitTest.sql")!=0) {
+      cout << "Database initialization failed" << endl;
+      return;
+    }
+    BOOST_TEST_MESSAGE( "== Test setup [END]: Initializing database ==" );
+
+  }
+
+  ~TMSFixtureInit() {
+    BOOST_TEST_MESSAGE( "== Test teardown [BEGIN]: Finalizing client ==" );
+    vishnu::vishnuFinalize();
+    BOOST_TEST_MESSAGE( "== Test teardown [END]: Finalizing client ==" );
+  }
+
+//private:
+  int mac;
+  char* mav[2];
+};
+
+typedef DietSeDFixture<TMSSeDInit, BinDirTMSSeDInit, ConfigTMSSeDInit, TMSFixtureInit> TMSSeDFixtureInit;
