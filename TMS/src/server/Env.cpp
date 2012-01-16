2020eb82b3152aa45d4e2fd987c99c92d92eacaf
Daouda Traore <daouda.traore@sysfera.com>
2012-01-16 18:20:41 +0100

    updates TMS enhancement

--- TMS/src/server/Env.cpp
+++ TMS/src/server/Env.cpp
@@ -64,30 +64,42 @@ void Env::replaceEnvVariables(std::string& scriptContent) {
     case TORQUE:
 
       //To replace VISHNU_BATCHJOB_ID
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_ID", "PBS_JOBID");
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_ID", "$PBS_JOBID");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_ID}", "$PBS_JOBID");
       //To replace VISHNU_BATCHJOB_NAME
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NAME", "PBS_JOBNAME");
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NAME", "$PBS_JOBNAME");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NAME}", "$PBS_JOBNAME");
       //To replace VISHNU_BATCHJOB_NODEFILE
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NODEFILE", "PBS_NODEFILE");
-      //To replace VISHNU_BATCHJOB_NODEFILE
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NODEFILE", "PBS_NODEFILE");
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NODEFILE", "$PBS_NODEFILE");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NODEFILE}", "$PBS_NODEFILE");
       //To replace VISHNU_BATCHJOB_NUM_NODES
       getTorqueNumNodes(scriptContent);
 
       break;
 
     case LOADLEVELER:
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_ID", "LOADL_STEP_ID"); 
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NAME", "LOADL_JOB_NAME"); 
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NODEFILE", "LOADL_HOSTFILE"); 
+      //To replace VISHNU_BATCHJOB_ID
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_ID", "$LOADL_STEP_ID"); 
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_ID}", "$LOADL_STEP_ID"); 
+      //To replace VISHNU_BATCHJOB_NAME
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NAME", "$LOADL_JOB_NAME"); 
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NAME}", "$LOADL_JOB_NAME"); 
+      //To replace VISHNU_BATCHJOB_NODEFILE
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NODEFILE", "$LOADL_HOSTFILE"); 
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NODEFILE}", "$LOADL_HOSTFILE"); 
       break;
     case SLURM:
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_ID", "SLURM_JOB_ID");
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NAME", "SLURM_JOB_NAME");
-      replaceAllOccurences(scriptContent, "VISHNU_BATCHJOB_NUM_NODES", "SLURM_JOB_NUM_NODES");
-      //replaceAllOccurences(scriptContent, "VISHNU_JOB_CPUS_PER_NODE", "SLURM_JOB_CPUS_PER_NODE");
+      //To replace VISHNU_BATCHJOB_ID
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_ID", "$SLURM_JOB_ID");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_ID}", "$SLURM_JOB_ID");
+      //To replace VISHNU_BATCHJOB_NAME
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NAME", "$SLURM_JOB_NAME");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NAME}", "$SLURM_JOB_NAME");
+      //To replace SLURM_JOB_NUM_NODES
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NUM_NODES", "$SLURM_JOB_NUM_NODES");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NUM_NODES}", "$SLURM_JOB_NUM_NODES");
       
-      pos = scriptContent.find("VISHNU_BATCHJOB_NODEFILE");
+      pos = scriptContent.find("$VISHNU_BATCHJOB_NODEFILE");
       if(pos!=std::string::npos) {
         std::string fileName = std::string(getenv("HOME"))+"/NODELIST_XXXXXX";
         vishnu::createTmpFile(const_cast<char*>(fileName.c_str()));
