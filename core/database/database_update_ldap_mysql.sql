f3086a2a7eef352a6c92d0ab48da7f42fb39258f
kcoulomb <kevin.coulomb@sysfera.com>
2012-02-02 10:50:00 +0100

    Add update scripts

---
+++ core/database/database_update_ldap_mysql.sql
@@ -0,0 +1,55 @@
+-- This script is for update of the VISHNU database content
+-- Script name          : database_update_ldap_mysql.sql
+-- Script owner         : SysFera SAS
+
+-- REVISIONS
+-- Revision nb          : 1.2.7
+-- Revision date        : 02/02/12
+-- Revision author      : Kevin Coulomb <kevin.coulomb@sysfera.com>
+-- Revision comment     : initial version for upgrade from release 1.2.6 to 1.2.7
+
+CREATE TABLE authaccount (
+  authaccountid SERIAL NOT NULL,
+  authsystem_authsystemid INTEGER UNSIGNED  NOT NULL  ,
+  users_numuserid INTEGER UNSIGNED  NOT NULL  ,
+  aclogin VARCHAR(255)  ,
+PRIMARY KEY(authaccountid),
+  FOREIGN KEY(users_numuserid)
+    REFERENCES users(numuserid),
+  FOREIGN KEY(authsystem_authsystemid)
+    REFERENCES authsystem(authsystemid)
+);
+
+CREATE TABLE ldapauthsystem (
+  ldapauthsystid SERIAL NOT NULL,
+  authsystem_authsystemid INTEGER UNSIGNED  NOT NULL  ,
+  ldapbase VARCHAR(255)  ,
+PRIMARY KEY(ldapauthsystid),
+  FOREIGN KEY(authsystem_authsystemid)
+    REFERENCES authsystem(authsystemid)
+);
+
+CREATE TABLE authsystem (
+  authsystemid SERIAL NOT NULL,
+  vishnu_vishnuid INTEGER UNSIGNED  NOT NULL  ,
+  name VARCHAR(255)  ,
+  uri VARCHAR(255)  ,
+  authlogin VARCHAR(255)  ,
+  authpassword VARCHAR(255)  ,
+  userpwdencryption INTEGER UNSIGNED  ,
+  types INTEGER UNSIGNED  ,
+  state INTEGER UNSIGNED  ,
+PRIMARY KEY(authsystemid),
+  FOREIGN KEY(vishnu_vishnuid)
+     REFERENCES vishnu(vishnuid)
+);    
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON authaccount TO "vishnu_db_admin";
+GRANT SELECT, INSERT, UPDATE, DELETE ON authsystem TO "vishnu_db_admin";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ldapauthsystem TO "vishnu_db_admin";
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON authaccount TO "vishnu_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON authsystem TO "vishnu_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ldapauthsystem TO "vishnu_user";
+
+ALTER TABLE vsession ADD COLUMN authid VARCHAR(255);
---
+++ core/database/database_update_ldap_postgre.sql
@@ -0,0 +1,58 @@
+-- This script is for update of the VISHNU database content
+-- Script name          : database_update_ldap_postgre.sql
+-- Script owner         : SysFera SAS
+
+-- REVISIONS
+-- Revision nb          : 1.2.7
+-- Revision date        : 02/02/12
+-- Revision author      : Kevin Coulomb <kevin.coulomb@sysfera.com>
+-- Revision comment     : initial version for upgrade from release 1.2.6 to 1.2.7
+
+
+CREATE TABLE authsystem (
+  authsystemid SERIAL  NOT NULL ,
+  vishnu_vishnuid INTEGER   NOT NULL ,
+  name VARCHAR(255)    ,
+  uri VARCHAR(255)    ,
+  authlogin VARCHAR(255)    ,
+  authpassword VARCHAR(255)    ,
+  userpwdencryption INTEGER    ,
+  types INTEGER    ,
+  state INTEGER      ,
+PRIMARY KEY(authsystemid)  ,
+  FOREIGN KEY(vishnu_vishnuid)
+    REFERENCES vishnu(vishnuid));
+
+CREATE TABLE authaccount (
+  authaccountid SERIAL  NOT NULL ,
+  authsystem_authsystemid INTEGER   NOT NULL ,
+  users_numuserid INTEGER   NOT NULL ,
+  aclogin VARCHAR(255)      ,
+PRIMARY KEY(authaccountid)    ,
+  FOREIGN KEY(users_numuserid)
+    REFERENCES users(numuserid),
+  FOREIGN KEY(authsystem_authsystemid)
+    REFERENCES authsystem(authsystemid));
+
+CREATE TABLE ldapauthsystem (
+  ldapauthsystid SERIAL  NOT NULL ,
+  authsystem_authsystemid INTEGER   NOT NULL ,
+  ldapbase VARCHAR(255)      ,
+PRIMARY KEY(ldapauthsystid)  ,
+  FOREIGN KEY(authsystem_authsystemid)
+    REFERENCES authsystem(authsystemid));
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON authaccount TO "vishnu_db_admin";
+GRANT SELECT, INSERT, UPDATE, DELETE ON authsystem TO "vishnu_db_admin";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ldapauthsystem TO "vishnu_db_admin";
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON authaccount TO "vishnu_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON authsystem TO "vishnu_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ldapauthsystem TO "vishnu_user";
+
+GRANT ALL ON SEQUENCE authaccount TO "vishnu_user";
+GRANT ALL ON SEQUENCE authsystem TO "vishnu_user";
+GRANT ALL ON SEQUENCE ldapauthsystem TO "vishnu_user";
+
+ALTER TABLE vsession ADD COLUMN authid VARCHAR(255);
+
