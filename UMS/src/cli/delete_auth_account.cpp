915b8ec0923aa5062e805dd3fdb732b2428071c0
Ibrahima Cisse <ibrahima.cisse@sysfera.com>
2012-02-09 14:27:08 +0100

    update authentification service CLI options

--- UMS/src/cli/delete_auth_account.cpp
+++ UMS/src/cli/delete_auth_account.cpp
@@ -82,14 +82,14 @@ int main (int ac, char* av[]){
   int ret = cmd.parse(env_name_mapper());
 
   if (ret != CLI_SUCCESS){
-    helpUsage(*opt,"authSystemId");
+    helpUsage(*opt," [options] authSystemId");
     return ret;
   }
 
   // PreProcess (adapt some parameters if necessary)
   checkVishnuConfig(*opt);
   if ( opt->count("help")){
-    helpUsage(*opt,"authSystemId");
+    helpUsage(*opt,"[options] authSystemId");
     return 0;
   }
 
