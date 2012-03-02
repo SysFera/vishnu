ff1f5669e3b57581855fa0f359fab28d867769ad
dtraore <sysfera@lsf.(none)>
2012-03-02 11:03:22 +0100

    updated TMS/src/server/Env.cpp

--- TMS/src/server/Env.cpp
+++ TMS/src/server/Env.cpp
@@ -99,6 +99,30 @@ void Env::replaceEnvVariables(std::string& scriptContent) {
       }
 
      break;
+    case LSF:
+      //To replace VISHNU_BATCHJOB_ID
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_ID", "$LSB_JOBID");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_ID}", "$LSB_JOBID");
+      //To replace VISHNU_BATCHJOB_NAME
+      replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NAME", "$LSB_JOB_NAME");
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NAME}", "$LSB_JOB_NAME");
+      //To replace VISHNU_BATCHJOB_NODEFILE
+      replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NODEFILE}", "$VISHNU_BATCHJOB_NODEFILE");      
+      pos = scriptContent.find("$VISHNU_BATCHJOB_NODEFILE");
+      if(pos!=std::string::npos) {
+        std::string fileName = std::string(getenv("HOME"))+"/LSF_NODELIST_XXXXXX";
+        vishnu::createTmpFile(const_cast<char*>(fileName.c_str()));
+        pos = scriptContent.rfind("\n", pos-1);
+        scriptContent.insert(pos+1, "echo $LSB_HOSTS > "+fileName+"\n");
+        std::string tmp = "echo $LSB_HOSTS > "+fileName+"\n";
+        scriptContent.insert(pos+1+tmp.size(), "sed -i 's/ /\\n/g' "+fileName+"\n");
+        replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NODEFILE", fileName);
+        replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NODEFILE}", fileName);
+        //To replace VISHNU_BATCHJOB_NUM_NODES
+        replaceAllOccurences(scriptContent, "$VISHNU_BATCHJOB_NUM_NODES", "$(cat "+fileName+" | sort | uniq | wc -l)");
+        replaceAllOccurences(scriptContent, "${VISHNU_BATCHJOB_NUM_NODES}", "$(cat "+fileName+" | sort | uniq | wc -l)"); 
+        //scriptContent.insert(scriptContent.size()-1, "\n rm "+fileName+"\n");
+      }
     default:
       break;
 
