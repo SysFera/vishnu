47d64e75a17d85505b4c3e74be0f75d2c5c12867
kcoulomb <kevin.coulomb@sysfera.com>
2012-03-22 10:56:52 +0100

    Update adminman and add a script to generate pdf and html automatically

---
+++ core/doc/adminmanual/docbook/update_files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# description : Script to generate pdf and html from docbook source file
+# author      : kevin coulomb (kevin.coulomb@sysfera.com)
+# date        : 22/03/2012
+
+
+# Génération du pdf
+dblatex -s ../../../tools/docbook/dblatex/docbook-sysfera.sty adminman-gen.docbook
+
+# Génération de l'html
+xsltproc --param ignore.image.scaling 1 ../../../tools/docbook/docbook-xsl/xhtml/docbook.xsl adminman-gen.docbook > adminman-gen.html
