486b54ffd9c75bfbcb100728592c60f3a9bca637
kcoulomb <kevin.coulomb@sysfera.com>
2012-04-05 09:16:00 +0200

    Fix mac compilation, now can use libcrypto instead of libcrypt

--- Cmake/FindLIBCRYPT.cmake
+++ Cmake/FindLIBCRYPT.cmake
@@ -1,9 +1,9 @@
 #
-# Try to find the TORQUE installation
+# Try to find libcrypt 
 #
 
 find_library(LIBCRYPT_LIB
-  crypt 
+  NAMES crypt crypto
   paths
 	${LIBCRYPT}/lib
         $ENV{LIBCRYPT}/lib
@@ -15,4 +15,4 @@ if (LIBCRYPT_LIB)
   set(LIBCRYPT_FOUND "Yes")
   mark_as_advanced(LIBCRYPT_DIR)
   mark_as_advanced(LIBCRYPT_LIB)
-endif(LIBCRYPT_LIB)
+endif()
