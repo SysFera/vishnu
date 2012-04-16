2e089565534a3f0be8188ec542be4e911caf97a5
kcoulomb <kevin.coulomb@sysfera.com>
2012-04-16 11:26:55 +0200

    Update script to regenerate english and french version

--- core/doc/adminmanual/docbook/update_files.sh
+++ core/doc/adminmanual/docbook/update_files.sh
@@ -5,8 +5,14 @@
 # date        : 22/03/2012
 
 
-# Génération du pdf
+# Génération du pdf fr
 dblatex -s ../../../tools/docbook/dblatex/docbook-sysfera.sty adminman-gen.docbook
 
-# Génération de l'html
+# Génération de l'html fr
 xsltproc --param ignore.image.scaling 1 ../../../tools/docbook/docbook-xsl/xhtml/docbook.xsl adminman-gen.docbook > adminman-gen.html
+
+# Génération du pdf eng
+dblatex -s ../../../tools/docbook/dblatex/docbook-sysfera.sty adminman_eng-gen.docbook
+
+# Génération de l'html eng
+xsltproc --param ignore.image.scaling 1 ../../../tools/docbook/docbook-xsl/xhtml/docbook.xsl adminman_eng-gen.docbook > adminman_eng-gen.html
