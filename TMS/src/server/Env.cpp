4e2116c802bf4c27026eaf5132e5dd5345c17f00
rchakode <rodrigue.chakode@sysfera.com>
2012-05-02 16:35:28 +0200

    code clean

--- TMS/src/server/Env.cpp
+++ TMS/src/server/Env.cpp
@@ -157,7 +157,7 @@ void Env::setParams(std::string& scriptContent, const std::string & params) {
 	size_t pos, pos1, pos2 ;
 
 	pos1 = 0;
-	while(pos2 = params.find(" "), pos2 != std::string::npos) {
+	while(pos2 = params.find(" ", pos1), pos2 != std::string::npos) {
 		param = params.substr(pos1, pos2) ;
 		pos = param.find("=");
 		if(pos != std::string::npos ) {
@@ -167,7 +167,6 @@ void Env::setParams(std::string& scriptContent, const std::string & params) {
 			replaceAllOccurences(scriptContent, "${" + paramName + "}", paramValue) ;
 		}
 		pos1 = pos2 + 1 ;
-		pos2 = params.find(" ", pos1) ;
 	}
 	param = params.substr(pos1, std::string::npos) ; //last token
 	if(pos = param.find("="), pos != std::string::npos ) {
@@ -188,15 +187,13 @@ void Env::setParamsEnvVars(const std::string& params) {
 	size_t pos, pos1, pos2 ;
 
 	pos1 = 0;
-	pos2 = params.find(" ") ;
-	while(pos2 != std::string::npos) {
+	while(pos2 = params.find(" ", pos1), pos2 != std::string::npos) {
 		param = params.substr(pos1, pos2) ;
 		pos = param.find("=");
 		if(pos != std::string::npos ) {
 			setenv(param.substr(0, pos).c_str(), param.substr(pos+1, std::string::npos).c_str(), 1) ;
 		}
 		pos1 = pos2 + 1 ;
-		pos2 = params.find(" ", pos1) ;
 	}
 	param = params.substr(pos1, std::string::npos) ; //last token
 	pos = param.find("=");
