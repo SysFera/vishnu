9fc9e094c1318c398c1c422c657d445c437ed8bd
Haikel Guemar <haikel.guemar@sysfera.com>
2012-05-07 13:50:23 +0200

    zmq: fix FindZMQ.cmake

--- Cmake/FindZMQ.cmake
+++ Cmake/FindZMQ.cmake
@@ -1,31 +1,40 @@
-#
-# Try to find the SLURM installation
-#
+# Find ZeroMQ library
+
+# This module defines:
+# ZMQ_FOUND          - whether library was found
+# ZMQ_INCLUDE_DIR    - include path
+# ZMQ_LIBRARIES      - libraries to link against
+
+# get some hints from pkg-config
+if(NOT WIN32)
+  include(FindPkgConfig)
+  # run quietly since we only retrieve some hints from pkg-config
+  pkg_check_modules(ZMQ QUIET libzmq)
+  # returns ZMQ_INCLUDE_DIRS and ZMQ_LIBRARY_DIRS
+endif()
 
 find_path(ZMQ_INCLUDE_DIR
-  zmq.hpp
-  paths
-	${ZMQ_DIR}/include
-	$ENV{ZMQ_DIR}/include
-	/usr/include
-	/usr/local/include
-	/opt/local/include
+  NAMES zmq.h zmq.hpp
+  PATHS
+  ${ZMQ_DIR}/include
+  $ENV{ZMQ_DIR}/include
+  /usr/local/include
+  /opt/local/include
+  ${ZMQ_INCLUDE_DIRS}
 )
 
 
-find_library(ZMQ_LIB
-  zmq
-  paths
-	${ZMQ_DIR}/lib
+find_library(ZMQ_LIBRARIES
+  NAMES zmq
+  PATHS
+  ${ZMQ_DIR}/lib
   $ENV{ZMQ_DIR}/lib
-	/usr/lib
-	/usr/local/lib
+  /usr/local/lib
   /opt/local/lib
+  ${ZMQ_LIBRARY_DIRS}
 )
 
-if (ZMQ_INCLUDE_DIR AND  ZMQ_LIB)
-  set(ZMQ_FOUND "Yes")
-  mark_as_advanced(ZMQ_INCLUDE_DIR)
-  mark_as_advanced(ZMQ_LIB)
-endif(ZMQ_INCLUDE_DIR AND ZMQ_LIB)
-
+# defines our variables
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ZMQ DEFAULT_MSG
+  ZMQ_LIBRARIES ZMQ_INCLUDE_DIR)
