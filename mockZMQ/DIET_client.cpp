71f4cc93557e0be02d39a625628116624e85129a
kcoulomb <kevin.coulomb@sysfera.com>
2012-05-22 11:50:55 +0200

    Use of FMS and IMS possible, ums on port 5555 and fms on port 5556

--- mockZMQ/DIET_client.cpp
+++ mockZMQ/DIET_client.cpp
@@ -46,15 +46,116 @@ diet_string_set(diet_arg_t* arg, char* value, int pers){
   return 0;
 }
 
+bool
+isUMS(std::string test){
+  return (
+    test.compare("sessionConnect") == 0 ||
+    test.compare("sessionReconnect") == 0 ||
+    test.compare("sessionClose") == 0 ||
+    test.compare("userCreate") == 0 ||
+    test.compare("userUpdate") == 0 ||
+    test.compare("userDelete") == 0 ||
+    test.compare("userPasswordChange") == 0 ||
+    test.compare("userPasswordReset") == 0 ||
+    test.compare("machineCreate") == 0 ||
+    test.compare("machineUpdate") == 0 ||
+    test.compare("machineDelete") == 0 ||
+    test.compare("localAccountCreate") == 0 ||
+    test.compare("localAccountUpdate") == 0 ||
+    test.compare("localAccountDelete") == 0 ||
+    test.compare("configurationSave") == 0 ||
+    test.compare("configurationRestore") == 0 ||
+    test.compare("optionValueSet") == 0 ||
+    test.compare("optionValueSetDefault") == 0 ||
+    test.compare("sessionList") == 0 ||
+    test.compare("localAccountList") == 0 ||
+    test.compare("machineList") == 0 ||
+    test.compare("commandList") == 0 ||
+    test.compare("optionValueList") == 0 ||
+    test.compare("userList") == 0 ||
+    test.compare("restore") == 0 ||
+    test.compare("authSystemCreate") == 0 ||
+    test.compare("authSystemUpdate") == 0 ||
+    test.compare("authSystemDelete") == 0 ||
+    test.compare("authSystemList") == 0 ||
+    test.compare("authAccountCreate") == 0 ||
+    test.compare("authAccountUpdate") == 0 ||
+    test.compare("authAccountDelete") == 0 ||
+    test.compare("authAccountList") == 0);
+}
+
+
+
 int
 diet_call(diet_profile_t* prof){
+
+  if (isUMS(std::string(prof->name))) {
+    diet_call_gen(prof, 5555);
+//  }
+//  else if (isFMS(std::string(prof->name))) {
+//    diet_call_gen(prof, 5556);
+  } else {
+    diet_call_gen(prof, 5556);
+//    std::cerr << "Unknown service" << std::endl;
+  }
+
+
+
+//  try {
+
+//  zmq::context_t ctx(1);
+//  zmq::socket_t sock(ctx, ZMQ_REQ);
+////  std::string address;
+////  address = std::string(prof->address);
+////  address += ":";
+////  address += vishnu::convertToString<int>(prof->port);
+//  sock.connect("tcp://localhost:5555");
+////  std::cerr << "send: \"" << my_serialize(prof).c_str() << std::endl;
+//  std::string s1 = my_serialize(prof);
+//  zmq::message_t request(s1.length()+1);
+//  memcpy((void*)request.data(), s1.c_str(), s1.length()+1);
+//  sock.send(request);
+//
+//  zmq::message_t reply;
+//  std::cout << "b4 recv " << std::endl;
+//  sock.recv(&reply);
+//  std::cout << "a4 recv " << std::endl;
+////  std::cout << "Client receive : " << (char*)reply.data() << std::endl;
+//
+//  boost::shared_ptr<diet_profile_t>
+//    tmp(my_deserialize(static_cast<char *>(reply.data())));
+//  std::cout << "a5 recv " << std::endl;
+////  *prof =
+//  prof->IN = tmp->IN;
+//  prof->OUT = tmp->OUT;
+//  prof->INOUT = tmp->INOUT;
+//  for(int i=0;i<=prof->OUT;++i){
+//    prof->param[i] = strdup(tmp->param[i]);
+//  }
+//  std::cout << "error:  " << prof->param[prof->OUT] << std::endl;
+//  if (strcmp(prof->param[prof->OUT], "")!=0){
+//    std::cout << "a7 recv " << std::endl;
+//    diet_call_fms(prof);
+//  }
+//
+//  std::cout << "a6 recv " << std::endl;
+//  } catch (...){
+//    std::cout << "Failed -> in exception " << std::endl;
+//    diet_call_fms(prof);
+//  }
+
+  return 0;
+}
+
+int
+diet_call_gen(diet_profile_t* prof, int my_port){
   zmq::context_t ctx(1);
   zmq::socket_t sock(ctx, ZMQ_REQ);
 //  std::string address;
 //  address = std::string(prof->address);
 //  address += ":";
-//  address += vishnu::convertToString<int>(prof->port);
-  sock.connect("tcp://localhost:5555");
+  std::string toto  = vishnu::convertToString<int>(my_port);
+  sock.connect(std::string("tcp://localhost:"+toto).c_str());
 //  std::cerr << "send: \"" << my_serialize(prof).c_str() << std::endl;
   std::string s1 = my_serialize(prof);
   zmq::message_t request(s1.length()+1);
@@ -77,6 +178,7 @@ diet_call(diet_profile_t* prof){
 
   return 0;
 }
+
 
 int
 diet_string_get(diet_arg_t* arg, char** value, void* ptr){
