a8391fbb1d19b3f0a8d6c2972622c84b6911a3ca
amine.bsila <amine.bsila@sysfera.com>
2012-07-10 10:25:04 +0200

    add tests for outputdir

--- TMS/test/src/tms_automTest3.cpp
+++ TMS/test/src/tms_automTest3.cpp
@@ -88,7 +88,7 @@ BOOST_AUTO_TEST_CASE(get_job_output_normal_call)
 
     BOOST_CHECK_EQUAL(getJobOutput(sessionKey,machineId, jobInfo.getJobId(), outputInfos, TMSWORKINGDIR),0  );
 
-    bool pathExist=bfs::exists(bfs::path(outputInfos.getOutputPath())) &&  bfs::exists(bfs::path(outputInfos.getOutputPath()));
+    bool pathExist=bfs::exists(bfs::path(outputInfos.getOutputPath())) &&  bfs::exists(bfs::path(outputInfos.getOutputPath())) ;
 
     BOOST_CHECK( pathExist );
 
@@ -103,6 +103,81 @@ BOOST_AUTO_TEST_CASE(get_job_output_normal_call)
   }
 }
 
+//------------------------------------------------------------------------------------------------------------------------
+
+//  get jobs output: normal call using $VISHNU_OUTPUTDIR in the script
+
+BOOST_AUTO_TEST_CASE(get_job_output_normal_call_with_outputdir)
+{
+  
+  BOOST_TEST_MESSAGE("Testing normal execution of the get jobs output function corresponding to use case T2.6 with the output directory" );
+  
+  
+  VishnuConnexion vc("root","vishnu_user");
+  
+  // get the session key and the machine identifier
+  
+  string sessionKey=vc.getConnexion();
+  
+  string machineId="machine_1";
+  
+  try {
+    //Setting submitjob parameters
+    
+    const std::string scriptFilePath=TMSFASTSCRIPTPATHOUTDIR;
+    Job jobInfo;
+    SubmitOptions options;
+    
+    BOOST_REQUIRE(submitJob(sessionKey, machineId, scriptFilePath, jobInfo,options)==0  );
+    
+    BOOST_TEST_MESSAGE("************ The job identifier is " << jobInfo.getJobId() );
+    
+    // wait a few seconds and check the success of cancelling job
+    Job job;
+    
+    getJobInfo(sessionKey, machineId, jobInfo.getJobId(), job);
+    
+    // ensure that the job is terminated
+    
+    while (5!=job.getStatus()){
+      
+      bpt::seconds sleepTime(5);
+      boost::this_thread::sleep(sleepTime);
+      
+      getJobInfo(sessionKey, machineId, jobInfo.getJobId(), job);
+    }
+    
+    // Check the success of get jobs output function
+    JobResult outputInfos;
+    
+    BOOST_CHECK_EQUAL(getJobOutput(sessionKey,machineId, jobInfo.getJobId(), outputInfos, TMSWORKINGDIR),0  );
+    
+    bool pathExist=bfs::exists(bfs::path(outputInfos.getOutputPath())) &&  bfs::exists(bfs::path(outputInfos.getOutputPath())) && bfs::is_directory(bfs::path(outputInfos.getOutputDir()));
+    
+    
+    BOOST_CHECK( pathExist );
+    std::string filepath =  outputInfos.getOutputDir()+"/TMS_res";
+    bool fileexist = bfs::exists(bfs::path(filepath)) && (bfs::path(filepath).parent_path().string().compare(bfs::path(outputInfos.getOutputDir()).string())== 0);
+
+    BOOST_CHECK( fileexist );
+    
+    bool correctpath = (bfs::path(outputInfos.getOutputPath()).parent_path().string().compare(bfs::path(outputInfos.getOutputPath()).parent_path().string()) == 0);
+    correctpath = correctpath && (bfs::path(outputInfos.getOutputPath()).parent_path().string().compare(outputInfos.getOutputDir())== 0);
+    
+    BOOST_CHECK( correctpath );
+    
+    if (pathExist){
+      bfs::remove (bfs::path(outputInfos.getOutputPath()));
+      bfs::remove (bfs::path(outputInfos.getErrorPath()));
+      bfs::remove (bfs::path(filepath));
+      bfs::remove_all(bfs::path(outputInfos.getOutputDir()));
+    }
+    BOOST_TEST_MESSAGE("*********************** get jobs output with outputdir: normal call ok!!!!*****************************");
+  } catch (VishnuException& e) {
+    BOOST_MESSAGE(e.what());
+    BOOST_CHECK(false);
+  }
+}
 
 //------------------------------------------------------------------------------------------------------------------------
 
