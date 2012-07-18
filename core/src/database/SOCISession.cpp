8c32576920d2336cfe9352a1337337a216316da5
Jean-Baptiste Ghoul <jean-baptiste.ghoul@sysfera.com>
2012-07-18 16:50:04 +0200

    suppression des services template process et getResult

--- core/src/database/SOCISession.cpp
+++ core/src/database/SOCISession.cpp
@@ -62,21 +62,6 @@ temporary_type SOCISession::operator<<(std::string const & query)
 	return ret;
 }
 
-void SOCISession::process(std::string request)
-{
-	std::vector<std::vector<std::string> > results;
-	std::vector<std::string> attributeNames;
-
-	try
-	{
-		(msession->once) << request;
-	} catch (std::exception const & e)
-	{
-		throw SystemException(ERRCODE_DBERR,
-				std::string("Cannot get query result : \n") + e.what());
-	}
-	return;
-}
 
 
 
