2db122444f06f5f6fd6fe75758f6bc062d043e04
Jean-Baptiste Ghoul <jean-baptiste.ghoul@sysfera.com>
2012-07-23 12:17:39 +0200

    compilation with ORACLE and SQLITE3 support

--- Cmake/FindSOCI.cmake
+++ Cmake/FindSOCI.cmake
@@ -4,17 +4,19 @@
 
 SET( SOCI_FOUND_STRING "Whether a SOCI installation was found." )
 
-set(SOCI_DBMS_LIB_NAME)
-set(SOCI_DBMS_DIR)
 set(SOCI_INCLUDE_NAMES soci.h soci-backend.h)
 if(ENABLE_MYSQL)
-  set(SOCI_DBMS_DIR mysql)
-  set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} ${SOCI_DMBS_DIR}/soci-mysql.h)
+  set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} mysql/soci-mysql.h)
 endif(ENABLE_MYSQL)
 if(ENABLE_POSTGRESQL)
-    set(SOCI_DBMS_DIR postgresql)
-    set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} ${SOCI_DMBS_DIR}/soci-postgresql.h)
+    set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} postgresql/soci-postgresql.h)
 endif(ENABLE_POSTGRESQL)
+if(ENABLE_ORACLE)
+    set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} oracle/soci-oracle.h)
+endif(ENABLE_ORACLE)
+if(ENABLE_SQLITE3)
+    set(SOCI_INCLUDE_NAMES ${SOCI_INCLUDE_NAMES} sqlite3/soci-sqlite3.h)
+endif(ENABLE_SQLITE3)
 
 
 find_path(SOCI_INCLUDE_DIR
@@ -75,6 +77,37 @@ if(ENABLE_POSTGRESQL)
 	set(SOCI_LIBS ${SOCI_LIBS} ${SOCI_POSTGRESQL_LIB})
 endif(ENABLE_POSTGRESQL)
 
+if(ENABLE_ORACLE)
+	find_library(SOCI_ORACLE_LIB
+	  NAMES soci_oracle 
+	  PATHS
+	  ${SOCI_DIR}
+	  ${SOCI_DIR}/lib
+	  $ENV{SOCI_DIR}/lib
+	  /usr/local/lib64
+	  /usr/lib
+	  /usr/lib64
+	  /usr/local/lib
+	  PATH_SUFFIXES soci
+	)
+	set(SOCI_LIBS ${SOCI_LIBS} ${SOCI_ORACLE_LIB})
+endif(ENABLE_ORACLE)
+
+if(ENABLE_SQLITE3)
+	find_library(SOCI_SQLITE3_LIB
+	  NAMES soci_sqlite3
+	  PATHS
+	  ${SOCI_DIR}
+	  ${SOCI_DIR}/lib
+	  $ENV{SOCI_DIR}/lib
+	  /usr/local/lib64
+	  /usr/lib
+	  /usr/lib64
+	  /usr/local/lib
+	  PATH_SUFFIXES soci
+	)
+	set(SOCI_LIBS ${SOCI_LIBS} ${SOCI_SQLITE3_LIB})
+endif(ENABLE_SQLITE3)
 
 if (SOCI_INCLUDE_DIR)
   set(SOCI_FOUND TRUE CACHE BOOL ${SOCI_FOUND_STRING} FORCE)
