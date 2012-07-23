c5948b336b235d027394e6ca63acac0553289be2
Haikel Guemar <haikel.guemar@sysfera.com>
2012-07-23 15:17:02 +0200

    TMS: fix Batch Schedulers plugins loading

--- TMS/src/server/BatchFactory.cpp
+++ TMS/src/server/BatchFactory.cpp
@@ -8,6 +8,7 @@
 #include "BatchFactory.hpp"
 #include "SharedLibrary.hh"
 
+#include <boost/format.hpp>
 #include <iostream>
 
 static int created=0;
@@ -20,6 +21,25 @@ BatchFactory::BatchFactory() {
 }
 
 
+BatchServer*
+loadPluginBatch(const char *name) {
+  dadi::SharedLibrary *plugin(NULL);
+  void *factory(NULL);
+  BatchServer *instance(NULL);
+
+  std::string libname = boost::str(boost::format("%1%%2%%3%")
+                                   % dadi::SharedLibrary::prefix()
+                                   % name
+                                   % dadi::SharedLibrary::suffix());
+
+  plugin = new dadi::SharedLibrary(libname);
+  if (plugin->isLoaded()) {
+    factory = plugin->symbol("create_plugin_instance");
+    ((int (*)(void **))(factory))((void**) &instance);
+  }
+
+}
+
 
 
 /**
@@ -29,46 +49,19 @@ BatchFactory::BatchFactory() {
  */
 BatchServer*
 BatchFactory::getBatchServerInstance() {
-  void *factory(NULL);
   BatchServer *instance(NULL);
-  dadi::SharedLibrary *plugin(NULL);
 #ifdef HAVE_TORQUE_2_3
-      plugin = new dadi::SharedLibrary("tms-server-torque2.3");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+    instance = loadPluginBatch("vishnu-tms-torque2.3");
 #elif HAVE_LOADLEVELER_2_5
-      plugin = new dadi::SharedLibrary("tms-server-loadleveler2.5");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+    instance = loadPluginBatch("vishnu-tms-loadleveler2.5");
 #elif HAVE_SLURM_2_2
-      plugin = new dadi::SharedLibrary("tms-server-slurm2.2");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+      instance = loadPluginBatch("vishnu-tms-slurm2.2");
 #elif HAVE_SLURM_2_3
-      plugin = new dadi::SharedLibrary("tms-server-slurm2.3");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+      instance = loadPluginBatch("vishnu-tms-slurm2.3");
 #elif HAVE_LSF_7_0
-      plugin = new dadi::SharedLibrary("tms-server-lsf7.0");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+      instance = loadPluginBatch("vishnu-tms-lsf7.0");
 #elif HAVE_SGE_11
-    case SGE:
-      plugin = new dadi::SharedLibrary("tms-server-sge11");
-      if (plugin->isLoaded()) {
-        factory = plugin->symbol("create_plugin_instance");
-        ((int (*)(void **))(factory))((void**) &instance);
-      }
+      instance = loadPluginBatch("vishnu-tms-sge11");
 #endif
 
   return static_cast<BatchServer *>(instance);
