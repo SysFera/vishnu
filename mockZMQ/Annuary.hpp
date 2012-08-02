1ed4fc13ca97516d05ae88c9e1ad2c4e69ec367e
kcoulomb <kevin.coulomb@sysfera.com>
2012-08-02 11:18:14 +0200

    Add doxygen documentation

--- mockZMQ/Annuary.hpp
+++ mockZMQ/Annuary.hpp
@@ -1,3 +1,8 @@
+/**
+ * \file Annuary.hpp
+ * \brief This file defines the Annuary used to keep the servers
+ * \author Kevin Coulomb (kevin.coulomb@sysfera.com)
+ */
 #ifndef __ANNUARY__H__
 #define __ANNUARY__H__
 
@@ -6,33 +11,73 @@
 #include <vector>
 #include <boost/shared_ptr.hpp>
 
+/**
+ * \brief This class represents the annuary to store the services
+ * \class Annuary
+ */
 class Annuary {
 
 public :
+
+  /**
+   * \brief Default constructor
+   */
   Annuary();
+  /**
+   * \brief Copy constructor
+   */
   ~Annuary();
 
+  /**
+   * \brief Add a server in the annuary
+   * \param name The name of the server (e.g. UMS, TMS, namer)
+   * \param services The services offered by the server
+   * \param port The port the server is listening
+   * \param port The address the server is running
+   * \return 0 on success, an error code otherwise
+   */
   int
   add(std::string name, std::vector<std::string> services, int port, std::string address);
 
+  /**
+   * \brief Remove the server called name listening on port at address of the annuary
+   * \param name The name of the server (e.g. UMS, TMS, namer)
+   * \param port The port the server is listening
+   * \param port The address the server is running
+   * \return 0 on success, an error code otherwise
+   */
   int
   remove(std::string name, int port, std::string address);
 
+  /**
+   * \brief Return all the servers offering the service called service
+   * \param service The name of the desired service
+   * \return A vector containing all the servers offering service
+   */
   std::vector<boost::shared_ptr<Server> >*
   get(std::string service);
 
-// Init the annuary from a file
+  /**
+   * \brief Init the annuary from a file
+   * \param file The file to init from
+   */
   void
   initFromFile(std::string file);
 
 
 private :
+  /**
+   * \brief Fill the services for a given server name. Function used to easily create services from given names
+   * \param services OUT, the list of services for name
+   * \param name The name of the server
+   */
   void
   fillServices(std::vector<std::string> & services, std::string name);
 
+  /**
+   * \param The servers
+   */
   std::vector<boost::shared_ptr<Server> > mservers;
-  int pos;
-
-};
+}; // End class annuary
 
 #endif // __ANNUARY__H__
--- mockZMQ/DIET_client.h
+++ mockZMQ/DIET_client.h
@@ -12,97 +12,210 @@
 #include <string>
 #include <boost/shared_ptr.hpp>
 
+/**
+ * \brief Overload of DIET internal structure
+ */
 typedef struct toto {
   char* id;
 } toto;
 
+/**
+ * \brief Overload of DIET structure
+ */
 typedef struct diet_arg_t {
+  /**
+   * \brief Overload of DIET param, position of the argument
+   */
   int pos;
+  /**
+   * \brief Overload of DIET param, profile
+   */
   void* prof;
+  /**
+   * \brief Overload of DIET param, description of the argument
+   */
   toto desc;
 } diet_arg_t;
 
+/**
+ * \brief Overload of DIET structure
+ */
 typedef struct diet_profile_t {
+  /**
+   * \brief Overload of DIET param, array of parameters
+   */
   char ** param;
+  /**
+   * \brief Overload of DIET param, last IN param in the array
+   */
   int IN;
+  /**
+   * \brief Overload of DIET param, last INOUT param in the array
+   */
   int INOUT;
+  /**
+   * \brief Overload of DIET param, last OUT param in the array
+   */
   int OUT;
+  /**
+   * \brief Overload of DIET param, name of the service
+   */
   char* name;
+  /**
+   * \brief Overload of DIET param
+   */
   diet_arg_t* parameters;
 } diet_profile_t;
 
 
+/**
+ * \brief Overload of DIET structure
+ */
 typedef struct diet_container_t {
+  /**
+   * \brief Overload of DIET param, size of the container
+   */
   int size;
+  /**
+   * \brief Overload of DIET param, id of elements in the container
+   */
   char* elt_ids[2];
 } diet_container_t;
 
 
+/**
+ * \brief Overload of DIET constant
+ */
 #define DIET_VOLATILE 1
+/**
+ * \brief Overload of DIET constant
+ */
 #define DIET_PERSISTENT_RETURN 1
 
+/**
+ * \brief Overload of DIET function, allocate the profile of a service
+ * \param name The name of the service
+ * \param IN number of IN parameters
+ * \param INOUT number of IN+INOUT parameters
+ * \param OUT number of IN+INOUT+OUT parameters
+ * \return The allocated profile
+ */
 diet_profile_t*
 diet_profile_alloc(const char* name, int IN, int INOUT, int OUT);
 
+/**
+ * \brief Overload of DIET function, set a param value in a profile to a string(char *)
+ * \param arg Structure containing the string param to set and its position
+ * \param value The value to set
+ * \param pers The persistency of the data (DEPRECATED, kept for compatibility)
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_string_set(diet_arg_t* arg, char* value, int pers);
 
+/**
+ * \brief Overload of DIET function, call to a DIET service
+ * \param prof The profile of the service to call
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_call(diet_profile_t* prof);
 
+/**
+ * \brief Generic function created to encapsulate the code
+ */
 int
 diet_call_gen(diet_profile_t* prof, int port, std::string addr);
 
+/**
+ * \brief Overload of DIET function, get the value of a string(char *) in the profile
+ * \param arg Structure containing the argument
+ * \param value OUT the value gotten
+ * \param ptr DEPRECATED, kept for compatibility
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_string_get(diet_arg_t* arg, char** value, void* ptr);
 
+/**
+ * \brief Overload of DIET function, free an allocated profile
+ * \param prof The profile to free
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_profile_free(diet_profile_t* prof);
 
+/**
+ * \brief Overload of DIET function, to get the arg in position pos in the profile prof
+ * \param prof The profile
+ * \param pos The position of the desired parameter
+ * \return Structure describing the argument
+ */
 diet_arg_t*
 diet_parameter(diet_profile_t* prof, int pos);
 
+/**
+ * \brief To know if test is a UMS service
+ * \param test name of a service
+ * \return True if test belongs to the UMS module
+ */
 bool
 isUMS(std::string test);
+/**
+ * \brief To know if test is a TMS service
+ * \param test name of a service
+ * \return True if test belongs to the TMS module
+ */
 bool
 isTMS(std::string test);
+/**
+ * \brief To know if test is a IMS service
+ * \param test name of a service
+ * \return True if test belongs to the IMS module
+ */
 bool
 isIMS(std::string test);
 
+/**
+ * \brief To deserialize a profile
+ * \param prof The serialized profile
+ * \return The deserialized profile
+ */
 boost::shared_ptr<diet_profile_t>
 my_deserialize(const std::string& prof);
 
+/**
+ * \brief To serialize a profile
+ * \param prof The profile
+ * \return The serialized profile
+ */
 std::string
 my_serialize(diet_profile_t* prof);
 
+/**
+ * \brief Overload of DIET function, to initialize
+ * \param cfg DEPRECATED, kept for compatibility
+ * \param argc DEPRECATED, kept for compatibility
+ * \param argv DEPRECATED, kept for compatibility
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_initialize(const char* cfg, int argc, char** argv);
 
+/**
+ * \brief Overload of DIET function, to finalize the call
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_finalize();
 
-void
-dagda_init_container(diet_arg_t* arg);
-
-void
-dagda_put_file(char * path, int val, char** id);
-
-void
-dagda_get_file(char* val, char** err);
-
-void
-dagda_add_container_element(char* arg, char* id, int val);
-
-void
-dagda_get_container(char* IDContainer);
-
-void
-dagda_get_container_elements(char* IDContainer, diet_container_t* content);
-
+/**
+ * \brief Overload of DIET function
+ * \param arg ?
+ * \param flag ?
+ * \return 0 on success, an error code otherwise
+ */
 int
 diet_container_set(diet_arg_t* arg, int flag);
-
-void
-dagda_delete_data(char* id);
 
 #endif // __DIETMOCK__
