688b1bbedc98a6194418032f48784cf0446beea0
kcoulomb <kevin.coulomb@sysfera.com>
2012-08-22 10:50:43 +0200

    Fix select * for process

--- IMS/src/server/data/ProcessServer.cpp
+++ IMS/src/server/data/ProcessServer.cpp
@@ -29,10 +29,10 @@ ProcessServer::list(){
     throw UMSVishnuException(ERRCODE_NO_ADMIN, "get processes is an admin function. A user cannot call it");
   }
 
-  string request = "SELECT DISTINCT * from process WHERE  pstatus ='"+convertToString(PRUNNING)+"' ";
+  string request = "SELECT DISTINCT dietname, launchscript, machineid, pstatus, uptime, vishnuname from process WHERE  pstatus ='"+convertToString(PRUNNING)+"' ";
   vector<string> results;
   vector<string>::iterator iter;
-  
+
   if (mop->getMachineId().compare("") != 0){
     string machine = "SELECT machineid from machine where machineid='"+mop->getMachineId()+"'";
     DatabaseResult *res = mdatabase->getResult(machine.c_str());
@@ -52,12 +52,12 @@ ProcessServer::list(){
       results = listOfProcess->get(i);
       iter  = results.begin();
       IMS_Data::Process_ptr proc = ecoreFactory->createProcess();
-      proc->setState(convertToInt(*(++iter)));
-      proc->setProcessName(*(++iter));
-      proc->setDietId(*(++iter));
-      proc->setMachineId(*(++iter));
-      proc->setTimestamp(convertToInt(*(++iter)));
-      proc->setScript(*(++iter));
+      proc->setState(convertToInt(*(iter+3)));
+      proc->setProcessName(*(iter+5));
+      proc->setDietId(*(iter));
+      proc->setMachineId(*(iter+2));
+      proc->setTimestamp(convertToInt(*(iter+4)));
+      proc->setScript(*(iter+1));
       mlistObject->getProcess().push_back(proc);
     }
   }catch (SystemException& e){
@@ -147,7 +147,7 @@ ProcessServer::stopAllProcesses(IMS_Data::Process_ptr proc){
 
 bool
 ProcessServer::isIMSSeD(string Pname){
-  string req = "SELECT * from process where dietname='"+Pname+"'";
+  string req = "SELECT vishnuname from process where dietname='"+Pname+"'";
   boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
   if(result->getNbTuples() == 0) {
     throw IMSVishnuException(ERRCODE_INVPROCESS, "Unknown process");
@@ -159,7 +159,7 @@ ProcessServer::isIMSSeD(string Pname){
 
 void
 ProcessServer::fillContent(IMS_Data::Process_ptr p) {
-  string req = "SELECT * from process where vishnuname='"+p->getProcessName()+"'";
+  string req = "SELECT pstatus, dietname, uptime, launchscript from process where vishnuname='"+p->getProcessName()+"'";
   req += " AND machineid='"+p->getMachineId()+"' order by uptime desc";
   boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
   if(result->getNbTuples() == 0) {
@@ -167,15 +167,15 @@ ProcessServer::fillContent(IMS_Data::Process_ptr p) {
   }
   vector<string> res;
   res = result->get(0);
-  p->setState(convertToInt(res.at(1)));
-  p->setDietId(res.at(3));
-  p->setTimestamp(convertToInt(res.at(5)));
-  p->setScript(res.at(6));
+  p->setState(convertToInt(res.at(0)));
+  p->setDietId(res.at(1));
+  p->setTimestamp(convertToInt(res.at(2)));
+  p->setScript(res.at(3));
 }
 
 void
 ProcessServer::getDataFromDietId(IMS_Data::Process_ptr p) {
-  string req = "SELECT * from process where dietname='"+p->getDietId()+"'";
+  string req = "SELECT pstatus, vishnuname, machineid, uptime, launchscript from process where dietname='"+p->getDietId()+"'";
   req += " order by uptime desc";
   boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
   if(result->getNbTuples() == 0) {
@@ -183,17 +183,17 @@ ProcessServer::getDataFromDietId(IMS_Data::Process_ptr p) {
   }
   vector<string> res;
   res = result->get(0);
-  p->setState(convertToInt(res.at(1)));
-  p->setProcessName(res.at(2));
-  p->setMachineId(res.at(4));
-  p->setTimestamp(convertToInt(res.at(5)));
-  p->setScript(res.at(6));
+  p->setState(convertToInt(res.at(0)));
+  p->setProcessName(res.at(1));
+  p->setMachineId(res.at(2));
+  p->setTimestamp(convertToInt(res.at(3)));
+  p->setScript(res.at(4));
 }
 
 
 bool
 ProcessServer::checkStopped(string machine, string type) {
-  string req = "select * from process where machineid='"+machine+"' and vishnuname='"+type+"' and (pstatus='"+convertToString(PDOWN)+"' or pstatus='"+convertToString(PRUNNING)+"')";
+  string req = "select vishnuname from process where machineid='"+machine+"' and vishnuname='"+type+"' and (pstatus='"+convertToString(PDOWN)+"' or pstatus='"+convertToString(PRUNNING)+"')";
   try {
     boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
     return(result->getNbTuples() == 0);
@@ -205,36 +205,36 @@ ProcessServer::checkStopped(string machine, string type) {
 
 void
 ProcessServer::getHost(string mid, string& hostname, string& acclog) {
-  string req = "select * from machine where machine.machineid='"+mid+"'";
+  string req = "select nummachineid, name from machine where machine.machineid='"+mid+"'";
   boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
   if(result->getNbTuples() == 0) {
     throw UMSVishnuException(ERRCODE_UNKNOWN_MACHINE, "Machine not found");
   }
   vector<string> res;
   res = result->get(0);
-  hostname = res.at(2);
+  hostname = res.at(1);
 
-  req = "select * from account, users where users.userid='root' and users.numuserid=account.users_numuserid and account.machine_nummachineid='"+res.at(0)+"'";
+  req = "select aclogin from account, users where users.userid='root' and users.numuserid=account.users_numuserid and account.machine_nummachineid='"+res.at(0)+"'";
   boost::scoped_ptr<DatabaseResult> result2(mdatabase->getResult(req.c_str()));
   if(result2->getNbTuples() == 0) {
     throw UMSVishnuException(ERRCODE_UNKNOWN_LOCAL_ACCOUNT, "No account found to restart the machine"+mid);
   }
   res = result2->get(0);
-  acclog = res.at(3);
+  acclog = res.at(0);
 }
 
 
 // Return the last actif ims server
 string
 ProcessServer::getElectedMid() {
-  string req = "select * from machine, process where machine.machineid=process.machineid and process.vishnuname='IMS' and process.pstatus='"+convertToString(PRUNNING)+"' order by uptime desc";
+  string req = "select process.machineid from machine, process where machine.machineid=process.machineid and process.vishnuname='IMS' and process.pstatus='"+convertToString(PRUNNING)+"' order by uptime desc";
   boost::scoped_ptr<DatabaseResult> result(mdatabase->getResult(req.c_str()));
   if(result->getNbTuples() == 0) {
     throw UMSVishnuException(ERRCODE_UNKNOWN_LOCAL_ACCOUNT, "No account found to restart on the machine");
   }
   vector<string> res;
   res = result->get(0);
-  return string(res.at(7));
+  return string(res.at(0));
 }
 
 
