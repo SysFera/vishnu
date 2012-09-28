bb6635e4f787cf6a9130dfc01df50fe1f9f0e451
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2012-09-28 10:22:45 +0200

    Added VISHNU_VERSION_COMMENTS

--- core/src/utils/cliUtil.cpp
+++ core/src/utils/cliUtil.cpp
@@ -2,26 +2,26 @@
 #include "vishnu_version.hpp"
 using namespace std;
 /**
- * \brief helper function to display help about a specific command 
+ * \brief helper function to display help about a specific command
  * \param opt: describes all options allowed by the command
- * \param signature: defines the usage of the command 
+ * \param signature: defines the usage of the command
  */
-void 
+void
 helpUsage (const Options& opt,const string& signature){
 
-cout << "\nVersion: " << VISHNU_VERSION << "\n";
+cout << "\nVersion: " << VISHNU_VERSION << " " << VISHNU_VERSION_COMMENTS << "\n";
   cout << "\nUsage: " << opt.getConfiguration()->getPgName()<<" " << signature <<"\n\n";
   cout <<opt<< endl;
 }
 
 
 /**
- * \brief helper function to display error about a specific command 
+ * \brief helper function to display error about a specific command
  * \param cli   :The name of the command
- * \param errMsg: The error message to display 
- * \param err   : The error type 
+ * \param errMsg: The error message to display
+ * \param err   : The error type
  */
-void 
+void
 errorUsage (const string & cli,const string& errMsg,const ErrorType& err){
 
   cerr << cli<<": "<<errMsg <<endl;
@@ -33,15 +33,15 @@ errorUsage (const string & cli,const string& errMsg,const ErrorType& err){
 
 
 /**
- * \brief Helper function to display information (error or usage) about a specific command 
+ * \brief Helper function to display information (error or usage) about a specific command
  * \param opt   :describes all options allowed by the command
- * \param mess: The help usage message to display 
+ * \param mess: The help usage message to display
  * \param eWhat   : The error message to display
  * \return 0 if an help is required or 1 if an error must me displayed
  */
 
 
-int 
+int
 usage (const Options & opt,const std::string& mess,const std::string& eWhat){
 
   int res=0;
@@ -67,7 +67,7 @@ usage (const Options & opt,const std::string& mess,const std::string& eWhat){
  */
 
 
-void 
+void
 checkVishnuConfig(const Options & opt){
   if ((opt.count("dietConfig") == 0) && (opt.count("help") == 0)) {
     throw runtime_error( "Set the VISHNU_CONFIG_FILE in your environment variable");
