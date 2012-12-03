8bb809d111a4ae6f5cfce527d748c5db6baedea0
kcoulomb <kevin.coulomb@sysfera.com>
2012-12-03 09:21:59 +0100

    Rename unit tests, chage check to require

--- mockZMQ/tests/TestDIET_client.cpp
+++ mockZMQ/tests/TestDIET_client.cpp
@@ -12,46 +12,46 @@ public:
 
   void
   testGetModuleUMS(){
-    BOOST_CHECK(get_module("sessionConnect").compare("UMS")==0);
+    BOOST_REQUIRE(get_module("sessionConnect").compare("UMS")==0);
   }
   void
   testGetModuleTMS(){
-    BOOST_CHECK(get_module("jobSubmit").compare("TMS")==0);
+    BOOST_REQUIRE(get_module("jobSubmit").compare("TMS")==0);
   }
   void
   testGetModuleFMS(){
-    BOOST_CHECK(get_module("DirRemove").compare("FMS")==0);
+    BOOST_REQUIRE(get_module("DirRemove").compare("FMS")==0);
   }
   void
   testGetModuleIMS(){
-    BOOST_CHECK(get_module("int_stop").compare("IMS")==0);
+    BOOST_REQUIRE(get_module("int_stop").compare("IMS")==0);
   }
   void
   testGetBadModule(){
-    BOOST_CHECK(get_module("bad").compare("")==0);
+    BOOST_REQUIRE(get_module("bad").compare("")==0);
   }
   void
   testDIETAlloc(){
     diet_profile_t* prof = diet_profile_alloc("alloc", 1, 2, 3);
-    BOOST_CHECK(prof->IN==1);
-    BOOST_CHECK(prof->INOUT==2);
-    BOOST_CHECK(prof->OUT==3);
-    BOOST_CHECK(std::string(prof->name).compare("alloc")==0);
+    BOOST_REQUIRE(prof->IN==1);
+    BOOST_REQUIRE(prof->INOUT==2);
+    BOOST_REQUIRE(prof->OUT==3);
+    BOOST_REQUIRE(std::string(prof->name).compare("alloc")==0);
   }
   void
   testDIET_parameter(){
     diet_profile_t* prof = diet_profile_alloc("alloc", 1, 2, 3);
     diet_arg_t* res = diet_parameter(prof, 1);
-    BOOST_CHECK(((diet_profile_t *)res->prof)->IN==1);
-    BOOST_CHECK(((diet_profile_t *)res->prof)->INOUT==2);
-    BOOST_CHECK(((diet_profile_t *)res->prof)->OUT==3);
-    BOOST_CHECK(std::string(((diet_profile_t *)res->prof)->name).compare("alloc")==0);
-    BOOST_CHECK(res->pos==1);
+    BOOST_REQUIRE(((diet_profile_t *)res->prof)->IN==1);
+    BOOST_REQUIRE(((diet_profile_t *)res->prof)->INOUT==2);
+    BOOST_REQUIRE(((diet_profile_t *)res->prof)->OUT==3);
+    BOOST_REQUIRE(std::string(((diet_profile_t *)res->prof)->name).compare("alloc")==0);
+    BOOST_REQUIRE(res->pos==1);
   }
   void
   testDIET_parameterBad(){
     diet_profile_t* prof = diet_profile_alloc("alloc", 1, 2, 3);
-    BOOST_CHECK_THROW(diet_parameter(prof, 10), SystemException);
+    BOOST_REQUIRE_THROW(diet_parameter(prof, 10), SystemException);
   }
   void
   testStringSet(){
@@ -64,7 +64,7 @@ public:
     diet_profile_t* prof = diet_profile_alloc("alloc", 1, 2, 3);
     diet_string_set(diet_parameter(prof,0), const_cast<char *>("toto"), DIET_VOLATILE);
     diet_string_get(diet_parameter(prof,0), &userId, NULL);
-    BOOST_CHECK(std::string(userId).compare("toto")==0);
+    BOOST_REQUIRE(std::string(userId).compare("toto")==0);
   }
   void
   testMySerialize(){
@@ -72,34 +72,34 @@ public:
     diet_string_set(diet_parameter(prof,0), const_cast<char *>("param1"), DIET_VOLATILE);
     diet_string_set(diet_parameter(prof,1), NULL, DIET_VOLATILE);
     std::string res = my_serialize(prof);
-    BOOST_CHECK(res.compare("alloc$$$0$$$0$$$1$$$param1$$$")==0);
+    BOOST_REQUIRE(res.compare("alloc$$$0$$$0$$$1$$$param1$$$")==0);
   }
   void
   testMySerializeNULLProfile(){
-    BOOST_CHECK_THROW(my_serialize(NULL), SystemException);
+    BOOST_REQUIRE_THROW(my_serialize(NULL), SystemException);
   }
   void
   testMyDeSerialize(){
     std::string profSer = "alloc$$$0$$$0$$$1$$$param1$$$";
     boost::shared_ptr<diet_profile_t> prof = my_deserialize(profSer);
-    BOOST_CHECK(std::string(prof.get()->name).compare("alloc")==0);
-    BOOST_CHECK(prof.get()->IN==0);
-    BOOST_CHECK(prof.get()->INOUT==0);
-    BOOST_CHECK(prof.get()->OUT==1);
-    BOOST_CHECK(std::string(prof.get()->param[0]).compare("param1")==0);
+    BOOST_REQUIRE(std::string(prof.get()->name).compare("alloc")==0);
+    BOOST_REQUIRE(prof.get()->IN==0);
+    BOOST_REQUIRE(prof.get()->INOUT==0);
+    BOOST_REQUIRE(prof.get()->OUT==1);
+    BOOST_REQUIRE(std::string(prof.get()->param[0]).compare("param1")==0);
   }
   void
   testMyDeSerializeEmpty(){
     std::string profSer = "";
-    BOOST_CHECK_THROW(my_deserialize(profSer), SystemException);
+    BOOST_REQUIRE_THROW(my_deserialize(profSer), SystemException);
   }
   void
   testInitNULL(){
-    BOOST_CHECK_THROW(diet_initialize(NULL, 0, NULL), SystemException);
+    BOOST_REQUIRE_THROW(diet_initialize(NULL, 0, NULL), SystemException);
   }
   void
   testInitInvalid(){
-    BOOST_CHECK_THROW(diet_initialize("/tmp/toto", 0, NULL), SystemException);
+    BOOST_REQUIRE_THROW(diet_initialize("/tmp/toto", 0, NULL), SystemException);
   }
 
 
--- mockZMQ/tests/TestServer.cpp
+++ mockZMQ/tests/TestServer.cpp
@@ -18,7 +18,7 @@ public:
   testFromString(){
     std::string res = mname+"$$$"+muri+"$$$"+mservices.at(0)+"$$$"+mservices.at(1);
     Server s = *(Server::fromString(res).get());
-    BOOST_CHECK(s.getName().compare(mname) == 0 &&
+    BOOST_REQUIRE(s.getName().compare(mname) == 0 &&
                 s.getURI().compare(muri) == 0 &&
                 s.getServices().size() == mservices.size());
   }
@@ -27,7 +27,7 @@ public:
   testToString(){
     Server s(mname, mservices, muri);
     std::string res = mname+"$$$"+muri+"$$$"+mservices.at(0)+"$$$"+mservices.at(1);
-    BOOST_CHECK(res.compare(s.toString()) == 0);
+    BOOST_REQUIRE(res.compare(s.toString()) == 0);
 }
 
   void
@@ -35,7 +35,7 @@ public:
     std::string str = "lapin";
     Server s(mname, mservices, muri);
     s.add(str);
-    BOOST_CHECK(s.getServices().size() == 3 &&
+    BOOST_REQUIRE(s.getServices().size() == 3 &&
                 s.hasService(str));
   }
 
@@ -44,7 +44,7 @@ public:
     std::string rem = mservices.at(1);
     Server s(mname, mservices, muri);
     s.remove(rem);
-    BOOST_CHECK(s.getServices().size() == 1 &&
+    BOOST_REQUIRE(s.getServices().size() == 1 &&
                 !s.hasService(rem));
   }
 
@@ -57,38 +57,38 @@ public:
     size = s.getServices().size();
     s.remove(rem);
     res = s.getServices().size();
-    BOOST_CHECK(res==size);
+    BOOST_REQUIRE(res==size);
   }
 
   void
   testHasService(){
     Server s(mname, mservices, muri);
-    BOOST_CHECK(s.hasService(mservices.at(0)) &&
+    BOOST_REQUIRE(s.hasService(mservices.at(0)) &&
                 s.hasService(mservices.at(1)));
   }
 
   void
   testHasServiceBad(){
     Server s(mname, mservices, muri);
-    BOOST_CHECK(!s.hasService("bad"));
+    BOOST_REQUIRE(!s.hasService("bad"));
   }
 
   void
   testGetName(){
     Server s(mname, mservices, muri);
-    BOOST_CHECK(s.getURI().compare(muri)==0);
+    BOOST_REQUIRE(s.getURI().compare(muri)==0);
   }
   void
   testGetServices(){
     Server s(mname, mservices, muri);
-    BOOST_CHECK(s.getServices().at(0).compare(mservices.at(0))==0 &&
+    BOOST_REQUIRE(s.getServices().at(0).compare(mservices.at(0))==0 &&
                 s.getServices().at(1).compare(mservices.at(1))==0);
   }
 
   void
   testGetURI(){
     Server s(mname, mservices, muri);
-    BOOST_CHECK(s.getURI().compare(muri)==0);
+    BOOST_REQUIRE(s.getURI().compare(muri)==0);
   }
 
 private:
