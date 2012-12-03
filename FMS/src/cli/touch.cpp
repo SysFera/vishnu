dc805966b4ac4c8db34146bce678ec10370103ce
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2012-12-03 17:01:37 +0100

    rename of createFile to touch

---
+++ FMS/src/cli/touch.cpp
@@ -0,0 +1,36 @@
+/**
+ * \file touch.cpp
+ * This file defines the VISHNU submit job command 
+ * \author Daouda Traore (daouda.traore@sysfera.com)
+ */
+
+
+#include "CLICmd.hpp"
+#include "utilVishnu.hpp"
+#include "cliError.hpp"
+#include "cliUtil.hpp"
+#include "api_ums.hpp"
+#include "api_fms.hpp"
+#include "sessionUtils.hpp"
+#include "cmdArgs.hpp"
+#include <boost/bind.hpp>
+#include "GenericCli.hpp"
+#include "remoteCommandUtils.hpp"
+namespace po = boost::program_options;
+
+using namespace std;
+using namespace vishnu;
+
+int main (int argc, char* argv[]){
+  
+  /******* Parsed value containers ****************/
+  string dietConfig;
+  string path;
+
+  //buid options and parse 
+  ParseRemoteCommandOptions (argc,argv,dietConfig,path);
+
+  FileActionFunc<CREATEFILE> apiFunc(path);
+  return GenericCli().run(apiFunc, dietConfig, argc, argv);
+
+}
