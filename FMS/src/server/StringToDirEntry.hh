ddcf323dcf80fdb23ef5da6fb2345cfe0fef6246
Benjamin Depardon <benjamin.depardon@sysfera.com>
2012-12-08 11:42:10 +0100

    coding standards

--- FMS/src/server/StringToDirEntry.hh
+++ FMS/src/server/StringToDirEntry.hh
@@ -17,128 +17,127 @@
 #include "FileTypes.hpp"
 
 struct DateHandler {
+  DateHandler (const std::string& rawDate):mrawDate(rawDate) {
+  }
 
-    DateHandler (const std::string& rawDate):mrawDate(rawDate) {
-    }
-   std::string getDate () const {
-      return mrawDate;
-    }
+  std::string
+  getDate () const {
+    return mrawDate;
+  }
 
-    std::string mrawDate;
+  std::string mrawDate;
 };
 
 
 class  PermsHandler {
-
-  public:
-    PermsHandler (const std::string& rawPerms):mrawPerms(rawPerms){ }
-    mode_t getPermissions () const {
-      if (mrawPerms.size() < 9){
+public:
+  PermsHandler (const std::string& rawPerms):mrawPerms(rawPerms){ }
+  mode_t getPermissions () const {
+    if (mrawPerms.size() < 9){
       throw FMSVishnuException (ERRCODE_RUNTIME_ERROR, "Can not get file permissions ");
-      }
-      std::istringstream iss (convertPerms(mrawPerms.substr(0,3))+convertPerms(mrawPerms.substr(3,3))+convertPerms(mrawPerms.substr(6,3)));
-      mode_t res;
-      iss >> std::oct >> res;
-      return res;
     }
+    std::istringstream iss (convertPerms(mrawPerms.substr(0,3))+convertPerms(mrawPerms.substr(3,3))+convertPerms(mrawPerms.substr(6,3)));
+    mode_t res;
+    iss >> std::oct >> res;
+    return res;
+  }
 
-  private:
-    std::string mrawPerms;
-    static std::string convertPerms(const std::string& perms) {
-
-      if (perms.size()!=3){
+private:
+  std::string mrawPerms;
+  static std::string convertPerms(const std::string& perms) {
+    if (perms.size() != 3) {
       throw FMSVishnuException (ERRCODE_RUNTIME_ERROR, "Can not get file permissions ");
-      }
-      int res=0;
-      BOOST_FOREACH (char right, perms){
-        res+=inOct (right);
-      }
-      std::stringstream ss ;
-      ss << res;
-      return ss.str();
     }
-    static  int inOct (char right){
-      int res=0;
-      switch (right) {
-        case 'r':
-          res=4;
-          break;
-        case 'w':
-          res=2;
-          break;
-        case 'x':
-          res=1;
-          break;
-        default:
-          res=0;
-      }
-      return res;
 
-
+    int res=0;
+    BOOST_FOREACH (char right, perms) {
+      res += inOct (right);
     }
+    std::stringstream ss ;
+    ss << res;
+    return ss.str();
+  }
+
+  static int inOct (char right) {
+    int res = 0;
+    switch (right) {
+    case 'r':
+      res=4;
+      break;
+    case 'w':
+      res=2;
+      break;
+    case 'x':
+      res=1;
+      break;
+    default:
+      res=0;
+    }
+    return res;
+  }
 };
 
 /**********************************************************************/
 
 class StringToDirEntry {
-  public:
+public:
 
-    StringToDirEntry (const std::string& rawDirEntry ):mrawDirEntry(rawDirEntry),
-                                                       mperms(""),
-                                                       mowner(""),
-                                                       mgroup(""),
-                                                       msize(0),
-                                                       mdate_time(""),
-                                                       mpath("") { }
+  StringToDirEntry (const std::string& rawDirEntry ):mrawDirEntry(rawDirEntry),
+                                                     mperms(""),
+                                                     mowner(""),
+                                                     mgroup(""),
+                                                     msize(0),
+                                                     mdate_time(""),
+                                                     mpath("") { }
 
-    void splitter() {
+  void splitter() {
+    std::istringstream iss (mrawDirEntry);
+    std::string date;
+    std::string time;
+    std::string utcOffset;
+    std::size_t nbLink;
 
-      std::istringstream iss (mrawDirEntry);
+    std::getline ((iss >> mperms >> nbLink >> mowner >> mgroup >> msize >> date >> time >> utcOffset), mpath) ;
+    mpath = mpath.substr(1);
+    mdate_time=date+ " "+ time + " " + utcOffset;
+  }
 
-      std::string date;
-      std::string time;
-      std::string  utcOffset;
-      std::size_t nbLink;
+  mode_t getPermissions(){
+    PermsHandler permsHandler(mperms.substr(1));
+    return permsHandler.getPermissions();
+  }
 
-      std::getline ((iss >> mperms >> nbLink >> mowner >> mgroup >> msize >> date >> time >> utcOffset), mpath) ;
-      mpath = mpath.substr(1);
-      mdate_time=date+ " "+ time + " " + utcOffset;
-    }
-    mode_t getPermissions(){
-      PermsHandler permsHandler(mperms.substr(1));
-      return permsHandler.getPermissions();
-    }
-    std::string getOwner(){
-      return mowner;
-    }
+  std::string getOwner(){
+    return mowner;
+  }
 
-    std::string getGroup(){
-      return mgroup;
-    }
+  std::string getGroup(){
+    return mgroup;
+  }
 
-     file_size_t getSize() {
-      return msize;
-    }
-    std::string getDateTime (){
+  file_size_t getSize() {
+    return msize;
+  }
+  std::string getDateTime (){
 
-      DateHandler dateHandler (mdate_time);
-      return dateHandler.getDate();
-    }
+    DateHandler dateHandler (mdate_time);
+    return dateHandler.getDate();
+  }
 
-    std::string getPath(){
-      return mpath;
-    }
+  std::string getPath(){
+    return mpath;
+  }
 
-  private:
-    std::string mrawDirEntry;
-    std::string mperms;
-    std::string mowner;
-    std::string mgroup;
-    file_size_t msize;
+private:
+  std::string mrawDirEntry;
+  std::string mperms;
+  std::string mowner;
+  std::string mgroup;
+  file_size_t msize;
 
 
-    std::string mdate_time;
-    std::string mpath;
+  std::string mdate_time;
+  std::string mpath;
 };
 
 /**********************************************************************/
