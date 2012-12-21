23b98f5f2f681fbd15e7133d9d1dbaa5e73e87a7
Benjamin Depardon <benjamin.depardon@sysfera.com>
2012-12-21 18:02:01 +0100

    use new diet_string_get2

--- FMS/src/sed/transferSrv.cpp
+++ FMS/src/sed/transferSrv.cpp
@@ -14,90 +14,7 @@
 #include "transferSrv.hpp"
 using namespace std;
 
-//diet_profile_desc_t* getTransferFileProfile(const std::string& serviceName){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc(serviceName.c_str(), 5, 5, 6);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 4), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 5), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 6), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//
-//
-//
-//}
 
-//diet_profile_desc_t* getTransferFileAsyncProfile(const std::string& serviceName){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc(serviceName.c_str(), 5, 5, 7);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 4), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 5), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 6), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 7), DIET_STRING, DIET_CHAR);
-//
-//
-//  return result;
-//
-//}
-//
-//diet_profile_desc_t* getTransferRemoteFileProfile(const std::string& serviceName){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc(serviceName.c_str(), 6, 6, 7);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 4), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 5), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 6), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 7), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//
-//}
-//
-//diet_profile_desc_t* getFileTransfersListProfile(){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc("FileTransfersList", 1, 1, 3);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//
-//}
-//
-//diet_profile_desc_t* getTransferRemoteFileAsyncProfile(const std::string& serviceName){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc(serviceName.c_str(), 6, 6, 8);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 4), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 5), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 6), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 7), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 8), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//
-//}
-//
 /**
  * \brief Function to solve the getListOfJobs service
  * \param pb is a structure which corresponds to the descriptor of a profile
@@ -108,16 +25,6 @@ solveGetListOfFileTransfers(diet_profile_t* pb) {
   return solveGenerique<FMS_Data::LsTransferOptions, FMS_Data::FileTransferList, ListFileTransfers >(pb);
 }
 
-//diet_profile_desc_t* getFileTransferStopProfile(){
-//
-//  diet_profile_desc_t* result = diet_profile_desc_alloc("FileTransferStop", 1, 1, 2);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//}
 
 
 /**
@@ -128,27 +35,27 @@ solveGetListOfFileTransfers(diet_profile_t* pb) {
 int
 solveFileTransferStop(diet_profile_t* pb) {
 
-  char* sessionKey = NULL;
-  char* optionsSerialized = NULL;
+  std::string sessionKey = "";
+  std::string optionsSerialized = "";
   std::string finishError ="";
   int mapperkey;
   std::string cmd = "";
   std::string errorInfo ="";
 
-  diet_string_get(diet_parameter(pb,0), &sessionKey, NULL);
-  diet_string_get(diet_parameter(pb,1), &optionsSerialized, NULL);
+  diet_string_get2(diet_parameter(pb,0), sessionKey);
+  diet_string_get2(diet_parameter(pb,1), optionsSerialized);
 
-  SessionServer sessionServer = SessionServer(std::string(sessionKey));
+  SessionServer sessionServer = SessionServer(sessionKey);
   FMS_Data::StopTransferOptions_ptr options_ptr = NULL;
 
   try {
     //MAPPER CREATION
     Mapper *mapper = MapperRegistry::getInstance()->getMapper(FMSMAPPERNAME);
     mapperkey = mapper->code("vishnu_stop_file_transfer");
-    mapper->code(std::string(optionsSerialized), mapperkey);
+    mapper->code(optionsSerialized, mapperkey);
     cmd = mapper->finalize(mapperkey);
 
-    if(!vishnu::parseEmfObject(std::string(optionsSerialized), options_ptr)) {
+    if(!vishnu::parseEmfObject(optionsSerialized, options_ptr)) {
       SystemException(ERRCODE_INVDATA, "solve_fileTransferStop: options object is not well built");
     }
 
@@ -158,7 +65,7 @@ solveFileTransferStop(diet_profile_t* pb) {
 
     fileTransferServer->stopThread(*options_ptr);
 
-    diet_string_set(diet_parameter(pb,2), const_cast<char*>(errorInfo.c_str()), DIET_VOLATILE);
+    diet_string_set(diet_parameter(pb,2), errorInfo.c_str(), DIET_VOLATILE);
     sessionServer.finish(cmd, FMS, vishnu::CMDSUCCESS);
   } catch (VishnuException& e) {
     try {
@@ -169,8 +76,7 @@ solveFileTransferStop(diet_profile_t* pb) {
     }
     e.appendMsgComp(finishError);
     errorInfo =  e.buildExceptionString();
-    diet_string_set(diet_parameter(pb,2), const_cast<char*>(errorInfo.c_str()), DIET_VOLATILE);
+    diet_string_set(diet_parameter(pb,2), errorInfo.c_str(), DIET_VOLATILE);
   }
   return 0;
 }
-
