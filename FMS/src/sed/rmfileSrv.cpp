15ee3ccd647c5629a009339792655622f5f2e26c
Benjamin Depardon <benjamin.depardon@sysfera.com>
2012-12-21 18:03:08 +0100

    use new diet_string_get2

--- FMS/src/sed/rmfileSrv.cpp
+++ FMS/src/sed/rmfileSrv.cpp
@@ -17,20 +17,7 @@
 using namespace std;
 
 
-/* DIET profile construction.
- * Use the serverHostname global variable to create the service name. */
-//diet_profile_desc_t* getRemoveFileProfile() {
-//  diet_profile_desc_t* result = diet_profile_desc_alloc("FileRemove", 4, 4, 5);
-//
-//  diet_generic_desc_set(diet_param_desc(result, 0), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 1), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 2), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 3), DIET_PARAMSTRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 4), DIET_STRING, DIET_CHAR);
-//  diet_generic_desc_set(diet_param_desc(result, 5), DIET_STRING, DIET_CHAR);
-//
-//  return result;
-//}
+
 
 /* mkdir DIET callback function. Proceed to the group change using the
  client parameters. Returns an error message if something gone wrong. */
@@ -41,13 +28,17 @@ int solveRemoveFile(diet_profile_t* profile) {
   std::string cmd = "";
   std::string errMsg = "";
   int mapperkey;
-  char* path, *user, *host, *sessionKey, *optionsSerialized= NULL;
+  std::string path = "";
+  std::string user = "";
+  std::string host = "";
+  std::string sessionKey = "";
+  std::string optionsSerialized = "";
 
-  diet_string_get(diet_parameter(profile, 0), &sessionKey, NULL);
-  diet_string_get(diet_parameter(profile, 1), &path, NULL);
-  diet_string_get(diet_parameter(profile, 2), &user, NULL);
-  diet_string_get(diet_parameter(profile, 3), &host, NULL);
-  diet_string_get(diet_parameter(profile, 4),&optionsSerialized, NULL);
+  diet_string_get2(diet_parameter(profile, 0), sessionKey);
+  diet_string_get2(diet_parameter(profile, 1), path);
+  diet_string_get2(diet_parameter(profile, 2), user);
+  diet_string_get2(diet_parameter(profile, 3), host);
+  diet_string_get2(diet_parameter(profile, 4), optionsSerialized);
 
 	localUser = user;
 	localPath = path;
@@ -57,8 +48,8 @@ int solveRemoveFile(diet_profile_t* profile) {
 	  //MAPPER CREATION
 	  Mapper *mapper = MapperRegistry::getInstance()->getMapper(FMSMAPPERNAME);
 	  mapperkey = mapper->code("vishnu_remove_file");
-	  mapper->code(std::string(host)+":"+std::string(path), mapperkey);
-	  mapper->code(std::string(optionsSerialized), mapperkey);
+	  mapper->code(host + ":" + path, mapperkey);
+	  mapper->code(optionsSerialized, mapperkey);
 	  cmd = mapper->finalize(mapperkey);
 
 	  // check the sessionKey
@@ -84,7 +75,7 @@ int solveRemoveFile(diet_profile_t* profile) {
 	  boost::scoped_ptr<File> file (ff.getFileServer(sessionServer,localPath, acLogin, userKey));
 
 	  RmFileOptions_ptr options_ptr= NULL;
-	  if(!vishnu::parseEmfObject(std::string(optionsSerialized), options_ptr )) {
+	  if(!vishnu::parseEmfObject(optionsSerialized, options_ptr )) {
 		throw SystemException(ERRCODE_INVDATA, "solve_remove_file: RmFileOptions object is not well built");
 	  }
 	  file->rm(*options_ptr);
@@ -101,6 +92,6 @@ int solveRemoveFile(diet_profile_t* profile) {
 	  errMsg = err.buildExceptionString();
 	}
 
-  diet_string_set(diet_parameter(profile, 5), const_cast<char*>(errMsg.c_str()), DIET_VOLATILE);
+  diet_string_set(diet_parameter(profile, 5), errMsg.c_str(), DIET_VOLATILE);
   return 0;
 }
