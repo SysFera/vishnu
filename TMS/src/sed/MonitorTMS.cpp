2beb85e487253577d151a43e3e2c7f9785007e38
R. Chakode <rodrigue.chakode@sysfera.com>
2013-01-14 14:02:24 +0100

    updated TMS monitor to avoid exiting when getJobState or database update raise exception for a single job

--- TMS/src/sed/MonitorTMS.cpp
+++ TMS/src/sed/MonitorTMS.cpp
@@ -1,10 +1,10 @@
 /**
-* \file MonitorTMS.cpp
-* \brief This file presents the implementation of the TMS Monitor
-* \authors Daouda Traore (daouda.traore@sysfera.com) and
-* Eugène PAMBA CAPO-CHICHI (eugene.capochichi@sysfera.com)
-* \date April
-*/
+ * \file MonitorTMS.cpp
+ * \brief This file presents the implementation of the TMS Monitor
+ * \authors Daouda Traore (daouda.traore@sysfera.com) and
+ * Eugène PAMBA CAPO-CHICHI (eugene.capochichi@sysfera.com)
+ * \date April
+ */
 
 #include <boost/scoped_ptr.hpp>
 #include "ServerTMS.hpp"
@@ -13,25 +13,27 @@
 #include "DbFactory.hpp"
 #include "utilVishnu.hpp"
 #include "MonitorTMS.hpp"
+#include <signal.h>
+#include <boost/format.hpp>
 
 /**
-* \brief Constructor, raises an exception on error
-* \param interval The interval to check the database
-*/
+ * \brief Constructor, raises an exception on error
+ * \param interval The interval to check the database
+ */
 MonitorTMS::MonitorTMS(int interval) {
-minterval = interval;
-mdatabaseVishnu = NULL;
+  minterval = interval;
+  mdatabaseVishnu = NULL;
 }
 
 /**
-* \brief Destructor
-* \fn ~MonitorTMS()
-*/
+ * \brief Destructor
+ * \fn ~MonitorTMS()
+ */
 
 MonitorTMS::~MonitorTMS() {
   if (mdatabaseVishnu != NULL) {
-    delete mdatabaseVishnu;
-  }
+      delete mdatabaseVishnu;
+    }
 }
 
 
@@ -42,7 +44,7 @@ MonitorTMS::~MonitorTMS() {
  * \param machineId The machine identifier
  * \param batchType The batch scheduler type
  * \return raises an execption
-*/
+ */
 void
 MonitorTMS::init(int vishnuId, DbConfiguration dbConfig, const std::string& machineId, const BatchType& batchType) {
 
@@ -61,8 +63,8 @@ MonitorTMS::init(int vishnuId, DbConfiguration dbConfig, const std::string& mach
     /* Checking of vishnuid on the database */
     boost::scoped_ptr<DatabaseResult> result(mdatabaseVishnu->getResult(sqlCommand.c_str()));
     if (result->getResults().size() == 0) {
-      throw SystemException(ERRCODE_DBERR, "The vishnuid is unrecognized");
-    }
+        throw SystemException(ERRCODE_DBERR, "The vishnuid is unrecognized");
+      }
   } catch (VishnuException& e) {
     exit(0);
   }
@@ -70,9 +72,9 @@ MonitorTMS::init(int vishnuId, DbConfiguration dbConfig, const std::string& mach
 }
 
 /**
-* \brief To launch the TMS Monitor
-* \return raises an exception
-*/
+ * \brief To launch the TMS Monitor
+ * \return raises an exception
+ */
 int
 MonitorTMS::run() {
 
@@ -83,45 +85,41 @@ MonitorTMS::run() {
   int state;
   std::string sqlUpdatedRequest;
   std::string sqlRequest = "SELECT jobId, batchJobId from job, vsession where vsession.numsessionid=job.vsession_numsessionid "
-                           " and status > 0 and status < 5 and submitMachineId='"+mmachineId+"'";
+      " and status > 0 and status < 5 and submitMachineId='"+mmachineId+"'";
 
-  try {
+  while(kill(getppid(), 0) == 0) {
+      try {
+        boost::scoped_ptr<DatabaseResult> result(mdatabaseVishnu->getResult(sqlRequest.c_str()));
+        if (result->getNbTuples() != 0) {
+            for (size_t i = 0; i < result->getNbTuples(); ++i) {
+                tmp.clear();
+                tmp = result->get(i);
 
-    boost::scoped_ptr<DatabaseResult> result(mdatabaseVishnu->getResult(sqlRequest.c_str()));
-    if (result->getNbTuples() != 0) {
-      for (size_t i = 0; i < result->getNbTuples(); ++i) {
-        tmp.clear();
-        tmp = result->get(i);
-
-        iter = tmp.begin();
-        jobId = *iter;
-        ++iter;
-        batchJobId = *iter;
-        BatchFactory factory;
-        boost::scoped_ptr<BatchServer> batchServer(factory.getBatchServerInstance());
-
-        state = batchServer->getJobState(batchJobId);
-        if(state!=-1) {
-          sqlUpdatedRequest = "UPDATE job SET status="+vishnu::convertToString(state)+" where jobId='"+jobId+"'";
-
-          mdatabaseVishnu->process(sqlUpdatedRequest.c_str());
-
-          if(state==5) {
-            sqlUpdatedRequest = "UPDATE job SET endDate=CURRENT_TIMESTAMP where jobId='"+jobId+"'";
-            mdatabaseVishnu->process(sqlUpdatedRequest.c_str());
+                iter = tmp.begin();
+                jobId = *iter;
+                ++iter;
+                batchJobId = *iter;
+                BatchFactory factory;
+                boost::scoped_ptr<BatchServer> batchServer(factory.getBatchServerInstance());
+                try {
+                  state = batchServer->getJobState(batchJobId);
+                  if (state!=-1) {
+                      sqlUpdatedRequest = "UPDATE job SET status="+vishnu::convertToString(state)+" where jobId='"+jobId+"'";
+                      mdatabaseVishnu->process(sqlUpdatedRequest.c_str());
+                      if (state==5) {
+                          sqlUpdatedRequest = "UPDATE job SET endDate=CURRENT_TIMESTAMP where jobId='"+jobId+"'";
+                          mdatabaseVishnu->process(sqlUpdatedRequest.c_str());
+                        }
+                    }
+                } catch (VishnuException& ex) {
+                  std::clog << boost::format("[TMS][MONITOR] ERROR: %1%\n")%ex.what();
+                }
+              }
           }
-        }
+      } catch (VishnuException& ex) {
+        std::clog << boost::format("[TMS][MONITOR] ERROR: %1%\n")%ex.what();
       }
+      sleep(minterval);
     }
-    sleep(minterval);
-
-  } catch (VishnuException& e) {
-    string errorInfo =  e.buildExceptionString();
-    if (e.getMsgI() == ERRCODE_DBERR) {
-      cerr << errorInfo << endl;
-      exit(1);
-    }
-    sleep(minterval);
-  }
   return 0;
 }
--- TMS/src/sed/tmssed.cpp
+++ TMS/src/sed/tmssed.cpp
@@ -10,7 +10,6 @@
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <sys/types.h>
-#include <signal.h>
 #include <boost/shared_ptr.hpp>
 
 #include "MachineServer.hpp"
@@ -62,8 +61,8 @@ int main(int argc, char* argv[], char* envp[]) {
   string uri;
 
   if (argc != 2) {
-    return usage(argv[0]);
-  }
+      return usage(argv[0]);
+    }
 
   // Read the configuration
   try {
@@ -74,63 +73,63 @@ int main(int argc, char* argv[], char* envp[]) {
     config.getRequiredConfigValue<std::string>(vishnu::URI, uri);
 
     if (interval < 0) {
-      throw UserException(ERRCODE_INVALID_PARAM, "The Monitor interval value is incorrect");
-    }
+        throw UserException(ERRCODE_INVALID_PARAM, "The Monitor interval value is incorrect");
+      }
     dbConfig.check();
     config.getRequiredConfigValue<std::string>(vishnu::BATCHTYPE, batchTypeStr);
     if (batchTypeStr == "TORQUE") {
 #ifndef HAVE_TORQUE_2_3
-      std::cerr << "\nError: can't initialize TORQUE batch type because "
+        std::cerr << "\nError: can't initialize TORQUE batch type because "
                 << "this server has not been compiled with TORQUE library\n\n";
-      exit(1);
+        exit(1);
 #endif
-      batchType = TORQUE;
-    } else if (batchTypeStr == "PBS") {
+        batchType = TORQUE;
+      } else if (batchTypeStr == "PBS") {
 #ifndef HAVE_PBSPRO_10_4
 
-      std::cerr << std::endl;
-      std::cerr << "Error: can't initialize PBSPRO batch type because this server has not been compiled with PBSPRO library" << std::endl;
-      std::cerr << std::endl;
-      exit(1);
+        std::cerr << std::endl;
+        std::cerr << "Error: can't initialize PBSPRO batch type because this server has not been compiled with PBSPRO library" << std::endl;
+        std::cerr << std::endl;
+        exit(1);
 #endif
-      batchType = PBSPRO;
-    } else if (batchTypeStr == "LOADLEVELER") {
+        batchType = PBSPRO;
+      } else if (batchTypeStr == "LOADLEVELER") {
 #ifndef HAVE_LOADLEVELER_2_5
-      std::cerr << "\nError: can't initialize LOADLEVELER batch type because "
+        std::cerr << "\nError: can't initialize LOADLEVELER batch type because "
                 << "this server has not been compiled with LOADLEVELER library\n\n";
-      exit(1);
+        exit(1);
 #endif
-      batchType = LOADLEVELER;
-    } else if (batchTypeStr == "SLURM") {
+        batchType = LOADLEVELER;
+      } else if (batchTypeStr == "SLURM") {
 #if !( HAVE_SLURM_2_2 || HAVE_SLURM_2_3)
-      std::cerr << "\nError: can't initialize SLURM batch type because "
+        std::cerr << "\nError: can't initialize SLURM batch type because "
                 << "this server has not been compiled with SLURM library\n\n";
-      exit(1);
+        exit(1);
 #endif
-      batchType = SLURM;
+        batchType = SLURM;
 
-    } else if (batchTypeStr == "LSF") {
+      } else if (batchTypeStr == "LSF") {
 #ifndef HAVE_LSF_7_0
-      std::cerr << "\nError: can't initialize LSF batch type because "
+        std::cerr << "\nError: can't initialize LSF batch type because "
                 << "this server has not been compiled with LSF library\n\n";
-      exit(1);
+        exit(1);
 #endif
-      batchType = LSF;
-    } else if (batchTypeStr == "SGE") {
+        batchType = LSF;
+      } else if (batchTypeStr == "SGE") {
 #ifndef HAVE_SGE_11
-      std::cerr << "\nError: can't initialize SGE batch type because "
+        std::cerr << "\nError: can't initialize SGE batch type because "
                 << "this server has not been compiled with SGE library\n\n";
-      exit(1);
+        exit(1);
 #endif
-      batchType = SGE;
-    } else {
-      std::cerr << "\nError: invalid value for batch type parameter (must be 'TORQUE' or 'LOADLEVELER' or 'SLURM' or 'LSF' or 'SGE')\n\n";
-      exit(1);
-    }
+        batchType = SGE;
+      } else {
+        std::cerr << "\nError: invalid value for batch type parameter (must be 'TORQUE' or 'LOADLEVELER' or 'SLURM' or 'LSF' or 'SGE')\n\n";
+        exit(1);
+      }
     config.getRequiredConfigValue<std::string>(vishnu::MACHINEID, machineId);
     if (!config.getConfigValue<std::string>(vishnu::REMOTEBINDIR, remoteBinDirectory)) {
-      remoteBinDirectory = ExecConfiguration::getCurrentBinaryDir();
-    }
+        remoteBinDirectory = ExecConfiguration::getCurrentBinaryDir();
+      }
   } catch (UserException& e) {
     std::cerr << "\n" << e.what() << "\n\n";
     exit(1);
@@ -146,64 +145,60 @@ int main(int argc, char* argv[], char* envp[]) {
 
   if (pid > 0) {
 
-    try {
-      //Check if machineId is authorized
-      if (0 == machineId.compare(AUTOMATIC_SUBMIT_JOB_KEYWORD)){
-        std::cerr << "\n" << AUTOMATIC_SUBMIT_JOB_KEYWORD
-                  << " is not authorized as machine identifier. "
-                  << "It is a TMS keyword.\n\n";
+      try {
+        //Check if machineId is authorized
+        if (0 == machineId.compare(AUTOMATIC_SUBMIT_JOB_KEYWORD)){
+            std::cerr << "\n" << AUTOMATIC_SUBMIT_JOB_KEYWORD
+                      << " is not authorized as machine identifier. "
+                      << "It is a TMS keyword.\n\n";
+            exit(1);
+          }
+        if (0 == machineId.compare(LIST_JOBS_ON_MACHINES_KEYWORD)) {
+            std::cerr << "\n" << LIST_JOBS_ON_MACHINES_KEYWORD
+                      << " is not authorized as machine identifier. "
+                      << "It is a TMS keyword.\n\n";
+            exit(1);
+          }
+
+        //Initialize the TMS Server
+        boost::shared_ptr<ServerTMS> server (ServerTMS::getInstance());
+        res = server->init(vishnuId, dbConfig, machineId,
+                           batchType, remoteBinDirectory, defaultBatchConfig);
+
+        std::vector<std::string> ls = server.get()->getServices();
+        registerSeD(TMSTYPE, config, cfg, ls);
+
+        UMS_Data::UMS_DataFactory_ptr ecoreFactory =
+            UMS_Data::UMS_DataFactory::_instance();
+        UMS_Data::Machine_ptr machine = ecoreFactory->createMachine();
+        machine->setMachineId(machineId);
+
+        MachineServer machineServer(machine);
+        machineServer.checkMachine();
+        delete machine;
+
+        // Initialize the DIET SeD
+        if (!res) {
+            ZMQServerStart(server, uri);
+            unregisterSeD(TMSTYPE, config);
+          } else {
+            std::cerr << "\nThere was a problem during services initialization\n\n";
+            exit(1);
+          }
+      } catch (VishnuException& e) {
+        std::cerr << e.what() << "\n";
         exit(1);
       }
-      if (0 == machineId.compare(LIST_JOBS_ON_MACHINES_KEYWORD)) {
-        std::cerr << "\n" << LIST_JOBS_ON_MACHINES_KEYWORD
-                  << " is not authorized as machine identifier. "
-                  << "It is a TMS keyword.\n\n";
-        exit(1);
-      }
 
-      //Initialize the TMS Server
-      boost::shared_ptr<ServerTMS> server (ServerTMS::getInstance());
-      res = server->init(vishnuId, dbConfig, machineId,
-                         batchType, remoteBinDirectory, defaultBatchConfig);
-
-      std::vector<std::string> ls = server.get()->getServices();
-      registerSeD(TMSTYPE, config, cfg, ls);
-
-      UMS_Data::UMS_DataFactory_ptr ecoreFactory =
-        UMS_Data::UMS_DataFactory::_instance();
-      UMS_Data::Machine_ptr machine = ecoreFactory->createMachine();
-      machine->setMachineId(machineId);
-
-      MachineServer machineServer(machine);
-      machineServer.checkMachine();
-      delete machine;
-
-      // Initialize the DIET SeD
-      if (!res) {
-        ZMQServerStart(server, uri);
-        unregisterSeD(TMSTYPE, config);
-      } else {
-        std::cerr << "\nThere was a problem during services initialization\n\n";
-        exit(1);
-      }
-    } catch (VishnuException& e) {
-      std::cerr << e.what() << "\n";
+    }  else if (pid == 0) {
+      // Initialize the TMS Monitor (Opens a connection to the database)
+      MonitorTMS monitor(interval);
+      dbConfig.setDbPoolSize(1);
+      monitor.init(vishnuId, dbConfig, machineId, batchType);
+      monitor.run();
+    } else {
+      std::cerr << "\nThere was a problem to initialize the server\n\n";
       exit(1);
     }
-
-  }  else if (pid == 0) {
-    // Initialize the TMS Monitor (Opens a connection to the database)
-    MonitorTMS monitor(interval);
-    dbConfig.setDbPoolSize(1);
-    monitor.init(vishnuId, dbConfig, machineId, batchType);
-    ppid = getppid();
-
-    while(kill(ppid,0) == 0) {
-      monitor.run();
-    }
-  } else {
-    std::cerr << "\nThere was a problem to initialize the server\n\n";
-    exit(1);
-  }
   return res;
 }
