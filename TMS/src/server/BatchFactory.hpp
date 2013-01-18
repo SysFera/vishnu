64e1c6c4d3012bb66be0f105b32dd8a3bd0ffc3a
Benjamin Depardon <benjamin.depardon@sysfera.com>
2013-01-18 14:52:41 +0100

    Removed argument 'batchType' in doxygen as it was not found in the argument list of BatchFactory::getBatchServerInstance()

--- TMS/src/server/BatchFactory.hpp
+++ TMS/src/server/BatchFactory.hpp
@@ -33,7 +33,6 @@ class BatchFactory
 
     /**
      * \brief Function to create a batchServer.
-     * \param batchType The type of batchServer to create
      * \return an instance of BatchServer
      */
     BatchServer*
