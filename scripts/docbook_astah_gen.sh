e228096cc9565bac52f2fa2492757516b67a1a95
Benjamin Depardon <benjamin.depardon@sysfera.com>
2013-02-12 15:07:31 +0100

    more checks on parameters relative paths can now be used

--- scripts/docbook_astah_gen.sh
+++ scripts/docbook_astah_gen.sh
@@ -23,6 +23,19 @@
 ## ./docbook_astah_gen.sh /home/keo/GIT/generatorVishnu /home/keo/Bureau/depot_git_edf/vishnu/UMS/design/docbook/UmsDesign-template.docbook 
 
 
+if [[ ! -f copyright ]]; then
+  echo "Missing copyright file - please start the script in VISHNU root directory"
+  exit 1
+fi
+
+if [ $# -lt 3 ]; then
+  echo "Usage: " $0 " <generator_dir> <rootmodeldir> <templatefile>"
+  echo " <generator_dir> path to sysfera's git vishnuGenerator containing the docbook generators."
+  echo " <rootmodeldir> Directory containing apim and ecore"
+  echo " <templatefile> Template file"
+  exit 1
+fi
+
 
 # Path to the root of the repositories containing the generators
 generatorpath=$1
@@ -31,7 +44,7 @@ rootmodeldir=$2
 # Template file
 templatefile=$3
 
-# NOTE : KEEP 2.0.0, otherwize generation will be wrong for python lists, 2.0.0 makes generator set list as return values
+# NOTE: KEEP 2.0.0, otherwise generation will be wrong for python lists, 2.0.0 makes generator set list as return values
 version=2.0.0
 
 java -Dfile.encoding=UTF-8 -classpath $generatorpath/libs/org.eclipse.core.runtime_3.6.0.v20100505.jar:$generatorpath/libs/org.eclipse.emf.ecore.xmi_2.5.0.v20100521-1846.jar:$generatorpath/libs/org.apache.commons.cli_1.0.0.v20080604-1500.jar:$generatorpath/com.sysfera.codegen.api/bin:$generatorpath/com.sysfera.codegen.docbook/bin:$generatorpath/com.sysfera.codegen.docbook/libs/astah-pro.jar:$generatorpath/com.sysfera.codegen.docbook/libs/org.kermeta.docbook.jar:$generatorpath/com.sysfera.codegen.docbook/libs/org.eclipse.emf.ecore_2.6.1.v20100914-1218.jar:$generatorpath/com.sysfera.codegen.docbook/libs/org.eclipse.emf.common_2.6.0.v20100914-1218.jar com.sysfera.codegen.docbook.DocBookDocumentGenerator -I $rootmodeldir $templatefile $version
