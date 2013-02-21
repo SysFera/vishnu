SysFera/vishnu

commit be820e9dcf22bcd33179d741d5d61a45166e9709

fix segfault error when loading zmq_help lib

communication/DIET_client.cpp
@@ -37,113 +37,117 @@ diet_call_gen(diet_profile_t* prof, const std::string& uri);
 ExecConfiguration config;
 
 typedef std::map<std::string, std::string> ServiceMap;
-static ServiceMap sMap = boost::assign::map_list_of
-                         /* UMS services */
-                         ("sessionConnect", "UMS")
-                         ("sessionReconnect", "UMS")
-                         ("sessionClose", "UMS")
-                         ("userCreate", "UMS")
-                         ("userUpdate", "UMS")
-                         ("userDelete", "UMS")
-                         ("userPasswordChange", "UMS")
-                         ("userPasswordReset", "UMS")
-                         ("machineCreate", "UMS")
-                         ("machineUpdate", "UMS")
-                         ("machineDelete", "UMS")
-                         ("localAccountCreate", "UMS")
-                         ("localAccountUpdate", "UMS")
-                         ("localAccountDelete", "UMS")
-                         ("configurationSave", "UMS")
-                         ("configurationRestore", "UMS")
-                         ("optionValueSet", "UMS")
-                         ("optionValueSetDefault", "UMS")
-                         ("sessionList", "UMS")
-                         ("localAccountList", "UMS")
-                         ("machineList", "UMS")
-                         ("commandList", "UMS")
-                         ("optionValueList", "UMS")
-                         ("userList", "UMS")
-                         ("restore", "UMS")
-                         ("authSystemCreate", "UMS")
-                         ("authSystemUpdate", "UMS")
-                         ("authSystemDelete", "UMS")
-                         ("authSystemList", "UMS")
-                         ("authAccountCreate", "UMS")
-                         ("authAccountUpdate", "UMS")
-                         ("authAccountDelete", "UMS")
-                         ("authAccountList", "UMS")
-                         /* TMS services */
-                         ("jobSubmit", "TMS")
-                         ("jobCancel", "TMS")
-                         ("jobInfo", "TMS")
-                         ("getListOfJobs", "TMS")
-                         ("getJobsProgression", "TMS")
-                         ("getListOfQueues", "TMS")
-                         ("jobOutputGetResult", "TMS")
-                         ("jobOutputGetCompletedJobs", "TMS")
-                         ("getListOfJobs_all", "TMS")
-                         ("jobSubmit_autom", "TMS")
-                         ("addWork", "TMS")
-                         /* IMS services */
-                         ("int_exportCommands", "IMS")
-                         ("int_getMetricCurentValue", "IMS")
-                         ("int_getMetricHistory", "IMS")
-                         ("int_getProcesses", "IMS")
-                         ("int_setSystemInfo", "IMS")
-                         ("int_setSystemThreshold", "IMS")
-                         ("int_getSystemThreshold", "IMS")
-                         ("int_defineUserIdentifier", "IMS")
-                         ("int_defineJobIdentifier", "IMS")
-                         ("int_defineTransferIdentifier", "IMS")
-                         ("int_defineMachineIdentifier", "IMS")
-                         ("int_loadShed", "IMS")
-                         ("int_setUpdateFrequency", "IMS")
-                         ("int_getUpdateFrequency", "IMS")
-                         ("int_restart", "IMS")
-                         ("int_stop", "IMS")
-                         ("int_getSystemInfo", "IMS")
-                         ("int_defineAuthIdentifier", "IMS")
-                         ("int_defineWorkIdentifier", "IMS")
-                         /* FMS services */
-                         ("FileCopyAsync", "FMS")
-                         ("FileMoveAsync", "FMS")
-                         ("FileMove", "FMS")
-                         ("FileCopy", "FMS")
-                         ("FileGetInfos", "FMS")
-                         ("FileChangeGroup", "FMS")
-                         ("FileChangeMode", "FMS")
-                         ("FileHead", "FMS")
-                         ("FileContent", "FMS")
-                         ("FileCreate", "FMS")
-                         ("DirCreate", "FMS")
-                         ("FileRemove", "FMS")
-                         ("DirRemove", "FMS")
-                         ("FileTail", "FMS")
-                         ("DirList", "FMS")
-                         ("RemoteFileCopyAsync", "FMS")
-                         ("RemoteFileMoveAsync", "FMS")
-                         ("RemoteFileCopy", "FMS")
-                         ("RemoteFileMove", "FMS")
-                         ("FileTransfersList", "FMS")
-                         ("FileTransferStop", "FMS")
-                         ;
+boost::shared_ptr<ServiceMap> sMap;
 
 // defines minimal number of elements in a diet profile
 static const int PROFILE_ELT_NB = 4;
+
+
+static void fill_sMap()
+{
+  sMap = boost::shared_ptr<ServiceMap>( new ServiceMap);
+  /* UMS services */
+  (*sMap)["sessionConnect"] = "UMS";
+  (*sMap)["sessionReconnect"] = "UMS";
+  (*sMap)["sessionClose"] = "UMS";
+  (*sMap)["userCreate"] = "UMS";
+  (*sMap)["userUpdate"] = "UMS";
+  (*sMap)["userDelete"] = "UMS";
+  (*sMap)["userPasswordChange"] = "UMS";
+  (*sMap)["userPasswordReset"] = "UMS";
+  (*sMap)["machineCreate"] = "UMS";
+  (*sMap)["machineUpdate"] = "UMS";
+  (*sMap)["machineDelete"] = "UMS";
+  (*sMap)["localAccountCreate"] = "UMS";
+  (*sMap)["localAccountUpdate"] = "UMS";
+  (*sMap)["localAccountDelete"] = "UMS";
+  (*sMap)["configurationSave"] = "UMS";
+  (*sMap)["configurationRestore"] = "UMS";
+  (*sMap)["optionValueSet"] = "UMS";
+  (*sMap)["optionValueSetDefault"] = "UMS";
+  (*sMap)["sessionList"] = "UMS";
+  (*sMap)["localAccountList"] = "UMS";
+  (*sMap)["machineList"] = "UMS";
+  (*sMap)["commandList"] = "UMS";
+  (*sMap)["optionValueList"] = "UMS";
+  (*sMap)["userList"] = "UMS";
+  (*sMap)["restore"] = "UMS";
+  (*sMap)["authSystemCreate"] = "UMS";
+  (*sMap)["authSystemUpdate"] = "UMS";
+  (*sMap)["authSystemDelete"] = "UMS";
+  (*sMap)["authSystemList"] = "UMS";
+  (*sMap)["authAccountCreate"] = "UMS";
+  (*sMap)["authAccountUpdate"] = "UMS";
+  (*sMap)["authAccountDelete"] = "UMS";
+  (*sMap)["authAccountList"] = "UMS";
+  /* TMS services */
+  (*sMap)["jobSubmit"] = "TMS";
+  (*sMap)["jobCancel"] = "TMS";
+  (*sMap)["jobInfo"] = "TMS";
+  (*sMap)["getListOfJobs"] = "TMS";
+  (*sMap)["getJobsProgression"] = "TMS";
+  (*sMap)["getListOfQueues"] = "TMS";
+  (*sMap)["jobOutputGetResult"] = "TMS";
+  (*sMap)["jobOutputGetCompletedJobs"] = "TMS";
+  (*sMap)["addWork"] = "TMS";
+  /* IMS services */
+  (*sMap)["int_exportCommands"] = "IMS";
+  (*sMap)["int_getMetricCurentValue"] = "IMS";
+  (*sMap)["int_getMetricHistory"] = "IMS";
+  (*sMap)["int_getProcesses"] = "IMS";
+  (*sMap)["int_setSystemInfo"] = "IMS";
+  (*sMap)["int_setSystemThreshold"] = "IMS";
+  (*sMap)["int_getSystemThreshold"] = "IMS";
+  (*sMap)["int_defineUserIdentifier"] = "IMS";
+  (*sMap)["int_defineJobIdentifier"] = "IMS";
+  (*sMap)["int_defineTransferIdentifier"] = "IMS";
+  (*sMap)["int_defineMachineIdentifier"] = "IMS";
+  (*sMap)["int_loadShed"] = "IMS";
+  (*sMap)["int_setUpdateFrequency"] = "IMS";
+  (*sMap)["int_getUpdateFrequency"] = "IMS";
+  (*sMap)["int_restart"] = "IMS";
+  (*sMap)["int_stop"] = "IMS";
+  (*sMap)["int_getSystemInfo"] = "IMS";
+  (*sMap)["int_defineAuthIdentifier"] = "IMS";
+  (*sMap)["int_defineWorkIdentifier"] = "IMS";
+  /* FMS services */
+  (*sMap)["FileCopyAsync"] = "FMS";
+  (*sMap)["FileMoveAsync"] = "FMS";
+  (*sMap)["FileMove"] = "FMS";
+  (*sMap)["FileCopy"] = "FMS";
+  (*sMap)["FileGetInfos"] = "FMS";
+  (*sMap)["FileChangeGroup"] = "FMS";
+  (*sMap)["FileChangeMode"] = "FMS";
+  (*sMap)["FileHead"] = "FMS";
+  (*sMap)["FileContent"] = "FMS";
+  (*sMap)["FileCreate"] = "FMS";
+  (*sMap)["DirCreate"] = "FMS";
+  (*sMap)["FileRemove"] = "FMS";
+  (*sMap)["DirRemove"] = "FMS";
+  (*sMap)["FileTail"] = "FMS";
+  (*sMap)["DirList"] = "FMS";
+  (*sMap)["RemoteFileCopyAsync"] = "FMS";
+  (*sMap)["RemoteFileMoveAsync"] = "FMS";
+  (*sMap)["RemoteFileCopy"] = "FMS";
+  (*sMap)["RemoteFileMove"] = "FMS";
+  (*sMap)["FileTransfersList"] = "FMS";
+  (*sMap)["FileTransferStop"] = "FMS";
+}
 
 
 std::string
 get_module(const std::string& service) {
   std::size_t pos = service.find("@");
   ServiceMap::const_iterator it;
-
+  fill_sMap();
+  
   if (std::string::npos != pos) {
-    it = sMap.find(service.substr(0, pos));
+    it = sMap->find(service.substr(0, pos));
   } else {
-    it = sMap.find(service);
+    it = sMap->find(service);
   }
 
-  if (it != sMap.end()) {
+  if (it != sMap->end()) {
     return it->second;
   }
 

commit c7541d55681b9fe9257f9a58b550f22d91018293

Fixed another regression introduced by commit be820e9dcf22bcd33179d741d5d61a45166e9709

communication/DIET_client.cpp
@@ -90,6 +90,7 @@ static void fill_sMap()
   (*sMap)["jobOutputGetResult"] = "TMS";
   (*sMap)["jobOutputGetCompletedJobs"] = "TMS";
   (*sMap)["getListOfJobs_all"] = "TMS";
+  (*sMap)["jobSubmit_autom"] = "TMS";
   (*sMap)["addWork"] = "TMS";
   /* IMS services */
   (*sMap)["int_exportCommands"] = "IMS";