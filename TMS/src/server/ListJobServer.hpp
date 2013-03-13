719d5b025a8ff6ed48d1bbb908d1d35c049ab6da
Benjamin Depardon <benjamin.depardon@sysfera.com>
2013-03-13 13:58:11 +0100

    Finally removed all using namespace in TMS header files

--- TMS/src/server/ListJobServer.hpp
+++ TMS/src/server/ListJobServer.hpp
@@ -71,7 +71,7 @@ public:
     //To check if the number of cpu is defined and positive
     if (options->getNbCpu() > 0) {
       //To add the number of the cpu to the request
-      addOptionRequest("nbCpus", convertToString(options->getNbCpu()), sqlRequest);
+      addOptionRequest("nbCpus", vishnu::convertToString(options->getNbCpu()), sqlRequest);
     } else {
       //If the options is not the default value -1
       if (options->getNbCpu() != -1) {
@@ -81,22 +81,22 @@ public:
 
     time_t fromSubmitDate = static_cast<time_t>(options->getFromSubmitDate());
     if(fromSubmitDate != -1) {
-      fromSubmitDate = convertUTCtimeINLocaltime(fromSubmitDate);
+      fromSubmitDate = vishnu::convertUTCtimeINLocaltime(fromSubmitDate);
       std::string submitDateStr =  boost::posix_time::to_simple_string(boost::posix_time::from_time_t(fromSubmitDate));
       addTimeRequest("submitDate", submitDateStr, sqlRequest, ">=");
     }
 
     time_t toSubmitDate = static_cast<time_t>(options->getToSubmitDate());
     if(toSubmitDate != -1) {
-      toSubmitDate = convertUTCtimeINLocaltime(toSubmitDate);
+      toSubmitDate = vishnu::convertUTCtimeINLocaltime(toSubmitDate);
       std::string submitDateStr =  boost::posix_time::to_simple_string(boost::posix_time::from_time_t(toSubmitDate));
       addTimeRequest("submitDate", submitDateStr, sqlRequest, "<=");
     }
 
     //Check the job status
     if (options->getStatus() != -1) {
-      checkJobStatus(options->getStatus()); //check the job state options
-      addOptionRequest("job.status", convertToString(options->getStatus()), sqlRequest); //Append the amount of the cpu
+      vishnu::checkJobStatus(options->getStatus()); //check the job state options
+      addOptionRequest("job.status", vishnu::convertToString(options->getStatus()), sqlRequest); //Append the amount of the cpu
     } else {
       if (options->getJobId().empty() && options->getMultipleStatus().empty()) {
         sqlRequest.append(" and job.status < "+vishnu::convertToString(vishnu::STATE_COMPLETED));
@@ -162,9 +162,9 @@ public:
     //To check the job priority
     if (options->getPriority() != -1) {
       //To check the job priority options
-      checkJobPriority(options->getPriority());
+      vishnu::checkJobPriority(options->getPriority());
       //To add the number of the cpu to the request
-      addOptionRequest("jobPrio", convertToString(options->getPriority()), sqlRequest);
+      addOptionRequest("jobPrio", vishnu::convertToString(options->getPriority()), sqlRequest);
     }
 
     if (options->getOwner().size() != 0) {
@@ -257,14 +257,14 @@ public:
         job->setSubmitMachineName(*(++ii));
         job->setJobId(*(++ii));
         job->setJobName(*(++ii));
-        job->setWorkId(convertToLong(*(++ii)));
+        job->setWorkId(vishnu::convertToLong(*(++ii)));
         job->setJobPath(*(++ii));
         job->setOutputPath(*(++ii));
         job->setErrorPath(*(++ii));
-        job->setJobPrio(convertToInt(*(++ii)));
-        job->setNbCpus(convertToInt(*(++ii)));
+        job->setJobPrio(vishnu::convertToInt(*(++ii)));
+        job->setNbCpus(vishnu::convertToInt(*(++ii)));
         job->setJobWorkingDir(*(++ii));
-        jobStatus = convertToInt(*(++ii));
+        jobStatus = vishnu::convertToInt(*(++ii));
         if (jobStatus == vishnu::STATE_RUNNING) {
           nbRunningJobs++;
         } else if(jobStatus >= vishnu::STATE_SUBMITTED && jobStatus <= vishnu::STATE_WAITING) {
@@ -272,18 +272,18 @@ public:
         }
         job->setStatus(jobStatus);
         //convert the endDate into UTC date
-        submitDate = convertLocaltimeINUTCtime(convertToTimeType(*(++ii)));
+        submitDate = vishnu::convertLocaltimeINUTCtime(vishnu::convertToTimeType(*(++ii)));
         job->setSubmitDate(submitDate);
         //convert the endDate into UTC date
-        endDate = convertLocaltimeINUTCtime(convertToTimeType(*(++ii)));
+        endDate = vishnu::convertLocaltimeINUTCtime(vishnu::convertToTimeType(*(++ii)));
         job->setEndDate(endDate);
         job->setOwner(*(++ii));
         job->setJobQueue(*(++ii));
-        job->setWallClockLimit(convertToInt(*(++ii)));
+        job->setWallClockLimit(vishnu::convertToInt(*(++ii)));
         job->setGroupName(*(++ii));
         job->setJobDescription(*(++ii));
-        job->setMemLimit(convertToInt(*(++ii)));
-        job->setNbNodes(convertToInt(*(++ii)));
+        job->setMemLimit(vishnu::convertToInt(*(++ii)));
+        job->setNbNodes(vishnu::convertToInt(*(++ii)));
         job->setNbNodesAndCpuPerNode(*(++ii));
 
         batchJobId = *(++ii);
