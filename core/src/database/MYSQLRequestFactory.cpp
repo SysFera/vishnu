ae0523e7ff8e727e6a026d90f440d64a5773cfb3
kcoulomb <kevin.coulomb@sysfera.com>
2013-06-04 16:44:13 +0200

    Fix with join update problems, all ums automtest are ok on postgre now

--- core/src/database/MYSQLRequestFactory.cpp
+++ core/src/database/MYSQLRequestFactory.cpp
@@ -4,6 +4,8 @@
 
 MYSQLRequestFactory::MYSQLRequestFactory():RequestFactory(){
   mrequest.insert (std::pair<int, std::string>(VR_UPDATE_ACCOUNT_WITH_USERS, "UPDATE account, users SET account.status='%1%' WHERE users.numuserid=account.users_numuserid and users.userid='%2%';"));
+  mrequest.insert (std::pair<int, std::string>(VR_UPDATE_ACCOUNT_WITH_MACHINE, "UPDATE account, machine SET account.status='%1%' WHERE machine.nummachineid=account.machine_nummachineid and machine.machineid='%2%';"));
+  mrequest.insert (std::pair<int, std::string>(VR_UPDATE_AUTHACCOUNT_WITH_AUTHSYSTEM, "UPDATE authaccount, authsystem SET authaccount.status='%1%' WHERE authsystem.numauthsystemid=authaccount.authsystem_authsystemid and authsystem.authsystemid='%2%';"));
 }
 
 MYSQLRequestFactory::~MYSQLRequestFactory(){
--- core/src/database/PGSQLRequestFactory.cpp
+++ core/src/database/PGSQLRequestFactory.cpp
@@ -4,6 +4,8 @@
 
 PGSQLRequestFactory::PGSQLRequestFactory():RequestFactory(){
   mrequest.insert (std::pair<int, std::string>(VR_UPDATE_ACCOUNT_WITH_USERS, "update account set status=%1 from users where users.numuserid=account.users_numuserid and users.userid='%2';"));
+  mrequest.insert (std::pair<int, std::string>(VR_UPDATE_ACCOUNT_WITH_MACHINE, "update account set status=%1 from machine where machine.nummachineid=account.machine_nummachineid and machine.machineid='%2';"));
+  mrequest.insert (std::pair<int, std::string>(VR_UPDATE_AUTHACCOUNT_WITH_AUTHSYSTEM, "update authaccount set status=%1 from authsystem where authsystem.numauthsystemid=authaccount.authsystem_numauthsystemid and authsystem.authsystemid='%2';"));
 }
 
 PGSQLRequestFactory::~PGSQLRequestFactory(){
