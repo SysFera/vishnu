585062344e1a7299e191e646a1176ac5b7dbc462
R. Chakode <rodrigue.chakode@sysfera.com>
2013-06-07 17:54:46 +0200

    fixed for make TLS optional

--- communication/dispatcher/dispatcher.cpp
+++ communication/dispatcher/dispatcher.cpp
@@ -67,59 +67,74 @@ Dispatcher::configureHandlers() {
   const std::string BACKEND_IPC_URI = "ipc:///tmp/vishnu-disp.back.sock";
   std::string rsaPrivkey;
   std::string sslCertificate;
-  bool useSsl = false;
-  if (config.getConfigValue<bool>(vishnu::USE_SSL, useSsl) &&  useSsl) {
-    config.getRequiredConfigValue<std::string>(vishnu::SERVER_PRIVATE_KEY, rsaPrivkey);
-    config.getRequiredConfigValue<std::string>(vishnu::SERVER_SSL_CERTICATE, sslCertificate);
-  }
 
-  pid_t pid = fork();
-  if (pid > 0) {
-    clientHandler.reset(new Handler4Clients(FRONTEND_IPC_URI, ann, nthread));
-    serverHandler.reset(new Handler4Servers(BACKEND_IPC_URI, ann, nthread));
+  bool useSsl = false;
+  if (! config.getConfigValue<bool>(vishnu::USE_SSL, useSsl) ||
+      ! useSsl) { /* TLS dont required */
+    clientHandler.reset(new Handler4Clients(uriAddr, ann, nthread));
+    serverHandler.reset(new Handler4Servers(uriSubs, ann, nthread));
     boost::thread th1(boost::bind(&Handler4Clients::run, clientHandler.get()));
     boost::thread th2(boost::bind(&Handler4Servers::run, serverHandler.get()));
     th1.join();
     th2.join();
-  } else if (pid == 0) {
-    pid_t pidTlsHandler = fork();
+  } else {
+    config.getRequiredConfigValue<std::string>(vishnu::SERVER_PRIVATE_KEY, rsaPrivkey);
+    config.getRequiredConfigValue<std::string>(vishnu::SERVER_SSL_CERTICATE, sslCertificate);
 
-    if (pidTlsHandler > 0) {
+    pid_t pid = fork();
+    if (pid > 0) {
+      clientHandler.reset(new Handler4Clients(FRONTEND_IPC_URI, ann, nthread));
+      serverHandler.reset(new Handler4Servers(BACKEND_IPC_URI, ann, nthread));
+      boost::thread th1(boost::bind(&Handler4Clients::run, clientHandler.get()));
+      boost::thread th2(boost::bind(&Handler4Servers::run, serverHandler.get()));
+      th1.join();
+      th2.join();
+    } else if (pid == 0) {
+      pid_t pidTlsHandler = fork();
 
-      /* Intializing the Frontend TLS Handler */
-      int sslPort = getPortFromUri(uriAddr);
-      if (sslPort <= 0 ) {
-        std::cerr << boost::format("[ERROR] *** Incorrect uri set for the service (%1%) ***\n")%uriAddr;
-        abort();
-      }
+      if (pidTlsHandler > 0) {
 
-      TlsServer tlsFrontHandler(rsaPrivkey, sslCertificate, sslPort, FRONTEND_IPC_URI);
-      try {
-        tlsFrontHandler.run();
-      } catch(VishnuException& ex) {
-        std::cerr << boost::format("[ERROR] *** %1% ***\n")%ex.what();
-        abort();
-      } catch(...) {
-        std::cerr << boost::format("[ERROR] *** %1% ***\n")%tlsFrontHandler.getErrorMsg();
-        abort();
-      }
+        /* Intializing the Frontend TLS Handler */
+        int sslPort = getPortFromUri(uriAddr);
+        if (sslPort <= 0 ) {
+          std::cerr << boost::format("[ERROR] *** Incorrect uri set for the service (%1%) ***\n")%uriAddr;
+          abort();
+        }
 
-    } else if (pidTlsHandler ==  0) {
+        config.getRequiredConfigValue<std::string>(vishnu::SERVER_PRIVATE_KEY, rsaPrivkey);
+        config.getRequiredConfigValue<std::string>(vishnu::SERVER_SSL_CERTICATE, sslCertificate);
+        TlsServer tlsFrontHandler(rsaPrivkey, sslCertificate, sslPort, FRONTEND_IPC_URI);
+        try {
+          tlsFrontHandler.run();
+        } catch(VishnuException& ex) {
+          std::cerr << boost::format("[ERROR] *** %1% ***\n")%ex.what();
+          abort();
+        } catch(...) {
+          std::cerr << boost::format("[ERROR] *** %1% ***\n")%tlsFrontHandler.getErrorMsg();
+          abort();
+        }
 
-      /* Intializing the Backend TLS Handler (for subscription) */
-      int port = getPortFromUri(uriSubs);
-      if (port <= 0 ) {
-        std::cerr << boost::format("[ERROR] *** Incorrect uri set for the service (%1%) ***\n")%uriSubs;
-        abort();
-      }
-      TlsServer tlsBackHandler(rsaPrivkey, sslCertificate, port, BACKEND_IPC_URI);
-      try {
-        tlsBackHandler.run();
-      } catch(VishnuException& ex) {
-        std::cerr << boost::format("[ERROR] *** %1% ***\n")%ex.what();
-        abort();
-      } catch(...) {
-        std::cerr << boost::format("[ERROR] *** %1% ***\n")%tlsBackHandler.getErrorMsg();
+      } else if (pidTlsHandler ==  0) {
+
+        /* Intializing the Backend TLS Handler (for subscription) */
+        int port = getPortFromUri(uriSubs);
+        if (port <= 0 ) {
+          std::cerr << boost::format("[ERROR] *** Incorrect uri set for the service (%1%) ***\n")%uriSubs;
+          abort();
+        }
+        TlsServer tlsBackHandler(rsaPrivkey, sslCertificate, port, BACKEND_IPC_URI);
+        try {
+          tlsBackHandler.run();
+        } catch(VishnuException& ex) {
+          std::cerr << boost::format("[ERROR] *** %1% ***\n")%ex.what();
+          abort();
+        } catch(...) {
+          std::cerr << boost::format("[ERROR] *** %1% ***\n")%tlsBackHandler.getErrorMsg();
+          abort();
+        }
+
+      } else {
+        std::cerr << "[ERROR] *** Problem initializing the service ***\n";
         abort();
       }
 
@@ -127,10 +142,6 @@ Dispatcher::configureHandlers() {
       std::cerr << "[ERROR] *** Problem initializing the service ***\n";
       abort();
     }
-
-  } else {
-    std::cerr << "[ERROR] *** Problem initializing the service ***\n";
-    abort();
   }
 }
 
