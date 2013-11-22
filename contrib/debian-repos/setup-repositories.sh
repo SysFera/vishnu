3156b5486290b49af28d54266caaeddb0d766b4b
R. Chakode <rodrigue.chakode@sysfera.com>
2013-11-22 13:34:12 +0100

    make the building of debian repository automatic

--- contrib/debian-repos/setup-repositories.sh
+++ contrib/debian-repos/setup-repositories.sh
@@ -11,15 +11,54 @@
 # ** You MUST run the script from the current directory
 # ** The script assume that 
 
-PACKAGE_BIN_DIR=$1
+if [ $# -ne 1 ] ; then
+  echo "`basename $0` <vishnu_version>"
+  exit 1
+fi
+
+VERSION=$1
 
 if [ $# -ne 1 ]; then
   echo "basename $0 <package_bin_dir>"
   exit 1
 fi
 
-DISTS="squeeze wheezy"
-for dist in $DISTS; do
-  reprepro -Vb . includedeb $dist $PACKAGE_BIN_DIR/$dist/*.deb
-  reprepro -Vb . includedsc $dist $PACKAGE_BIN_DIR/$dist/*.dsc
-done
+mkdir -p obs-packages; cd obs-packages
+
+# Download packages for debian wheezy
+dist="wheezy"
+archs="i386 amd64"
+
+mkdir $dist; cd $dist
+wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/all/vishnu-config_${VERSION}_all.deb 
+wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/all/vishnu-dev_${VERSION}_all.deb
+wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/vishnu-${VERSION}.tgz_${VERSION}.diff.gz
+wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/vishnu-${VERSION}.tgz_${VERSION}.dsc 
+wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/vishnu-${VERSION}.tgz_${VERSION}.orig.tar.gz
+for arch in $archs; do 
+  wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/$arch/libvishnu_${VERSION}_${arch}.deb 
+  wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/$arch/vishnu-client_${VERSION}_${arch}.deb
+  wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/$arch/vishnu-server_${VERSION}_${arch}.deb
+  wget -N http://download.opensuse.org/repositories/home:/techsysfera/Debian_7.0/$arch/vishnu_${VERSION}_${arch}.deb  
+done 
+
+TGZ=vishnu-${VERSION}.tgz_${VERSION}.orig.tar.gz
+DSC=vishnu-${VERSION}.tgz_${VERSION}.dsc
+DSC_TMP=../../vishnu.dsc
+DEFAULT_VERSION=X.Y.Z
+
+tgz_md5sum=`md5sum ${TGZ} | awk '{print $1}'`
+tgz_size=`stat --printf "%s" ${TGZ}`
+sed --in-place "s/$DEFAULT_VERSION/$VERSION/g" ${DSC_TMP}
+sed --in-place "s/<TGZ_MD5SUM>/$tgz_md5sum/g" ${DSC_TMP}
+sed --in-place "s/<TGZ_SIZE>/$tgz_size/g" ${DSC_TMP}
+cp ${DSC_TMP} $DSC
+
+# Make repository
+cd ../..
+echo $PWD
+reprepro -Vb . includedeb $dist $PWD/obs-packages/$dist/*.deb
+reprepro -Vb . includedsc $dist $PWD/obs-packages/$dist/*.dsc
+
+# Copy to OVH
+#scp -r conf/ dists/ db/ pool/ root@91.121.160.100:/var/lib/lxc/sysfera/rootfs/var/www/web/download/vishnu/repos/debs
---
+++ contrib/debian-repos/vishnu.dsc
@@ -0,0 +1,16 @@
+Format: 1.0
+Source: vishnu-3.2.0.tgz
+Binary: vishnu
+Architecture: any
+Version: 3.2.0
+Maintainer: Rodrigue Chakode <rodrigue.chakode@sysfera.com>
+Homepage: http://sysfera.github.io/vishnu.html
+Standards-Version: 3.9.3
+Build-Depends: debhelper (>= 4.1.16), g++ (>= 4.6.3), libzmq-dev, cmake (>=2.8.5), libboost-all-dev, swig (>=1.3.40), openjdk-6-jdk, libmysqlclient-dev
+Package-List: 
+ vishnu deb admin extra 
+Files:   
+ ddbf62f1be434ff4303313ab3564e9c7 6145251 vishnu-3.2.0.tgz
+ 00c912021a3aa56a93b49313751bad7d 1817 vishnu-3.2.0.diff.gz
+
+
