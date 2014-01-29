79d4779b14dca0b9f42b1da750c12748815bee72
Haikel Guemar <haikel.guemar@sysfera.com>
2014-01-29 15:30:05 +0100

    VersionProxy: code formatting fixes

--- UMS/src/client/VersionProxy.cpp
+++ UMS/src/client/VersionProxy.cpp
@@ -39,11 +39,13 @@ VersionProxy::getLongestService(std::vector<std::string>& list,
 }
 
 void
-VersionProxy::selectServerToPing(std::string filterName, std::vector<boost::shared_ptr<Server> >& allServers, std::vector<boost::shared_ptr<Server> >& result){
+VersionProxy::selectServerToPing(std::string filterName,
+                                 std::vector<boost::shared_ptr<Server> >& allServers,
+                                 std::vector<boost::shared_ptr<Server> >& result) {
   std::vector<boost::shared_ptr<Server> >::iterator it;
-  for (it = allServers.begin() ; it != allServers.end() ; ++it){
-    if( (filterName.empty() ||
-         it->get()->getName()==filterName) ){
+
+  for (it = allServers.begin() ; it != allServers.end() ; ++it) {
+    if (filterName.empty() || (*it)->getName() == filterName ) {
       result.push_back(*it);
     }
   }
@@ -51,13 +53,16 @@ VersionProxy::selectServerToPing(std::string filterName, std::vector<boost::shar
 
 
 void
-VersionProxy::getVersion(const std::string& server, const std::string mid, std::map<std::string, std::string>& result){
+VersionProxy::getVersion(const std::string& server,
+                         const std::string mid,
+                         std::map<std::string, std::string>& result) {
   std::string response;
-// Get version of the dispatcher
   bool empty = true;
   int res = communicate_dispatcher("3", response);
-  if (server=="dispatcher" || (server.empty() && mid.empty() && res!=-1)){
-    result.insert(std::pair<std::string, std::string>("dispatcher", response));
+
+  if (server == "dispatcher" ||
+      (server.empty() && mid.empty() && res !=- 1)) {
+    result.insert(std::make_pair("dispatcher", response));
     empty = false;
   }
 
@@ -67,15 +72,13 @@ VersionProxy::getVersion(const std::string& server, const std::string mid, std::
   std::vector<boost::shared_ptr<Server> > allServers;
   std::vector<boost::shared_ptr<Server> > servers;
   extractServersFromMessage(response, allServers);
-
   selectServerToPing(server, allServers, servers);
-
 
   // For each server get its version
   std::vector<boost::shared_ptr<Server> >::iterator it;
-  for (it = servers.begin() ; it != servers.end() ; ++it){
+  for (it = servers.begin() ; it != servers.end() ; ++it) {
     diet_profile_t* profile = NULL;
-    std::string service = getLongestService(it->get()->getServices(), "heartbeat");
+    std::string service = getLongestService((*it)->getServices(), "heartbeat");
     // If service asked on a machine and none corresponding
     if (!mid.empty() && service.find(mid) == std::string::npos)
       continue;
