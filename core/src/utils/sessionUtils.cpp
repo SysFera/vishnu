3f9242d7dc3b50eb794dad3761138e21ebb159a1
kcoulomb <kevin.coulomb@sysfera.com>
2014-03-31 13:53:14 +0200

    Fix regression (with old PR integrated when should not have been)

--- core/src/utils/sessionUtils.cpp
+++ core/src/utils/sessionUtils.cpp
@@ -11,10 +11,9 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <boost/interprocess/sync/file_lock.hpp>
-#include <boost/interprocess/sync/scoped_lock.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <sys/stat.h>
 
 #include "UMSVishnuException.hpp"
 #include "VishnuException.hpp"
@@ -22,7 +21,6 @@
 
 namespace bfs = boost::filesystem;
 namespace bs = boost::serialization;
-namespace bi = boost::interprocess;
 using namespace boost::archive;
 
 
@@ -140,20 +138,16 @@ operator <<(std::ostream& os, const SessionEntry& session){
 
 template <class T>
 void
-saveIntoFile(SessionContainer& allSessions, const char* file) {
-  try {
-    bi::file_lock sessionLock(file);
-    bi::scoped_lock<bi::file_lock> lock(sessionLock);
+saveIntoFile(SessionContainer& allSessions, const char* file){
+  {
+
     std::ofstream ofile (file);
 
     T ar(ofile);
 
     ar << bs::make_nvp("sessions",allSessions);
-
-    ofile.close();
-  } catch (const bi::interprocess_exception& e) {
-    std::cerr << "ERROR: " << e.what() << "\n";
   }
+  chmod(file, S_IRUSR | S_IWUSR);
 }
 
 
@@ -248,7 +242,7 @@ getLastSession(const std::string& terminal){
     //std::cerr <<"There is no open session in this terminal\n";
 
     //exit (CLI_ERROR_NO_SESSION);
-    throw UserException (ERRCODE_CLI_ERROR_NO_SESSION,"There is no open session in this terminal");
+    throw UserException (ERRCODE_CLI_ERROR_NO_SESSION,"There is no open session in this shell terminal");
 
   }
 
