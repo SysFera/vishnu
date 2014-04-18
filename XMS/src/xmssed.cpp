1133f711f044f5e6c7b55a1cf1bddcc23b4eb16d
Haikel Guemar <haikel.guemar@sysfera.com>
2014-04-18 17:41:01 +0200

    b: POSIX backend has no version attached

--- XMS/src/xmssed.cpp
+++ XMS/src/xmssed.cpp
@@ -55,6 +55,7 @@ getBatchConfiguration(SedConfig& cfg) {
 
   if (cfg.batchType == DELTACLOUD || cfg.batchType == OPENNEBULA) {
     exportCloudSpecificParam(&cfg.config);
+  } else if (cfg.batchType == POSIX) {
   } else {
     std::string versError;
     if (! vishnu::checkIfSupportedBatchVersion(cfg.batchType, cfg.batchVersion, versError)) {
