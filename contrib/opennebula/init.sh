ec87ae83a650a2fb86811057d298113c3660df7f
R. Chakode <rodrigue.chakode@sysfera.com>
2014-05-27 18:29:12 +0200

    updated templates

--- contrib/opennebula/init.sh
+++ contrib/opennebula/init.sh
@@ -33,19 +33,20 @@ if [ -n "$NETMASK" ]; then
 	ifconfig eth0 netmask $NETMASK
 fi
 
-
-if [ -f /mnt/$ROOT_PUBKEY ]; then
-	mkdir -p /root/.ssh
-	cat /mnt/$ROOT_PUBKEY >> /root/.ssh/authorized_keys
-	chmod -R 600 /root/.ssh/
-fi
-
+# commented since root public key is handled by cloud-init
+#if [ -f /mnt/$ROOT_PUBKEY ]; then
+#	mkdir -p /root/.ssh
+#	cat /mnt/$ROOT_PUBKEY >> /root/.ssh/authorized_keys
+#	chmod -R 600 /root/.ssh/
+#fi
+#
 if [ -n "$USERNAME" ]; then
 	useradd $USERNAME
-	if [ -f /mnt/$USER_PUBKEY ]; then
+	if [ -n "$USER_PUBKEY" ]; then
 		mkdir -p /home/$USERNAME/.ssh/
-		cat /mnt/$USER_PUBKEY >> /home/$USERNAME/.ssh/authorized_keys
+		echo "$USER_PUBKEY"  >> /home/$USERNAME/.ssh/authorized_keys
 		chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+		chmod -R 700 /home/$USERNAME/.ssh/
 		chmod -R 600 /home/$USERNAME/.ssh/authorized_keys
 	fi
 fi
