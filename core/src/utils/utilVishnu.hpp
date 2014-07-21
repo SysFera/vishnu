fd87a4a7d6ad835e32481cdad215dc4bf0aab2cc
R. Chakode <rodrigue.chakode@sysfera.com>
2014-07-21 11:46:22 +0200

    refactoring the sourcing of vishnurc to simplify the call

--- core/src/utils/utilVishnu.hpp
+++ core/src/utils/utilVishnu.hpp
@@ -41,80 +41,80 @@ crypt(const char* clef, const char* salt);
  * \brief This naspace contains utils functions of the vishnu system
  */
 namespace vishnu {
-static const std::string ROOTUSERNAME = "root";
-static const std::string UMSMAPPERNAME = "UMS";
-static const std::string TMSMAPPERNAME = "TMS";
-static const std::string FMSMAPPERNAME = "FMS";
-static const int PASSWORD_MAX_SIZE = 8;
-static const int PASSWORD_MIN_SIZE = 6;
-static const int DEFAUT_TIMEOUT = 10;
+  static const std::string ROOTUSERNAME = "root";
+  static const std::string UMSMAPPERNAME = "UMS";
+  static const std::string TMSMAPPERNAME = "TMS";
+  static const std::string FMSMAPPERNAME = "FMS";
+  static const int PASSWORD_MAX_SIZE = 8;
+  static const int PASSWORD_MIN_SIZE = 6;
+  static const int DEFAUT_TIMEOUT = 10;
 
-static boost::shared_mutex mutex;
-/**
+  static boost::shared_mutex mutex;
+  /**
  * \brief Generic function to convert an object to string
  * \param val is a generic data to be transformed to string
  * \return the string version of val
  */
-template <class T>
-std::string
-convertToString(T val) {
-  std::ostringstream out;
-  out << val;
-  return out.str();
-}
+  template <class T>
+  std::string
+  convertToString(T val) {
+    std::ostringstream out;
+    out << val;
+    return out.str();
+  }
 
-/**
+  /**
  * \brief Function to convert a string to int
  * \param  val a value to convert to int
  * \return int value of the corresponding string
  */
-int
-convertToInt(const std::string& val);
+  int
+  convertToInt(const std::string& val);
 
-/**
+  /**
  * \brief Function to convert a string to long int
  * \param  val a value to convert to int
  * \return int value of the corresponding string
  */
-long
-convertToLong(const std::string& val) ;
+  long
+  convertToLong(const std::string& val) ;
 
-/**
+  /**
  * \brief To crypt a password
  * \param salt The salt to use to crypt
  * \param password The password to crypt
  * \param encrypted The flag that password encryption
  * \return The crypted password
  */
-std::string
-cryptPassword(const std::string& salt, const std::string& password, bool encrypted = true) ;
+  std::string
+  cryptPassword(const std::string& salt, const std::string& password, bool encrypted = true) ;
 
-/**
+  /**
  * \brief Function to get a random number
  * \return the number generated
  */
-int
-generateNumbers();
+  int
+  generateNumbers();
 
-/**
+  /**
  * \brief To retrieve the password
  * \param prompt: The message inviting the user to enter his/her password
  * \return The password entered.
  */
-std::string
-takePassword(const std::string& prompt);
+  std::string
+  takePassword(const std::string& prompt);
 
-/**
+  /**
  * \brief Simple function to convert time
  * from string format (YYYY-MM-DD H:mm:ss) to
  * long integer format in seconds
  * \param ts: the time in string format
  * \return the time in long integer format in seconds
  */
-std::time_t
-string_to_time_t(const std::string& ts);
+  std::time_t
+  string_to_time_t(const std::string& ts);
 
-/**
+  /**
  * \brief Simple function to convert time
  * from string format (YYYY-MM-DD H:M:S) to
  * long integer format in seconds in utc
@@ -124,201 +124,201 @@ string_to_time_t(const std::string& ts);
  */
 
 
-std::time_t
-string_lc_to_utc_time_t(const std::string & ts,const std::string& utcOffset);
+  std::time_t
+  string_lc_to_utc_time_t(const std::string & ts,const std::string& utcOffset);
 
 
-/**
+  /**
  * \brief Simple function to read the content of a regular file
  * \param filePath: the path to the file
  * \param rejectEmptyFile: tell whether empty file will be rejected
  * \return The content of the file
  */
-std::string
-get_file_content(const std::string& filePath, const bool& rejectEmptyFile=true);
+  std::string
+  get_file_content(const std::string& filePath, const bool& rejectEmptyFile=true);
 
-/**
+  /**
  * \brief Function to move file
  * \param src: the path of the file to move
  * \param dest: the destination where the file will be moved
  * \param filename: the name of the file in dest
  * \return raises an exception on error
  */
-int
-boostMoveFile(const std::string& src, const std::string& dest,  const std::string& filename="");
+  int
+  boostMoveFile(const std::string& src, const std::string& dest,  const std::string& filename="");
 
-/**
+  /**
  * \brief Function to check a numerical value
  * \param value The value to check
  * \return raises an exception on error
  */
-bool
-isNumericalValue(const std::string& value);
+  bool
+  isNumericalValue(const std::string& value);
 
 
-/**
+  /**
  * \brief Return all the IP of the machines (IPV4, except 127.0.0.1)
  * \return The IP of the machine
  */
-std::vector<std::string>
-getIPList();
+  std::vector<std::string>
+  getIPList();
 
-/**
+  /**
  * \brief Replace localhost by the IP of the machine in the path
  * \param name The string to replace the machine name
  * \param IP The IP of the machine
  */
-void
-setIP(std::string& name, std::string IP);
+  void
+  setIP(std::string& name, std::string IP);
 
-/**
+  /**
  * \brief Tells if the string is an IP address
  * \param name The string to test
  * \return true is it is not an IPv4 or IPv6 address
  */
-bool
-isNotIP(std::string name);
+  bool
+  isNotIP(std::string name);
 
 
-/**
+  /**
  * \brief Function to get current time in seconds (UTC)
  * \return the time as the number of seconds since the Epoch, 1970-01-01 00:00:00 +0000 (UTC)
  */
-time_t
-getCurrentTimeInUTC();
+  time_t
+  getCurrentTimeInUTC();
 
-/**
+  /**
  * \brief Function to convert UTC time into localtime (seconds)
  * \param localtime the local time
  * \return the correspondant localtime (seconds)
  */
-time_t
-convertUTCtimeINLocaltime(const time_t& localtime);
+  time_t
+  convertUTCtimeINLocaltime(const time_t& localtime);
 
-/**
+  /**
  * \brief Function to return the difference between localtime and UTC time (seconds)
  * \return the difference time (seconds)
  */
-long
-diffLocaltimeUTCtime();
+  long
+  diffLocaltimeUTCtime();
 
-/**
+  /**
  * \brief Function to delete file
  * \param fileName The name of the file to create
  * \return 0 in case of success, another value otherwise
  */
-int
-deleteFile(const char* fileName);
+  int
+  deleteFile(const char* fileName);
 
-/**
+  /**
  * \brief Function to check the metric value
  * \param metric: the metric value
  * \return raises an exception on error
  */
-void
-checkMetricHistoryValue(const int& metric);
+  void
+  checkMetricHistoryValue(const int& metric);
 
-/**
+  /**
  * \brief Function to check the remote file path
  * \param path the remote file path
  * \return raises an exception on error
  */
-void
-checkRemotePath(const std::string& path);
+  void
+  checkRemotePath(const std::string& path);
 
-/**
+  /**
  * \brief Check if the pid exists
  * \param pid The given pid
  * \param proc_dir The given process info directory
  * \return True if the pid exists
  */
-bool
-process_exists(const std::string& pid, const bfs::path& proc_dir="/proc");
+  bool
+  process_exists(const std::string& pid, const bfs::path& proc_dir="/proc");
 
-/**
+  /**
  * \brief Get the fully qualified name for the current system
  * \param port the port
  * \return the fully qualified name for the current system
  */
-std::string
-getLocalMachineName(const std::string& port );
+  std::string
+  getLocalMachineName(const std::string& port );
 
-/**
+  /**
  * \brief Function to check if a string is empty
  * \param str The string to check
  * \param compMsg The complementary message to print
  * \return raises an exception on error
  */
-void
-checkEmptyString(const std::string& str,
-                 const std::string& compMsg);
+  void
+  checkEmptyString(const std::string& str,
+                   const std::string& compMsg);
 
-/**
+  /**
  * \brief Function to create a unique file suffix with the current time
  * \return tha suffix created
  * */
-std::string
-generatedUniquePatternFromCurTime(const std::string& prefix) ;
+  std::string
+  generatedUniquePatternFromCurTime(const std::string& prefix) ;
 
-/**
+  /**
  * \brief Function to make a link from a given
  * \param src : the source file
  * \return the path of the link
  * Throw exception on error
  */
-std::string
-mklink(const std::string& src) ;
+  std::string
+  mklink(const std::string& src) ;
 
-/**
+  /**
  * \brief Function to parse the string representing the version
  * \param version the string representing the version
  * \return  The version object
  */
-UMS_Data::Version_ptr
-parseVersion(const std::string& version);
+  UMS_Data::Version_ptr
+  parseVersion(const std::string& version);
 
 
-/**
+  /**
  * \brief Source an configuration file
- * \param filePath: the path to the file
+ * \param
  */
-void
-sourceFile(const std::string& filePath);
+  void
+  sourceRcFile(void);
 
-/**
+  /**
  * \brief  function to convert a status (locked, active...) to string
  * \param status: The status to convert
  * \return The corresponding string
  */
-std::string
-statusToString(const int& status);
+  std::string
+  statusToString(const int& status);
 
-/**
+  /**
  * @brief validateSshPath: check that a file path doesn't contain some special characters subject to security issues
  * @param path : the path
  * @return
  */
-void
-validatePath(const std::string& path);
+  void
+  validatePath(const std::string& path);
 
 
-/**
+  /**
  * @brief Execute a system command and return whether it succeeded or not
  * @param command The command
  * @param msg The standard output/error message
  * @return A string
  */
-bool
-execSystemCommand(const std::string& command, std::string& msg);
+  bool
+  execSystemCommand(const std::string& command, std::string& msg);
 
 
-/**
+  /**
  * @brief Make a given file executable
  * @param path The path of the file
  * @return Throw exception on error
  */
-void
-makeFileExecutable(const std::string& path);
+  void
+  makeFileExecutable(const std::string& path);
 
 } //END NAMESPACE
 #endif // _UTILVISHNU_H_
